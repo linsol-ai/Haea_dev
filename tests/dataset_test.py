23029952b4377f8771221d4c38a8e266edf327a7
wooj1nBot <1403jung@gmail.com>
2024-02-06 18:52:54 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     lon_min, lon_max = 120, 139.5
 
     # isel 함수 대신 sel 함수를 사용하여 경위도 범위를 필터링
-    arr = arr.sel(latitude=slice(lat_min, lat_max), longitude=slice(lon_min, lon_max))
+    arr = arr.sel(latitude=slice(lat_max, lat_max), longitude=slice(lon_min, lon_max))
 
     # 새 차원을 추가하고 데이터 변수를 결합
 

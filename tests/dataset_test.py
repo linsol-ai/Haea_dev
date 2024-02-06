3e489d8ad4bdb1f79deb1d2dc81b71afe8456228
wooj1nBot <1403jung@gmail.com>
2024-02-06 18:47:05 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -12,12 +12,12 @@ variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_
 # 파이프라인 실행
 if __name__ == '__main__':
     drop_vars.remove(variable)
-    
+
     start = time.time()
     ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', 
                           consolidated=True, 
                           chunks={'time': 10},
-                          drop_variables=[]
+                          drop_variables=drop_vars
                           )
     print(f'size: {ds.nbytes / (1024 ** 4)} TiB')
     start_date = pd.to_datetime('2021-01-01')

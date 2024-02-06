dad22a6675c898ecd80bf7f2e617e86f0ed683c6
wooj1nBot <1403jung@gmail.com>
2024-02-06 18:29:37 +0000

    Auto commit

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -30,7 +30,9 @@ class HAEADataset(Dataset):
             src = [(t, v) for v in range(self.var_len)]
             tgt = []
             for t_n in range(t, min(self.dataset.size(0), t + self.time_len)):
-                
+                tgt.extend(
+                    
+                )
 
 
     def __getitem__(self, item):

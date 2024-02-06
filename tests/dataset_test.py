0cebedb761a1b7c0fb0093179d4a26a7835a6f42
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:57:40 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -16,11 +16,11 @@ def run_pipeline():
     with beam.Pipeline(options=pipeline_options) as p:
         _ = (
             p
-            | xbeam.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr')  # NetCDF 파일 읽기
-            | xbeam.DatasetToChunks()  # 데이터셋을 청크로 분할
-            | beam.Map(lambda chunk: chunk.mean(dim='time'))  # 시간에 따른 평균 계산
-            | xbeam.ChunksToDataset()  # 결과 청크를 데이터셋으로 병합
-            | xbeam.WriteNetcdf('path/to/output/data.nc')  # 결과 NetCDF 파일로 저장
+            | 'Read NetCDF' >> xbeam.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr')  # NetCDF 파일 읽기
+            | 'Dataset to chunks' >> xbeam.DatasetToChunks()  # 데이터셋을 청크로 분할
+            | 'Calculate mean' >> beam.Map(lambda chunk: chunk.mean(dim='time'))  # 시간에 따른 평균 계산
+            | 'Chunks to dataset' >> xbeam.ChunksToDataset()  # 결과 청크를 데이터셋으로 병합
+            | 'Write results' >> xbeam.WriteNetcdf('path/to/output/data.nc')  # 결과 NetCDF 파일로 저장
         )
 
 

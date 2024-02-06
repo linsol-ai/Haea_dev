e112bfe9ae5f654d9dbf2d796e605545107a29c3
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:51:30 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -18,7 +18,7 @@ def run_pipeline():
             p
             | xbeam.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr')  # NetCDF 파일 읽기
             | xbeam.DatasetToChunks()  # 데이터셋을 청크로 분할
-            | 'Calculate mean' >> beam.Map(lambda chunk: chunk.mean(dim='time'))  # 시간에 따른 평균 계산
+            | beam.Map(lambda chunk: chunk.mean(dim='time'))  # 시간에 따른 평균 계산
             | 'Chunks to dataset' >> xbeam.ChunksToDataset()  # 결과 청크를 데이터셋으로 병합
         )
 

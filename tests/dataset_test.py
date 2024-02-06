19b8146fbf46ae27fb895969f2f2a1bc11955e80
wooj1nBot <1403jung@gmail.com>
2024-02-06 18:46:27 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     print(f'size: {ds.nbytes / (1024 ** 4)} TiB')
     start_date = pd.to_datetime('2021-01-01')
     end_date = pd.to_datetime('2021-02-01')
-    variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
+   
 
     arr = ds.sel(time=slice(start_date, end_date))
     lat_min, lat_max = 24.5, 44.0

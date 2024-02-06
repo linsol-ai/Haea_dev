8967ef93ba4f458acce1939b15691ac0e9c61195
wooj1nBot <1403jung@gmail.com>
2024-02-06 19:01:08 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     stacked_ds = combined_ds.stack(variable_level=('variable', 'level'))
     stacked_ds = stacked_ds.stack(hidden_dim=('latitude', 'longitude'))
     print(stacked_ds)
-    arr = stacked_ds.compute()
+    numpy_array = result.compute().values
 
     end = time.time()
     print(f"{end - start:.5f} sec")

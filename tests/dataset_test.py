20a82a283c6334eb53c546c6092ec1b2e99ef435
wooj1nBot <1403jung@gmail.com>
2024-02-06 12:15:17 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -29,5 +29,10 @@ if __name__ == '__main__':
 
     variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
     arr = ds[variable]
-    arr = arr.compute()
-    print(arr)
+    # 새 차원을 추가하고 데이터 변수를 결합
+data_arrays = [arr[var].expand_dims('variable').assign_coords(variable=[var]) for var in variable]
+combined_ds = xarray.concat(data_arrays, dim='variable')
+# 결과 확인
+stacked_ds = combined_ds.stack(variable_level=('variable', 'level'))
+stacked_ds = stacked_ds.stack(hidden_dim=('latitude', 'longitude'))
+

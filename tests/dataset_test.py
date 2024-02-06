b1626549bcdc72f4b3234e2f92d38cb5e21ea389
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:41:31 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -12,7 +12,7 @@ import apache_beam as beam
 
 ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', chunks={'time': 100})
 start_date = pd.to_datetime('2021-01-01')
-end_date = pd.to_datetime('2021-08-01')
+end_date = pd.to_datetime('2021-12-01')
 lat_min, lat_max = 24.5, 44.0
 lon_min, lon_max = 120, 139.5
 

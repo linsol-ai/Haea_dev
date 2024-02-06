f6457dd102b75770db7ecba23b030e82695cfa9b
wooj1nBot <1403jung@gmail.com>
2024-02-06 13:11:42 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # 파이프라인 실행
 if __name__ == '__main__':
-    ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', chunks={'time': 50})
+    ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', chunks={'time': 10})
     start_date = pd.to_datetime('2021-01-01')
     end_date = pd.to_datetime('2021-08-01')
     ds = ds.sel(time=slice(start_date, end_date))

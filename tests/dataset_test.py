366fdc5af587e9ded67a82074489106096f8ad5f
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:34:44 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -13,6 +13,9 @@ import apache_beam as beam
 ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', chunks={'time': 1000})
 start_date = pd.to_datetime('2021-01-01')
 end_date = pd.to_datetime('2021-08-01')
+ lat_min, lat_max = 24.5, 44.0
+    lon_min, lon_max = 120, 139.5
+
 ds = ds.sel(time=slice(start_date, end_date))
 lat_indices = np.where((ds.latitude >= lat_min) & (ds.latitude <= lat_max))[0]
 lon_indices = np.where((ds.longitude >= lon_min) & (ds.longitude <= lon_max))[0]

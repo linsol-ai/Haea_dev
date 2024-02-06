df21e9951c5ab9bd16d1f674a9195f68ca4f1090
wooj1nBot <1403jung@gmail.com>
2024-02-06 09:05:36 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -17,7 +17,7 @@ def run_pipeline(dataset:xarray.Dataset):
 
 # 파이프라인 실행
 if __name__ == '__main__':
-    ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-full_37-1h-512x256_equiangular_conservative.zarr')
+    ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-full_37-1h-512x256_equiangular_conservative.zarr', )
     start_date = pd.to_datetime('2021-01-01')
     end_date = pd.to_datetime('2021-08-01')
     ds = ds.sel(time=slice(start_date, end_date))

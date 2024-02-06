41b8673c47a70207e9aeabfee83112fb50bcdecb
wooj1nBot <1403jung@gmail.com>
2024-02-06 16:24:29 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -98,7 +98,7 @@ class WeatherDataset:
         arr = self.ds[key]
         arr = arr.sel(time=slice(start_date, end_date))
         data = arr.sel(level=level)
-        
+        arr = arr.sel(latitude=slice(lat_max, lat_min), longitude=slice(lon_min, lon_max))
         data = data.to_numpy()
         data = torch.from_numpy(data)
         if normalize:

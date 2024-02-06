a5bc0c52fb408d6ae9422e5aef83dc7dcc914abf
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:28:48 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -8,4 +8,4 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 import torch
 
-ds, chunks = xbeam.open_zarr(self.url)
+ds, chunks = xbeam.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr')

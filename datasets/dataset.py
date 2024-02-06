d5a366365d08d371f6421e13a2e1a93f2cfc427e
wooj1nBot <1403jung@gmail.com>
2024-02-06 18:30:44 +0000

    Auto commit

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -34,7 +34,7 @@ class HAEADataset(Dataset):
                     [(t_n, v) for v in range(self.var_len)]
                 )
             dataset.append((src, tgt))
-        return 
+        return dataset
 
 
     def __getitem__(self, item):

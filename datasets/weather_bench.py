270049b0802a7bcd42e854a84ef677f28ed99996
wooj1nBot <1403jung@gmail.com>
2024-02-06 06:41:51 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -263,6 +263,9 @@ class WeatherDataset:
 
 if __name__ == '__main__':
     weather = WeatherDataset(url='gs://weatherbench2/datasets/era5/1959-2023_01_10-full_37-1h-512x256_equiangular_conservative.zarr')
+        start_date = pd.to_datetime('2021-01-01')
+    end_date = pd.to_datetime('2021-08-01')
+    
     weather.load_init(start_date, end_date)
 
     variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']

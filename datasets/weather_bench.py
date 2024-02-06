7259ba18f114062fb607865039595beea1b612cc
wooj1nBot <1403jung@gmail.com>
2024-02-06 06:41:48 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -268,9 +268,6 @@ if __name__ == '__main__':
     variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
     levels = [50, 100, 150, 200, 250, 300, 400, 500, 600, 700, 850, 925, 1000]
 
-    start_date = pd.to_datetime('2021-01-01')
-    end_date = pd.to_datetime('2021-08-01')
-
     device = ("cuda" if torch.cuda.is_available() else "cpu" )
     device = torch.device(device)
 

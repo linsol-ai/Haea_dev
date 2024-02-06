e6652b05a7687994e0c48d4134e043fdd514e15d
wooj1nBot <1403jung@gmail.com>
2024-02-06 16:24:53 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -95,6 +95,7 @@ class WeatherDataset:
 
 
     def load_level_val(self, key, level, start_date, end_date, normalize):
+        
         arr = self.ds[key]
         arr = arr.sel(time=slice(start_date, end_date))
         data = arr.sel(level=level)

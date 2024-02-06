7ad62f8271e3dac4b135ec2158ea1177dc73a2cf
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:04:02 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -99,7 +99,7 @@ class WeatherDataset:
         data = arr.sel(level=level)
 
         lat_indices = np.where((data.latitude >= lat_min) & (data.latitude <= lat_max))[0]
-        lon_indices = np.where((data.longitude >= lon_min) & (ds.longitude <= lon_max))[0]
+        lon_indices = np.where((data.longitude >= lon_min) & (data.longitude <= lon_max))[0]
 
         data = data.to_numpy()
         data = torch.from_numpy(data)

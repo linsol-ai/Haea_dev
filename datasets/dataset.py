557217c999cfea7a11d3630cfcc3af7693517f6c
wooj1nBot <1403jung@gmail.com>
2024-02-06 18:24:26 +0000

    Auto commit

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -26,7 +26,7 @@ class HAEADataset(Dataset):
     def make_dataset(self):
         dataset = []
         for t in range(len(self.dataset)):
-            src = [(t, v) for ]
+            src = [(t, v) for v in range()]
 
     def __getitem__(self, item):
         t1, t2, is_next_label = self.random_sent(item)

98e730ea0db86afac7dac496ebfef6aff866e471
wooj1nBot <1403jung@gmail.com>
2024-02-06 20:38:03 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -77,7 +77,6 @@ def main(argv):
             | xbeam.DatasetToChunks(source_dataset, source_chunks)
             | xbeam.SplitChunks({'time': 1})
             | beam.MapTuple(rekey_chunk_on_month_hour)
-    
         )
 
 

73509a0d5e1fa18fd8d47651a908ac146094927f
wooj1nBot <1403jung@gmail.com>
2024-02-06 20:29:27 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -67,7 +67,7 @@ def main(argv):
     template = (
         xbeam.make_template(source_dataset)
         .sel(time=slice(start_date, end_date))
-       
+       .sel(latitude=slice(lat_max, lat_min), longitude=slice(lon_min, lon_max))
     )
     output_chunks = {'hour': 1, 'month': 1}
 

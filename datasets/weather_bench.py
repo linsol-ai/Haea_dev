26b7eac3c9f0624a2a6d51dfde95643d147b74ef
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:08:03 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -268,7 +268,7 @@ class WeatherDataset:
 
 
 if __name__ == '__main__':
-    weather = WeatherDataset(url='gs://weatherbench2/datasets/era5/1959-2023_01_10-full_37-1h-512x256_equiangular_conservative.zarr')
+    weather = WeatherDataset(url='gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr')
     weather.load_init()
 
     variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']

0a04151bd0d5758a997b4610ae9dfea52230aaee
wooj1nBot <1403jung@gmail.com>
2024-02-06 16:23:27 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -279,4 +279,4 @@ if __name__ == '__main__':
     device = torch.device(device)
 
 
-    output = weather.load_bart(variable, levels, start_date, end_date, (lat_min, la), (), 128, device)
+    output = weather.load_bart(variable, levels, start_date, end_date, (lat_min, lat_max), (), 128, device)

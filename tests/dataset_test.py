8d66594f0eeeb55730de4b569c18bfc6673ef048
wooj1nBot <1403jung@gmail.com>
2024-02-06 11:32:58 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -17,12 +17,12 @@ def run_pipeline(dataset:xarray.Dataset):
 
 # 파이프라인 실행
 if __name__ == '__main__':
-ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', chunks={'time': 10})
-start_date = pd.to_datetime('2021-01-01')
-end_date = pd.to_datetime('2021-08-01')
-ds = ds.sel(time=slice(start_date, end_date))
-lat_min, lat_max = 32.2, 39.0
-lon_min, lon_max = 124.2, 131
-lat_indices = np.where((ds.latitude >= lat_min) & (ds.latitude <= lat_max))[0]
-lon_indices = np.where((ds.longitude >= lon_min) & (ds.longitude <= lon_max))[0]
-ds = ds.isel(latitude=lat_indices, longitude=lon_indices)
+    ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', chunks={'time': 10})
+    start_date = pd.to_datetime('2021-01-01')
+    end_date = pd.to_datetime('2021-08-01')
+    ds = ds.sel(time=slice(start_date, end_date))
+    lat_min, lat_max = 32.2, 39.0
+    lon_min, lon_max = 124.2, 131
+    lat_indices = np.where((ds.latitude >= lat_min) & (ds.latitude <= lat_max))[0]
+    lon_indices = np.where((ds.longitude >= lon_min) & (ds.longitude <= lon_max))[0]
+    ds = ds.isel(latitude=lat_indices, longitude=lon_indices)

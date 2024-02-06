6d902851dc1f8d88febf27620597f9d746eecfcc
wooj1nBot <1403jung@gmail.com>
2024-02-06 20:46:13 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -40,11 +40,12 @@ def rekey_chunk_on_month_hour(
 
 def main():
     source_dataset, source_chunks = xbeam.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr')
-    
+
     start_date = pd.to_datetime('2021-01-01')
     end_date = pd.to_datetime('2021-02-01')
     lat_min, lat_max = 32.2, 39.0
     lon_min, lon_max = 124.2, 131
+    
     template = (
       xbeam.make_template(source_dataset)
       .sel(time=slice(start_date, end_date))

7aaf850dcc1eaaa885d9ac8f26efcbb2ccbe9161
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:15:57 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -76,7 +76,7 @@ class WeatherDataset:
         ds, chunks = xbeam.open_zarr(self.url)
         ds = ds.sel(time=slice(start_date, end_date))
         lat_min, lat_max = lat
-    lon_min, lon_max = 124.2, 131
+        lon_min, lon_max = lon
         lat_indices = np.where((ds.latitude >= lat_min) & (ds.latitude <= lat_max))[0]
         lon_indices = np.where((ds.longitude >= lon_min) & (ds.longitude <= lon_max))[0]
         ds = ds.isel(latitude=lat_indices, longitude=lon_indices)

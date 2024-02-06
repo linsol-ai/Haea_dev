a03a79d969d3ae6e8bcdc3e60ac5cc852a638eb6
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:18:40 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -71,7 +71,7 @@ class WeatherDataset:
         self.url = url
     
 
-    def load_init(self, start_date, end_date, lat, lon, vars, levels):
+    def load_init(self, start_date, end_date, lat, lon, vars):
         print("데이터셋 불러오는 중...")
         ds, chunks = xbeam.open_zarr(self.url)
         ds = ds.sel(time=slice(start_date, end_date))

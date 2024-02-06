2753e35ea301bf16ad77cac3a8f082166ceea158
wooj1nBot <1403jung@gmail.com>
2024-02-06 07:34:13 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -13,7 +13,7 @@ import apache_beam as beam
 ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', chunks={'time': 1000})
 start_date = pd.to_datetime('2021-01-01')
 end_date = pd.to_datetime('2021-08-01')
-
+ds = ds.sel(time=slice(start_date, end_date))
 
 with beam.Pipeline() as p:
     p | xbeam.DatasetToChunks(ds) | beam.MapTuple(lambda k, v: print(k, type(v)))

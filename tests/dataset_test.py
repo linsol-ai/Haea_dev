5b726a6f877bfe60ee5ea22108643d2cda381a28
wooj1nBot <1403jung@gmail.com>
2024-02-06 16:14:08 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -11,7 +11,6 @@ import time
 if __name__ == '__main__':
     start = time.time()
     ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr', consolidated=True, chunks={'latitude': 'auto', 'longitude': 'auto'})
-    ds = ds.chunk({'latitude': 'auto', 'longitude': 100})
     print(f'size: {ds.nbytes / (1024 ** 4)} TiB')
     start_date = pd.to_datetime('2021-01-01')
     end_date = pd.to_datetime('2021-01-03')

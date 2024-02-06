7439eb4275b50d19817d57f3b8862c9212b46874
wooj1nBot <1403jung@gmail.com>
2024-02-06 12:16:00 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -35,5 +35,6 @@ if __name__ == '__main__':
     # 결과 확인
     stacked_ds = combined_ds.stack(variable_level=('variable', 'level'))
     stacked_ds = stacked_ds.stack(hidden_dim=('latitude', 'longitude'))
-    
+    arr = stacked_ds.compute()
+print(arr)
 

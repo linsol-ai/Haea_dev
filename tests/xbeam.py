6ec6d8a8162a5210d26fd31aa183e05c2d5badcb
wooj1nBot <1403jung@gmail.com>
2024-02-07 15:27:00 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -1,80 +1,50 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Calculate climatology for the Pangeo ERA5 surface dataset."""
-from typing import Tuple
-
-from absl import app
-from absl import flags
 import apache_beam as beam
-import numpy as np
-import xarray
-import xarray_beam as xbeam
-import pandas as pd
-import xarray as xr
 from apache_beam.options.pipeline_options import PipelineOptions
+import xarraybeam as xbeam
+import xarray as xr
+import zarr
+import gcsfs
 
-variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
+class PreprocessERA5Data(beam.DoFn):
+    def process(self, element, bucket_name, variable, latitude_range, longitude_range, time_range):
+        fs = gcsfs.GCSFileSystem(project='your-gcp-project')
+        store = fs.get_mapper(f'gs://{bucket_name}/{element}')
+        ds = xr.open_zarr(store)
+        
+        # 경위도 및 시간대에 따라 데이터 필터링
+        ds_filtered = ds.sel(latitude=slice(*latitude_range), longitude=slice(*longitude_range), time=slice(*time_range))
+        
+        # 선택된 변수만 유지
+        ds_filtered = ds_filtered[[variable]]
+        
+        # 결과를 Zarr 파일로 저장
+        output_store = fs.get_mapper(f'gs://{bucket_name}/preprocessed/{element}_preprocessed.zarr')
+        ds_filtered.to_zarr(output_store, mode='w')
+        
+        yield f'Processed and saved: {element}_preprocessed.zarr'
 
-
-def proprocess_data(
-    key: xbeam.Key, dataset: xarray.Dataset
-) -> Tuple[xbeam.Key, xarray.Dataset]:
-
-    dataset = dataset[variable]
-    
-    start_date = pd.to_datetime('2021-01-01')
-    end_date = pd.to_datetime('2021-02-01')
-   
-    dataset = dataset.sel(time=slice(start_date, end_date))
-    lat_min, lat_max = 32.2, 39.0
-    lon_min, lon_max = 124.2, 131
-
-    # isel 함수 대신 sel 함수를 사용하여 경위도 범위를 필터링
-    dataset = dataset.sel(latitude=slice(lat_max, lat_min), longitude=slice(lon_min, lon_max))
-
-    return key, dataset
-
-
-
-def main():
-
-    source_dataset, source_chunks = xbeam.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr')
-
-    print(source_chunks)
-
-    # Apache Beam 파이프라인 설정
-    options = PipelineOptions(
-        input='',
-        output='',
+def run_pipeline(bucket_name, input_filename, variable, latitude_range, longitude_range, time_range):
+    pipeline_options = PipelineOptions(
         runner='DataflowRunner',
-        project='genfit-7ba0d',
-        job_name='unique-job-name',
-        temp_location='gs://dataflow_preprocess/test',
-        region='us-east1',
-        requirements_file='/workspace/Haea/req.txt'
+        project='your-gcp-project',
+        temp_location=f'gs://{bucket_name}/temp',
+        region='your-gcp-region',
     )
-
-    with beam.Pipeline(options=options) as p:
-        # 데이터셋을 Beam PCollection으로 로드
+    
+    with beam.Pipeline(options=pipeline_options) as p:
         (
-            p 
-            | "Read Dataset" >> xbeam.DatasetToChunks(source_dataset, {'time': 10}, split_vars=False,)
-            | "Preprocess Dataset" >> beam.MapTuple(proprocess_data)
-            | "Save Dataset" >> xbeam.ChunksToZarr('6h-1440x721.zarr')
+            p
+            | 'CreateFileList' >> beam.Create([input_filename])
+            | 'PreprocessData' >> beam.ParDo(PreprocessERA5Data(), bucket_name, variable, latitude_range, longitude_range, time_range)
         )
 
-
-
+# 파이프라인 실행
 if __name__ == '__main__':
-  main()
+    bucket_name = 'your-bucket-name'
+    input_filename = 'path/to/your/data.zarr'  # Zarr 파일 경로
+    variable = 'your-variable-name'
+    latitude_range = (-10, 10)  # 예시 경위도 범위
+    longitude_range = (100, 120)  # 예시 경도 범위
+    time_range = ('2020-01-01', '2020-12-31')  # 예시 시간 범위
+    
+    run_pipeline(bucket_name, input_filename, variable, latitude_range, longitude_range, time_range)

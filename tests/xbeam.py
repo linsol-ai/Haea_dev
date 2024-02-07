a99b3331fe7ec5f4463c4224263d235b7d74f40e
wooj1nBot <1403jung@gmail.com>
2024-02-07 19:21:44 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -27,7 +27,7 @@ def preprocess_dataset(element):
     return ds_filtered
 
 def run():
-    dataset = xarray.open_zarr(INPUT_ZARR_PATH)
+    source_dataset, source_chunks = xbeam.open_zarr(INPUT_PATH.value)
     template = (
       xarray_beam.make_template(dataset)
       .sel(time=slice('2023-01-01', '2023-01-31'), latitude=slice(32.2, 39.0), longitude=slice(124.2, 131))

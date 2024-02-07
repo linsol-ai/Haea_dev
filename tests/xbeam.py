c1c3133702d0e207aeabf7ac6b5b71a1037a5d17
wooj1nBot <1403jung@gmail.com>
2024-02-07 17:22:44 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -24,7 +24,7 @@ def run():
     with beam.Pipeline(options=options) as p:
         _ = (
             p
-            | 'OpenZarrDataset' >> xr.open_zarr(INPUT_ZARR_PATH, ch)
+            | 'OpenZarrDataset' >> xr.open_zarr(INPUT_ZARR_PATH, chunks=None)
             | 'PreprocessDataset' >> beam.Map(preprocess_dataset)
             | 'WriteZarrToGCS' >> xarray_beam.ChunksToZarr(OUTPUT_ZARR_PATH)
         )

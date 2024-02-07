ab9abca4649f21dfa2abc88ff4dd64dac2a42ddf
wooj1nBot <1403jung@gmail.com>
2024-02-07 17:42:17 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -27,7 +27,7 @@ def run():
         _ = (
             p
             | 'CreateDatasetPattern' >> beam.Create([INPUT_ZARR_PATH])
-            | 'OpenZarrDataset' >> beam.Map(xr.open_zarr(INPUT_ZARR_PATH, chunks=None))
+            | 'OpenZarrDataset' >> beam.Map(xr.open_zarr, chunks=None))
             | 'ChunkingDataset' >> beam.Map(xbeam.DatasetToChunks, chunks={'time: 10'}, split_vars=False)
             | 'PreprocessDataset' >> beam.Map(preprocess_dataset)
             | 'WriteZarrToGCS' >> xbeam.ChunksToZarr(OUTPUT_ZARR_PATH)

819f8b96b18745d35af793dfc655603a4b522b89
wooj1nBot <1403jung@gmail.com>
2024-02-07 18:28:46 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -40,7 +40,7 @@ def run():
             | 'ChunkingDataset' >> xarray_beam.DatasetToChunks(d1, chunks={'time': 10}, split_vars=False)
             | 'PreprocessDataset' >> beam.Map(preprocess_dataset)
         )
-        __name__ = (
+        _ = (
             d1
             | 'ChunkingDataset' >> xarray_beam.DatasetToChunks(d1, chunks={'time': 10}, split_vars=False)
             | 'PreprocessDataset' >> beam.Map(preprocess_dataset)

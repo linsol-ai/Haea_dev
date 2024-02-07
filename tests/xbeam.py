ffc4ee6dd930d0a61af1933cb9b24345a9acf49f
wooj1nBot <1403jung@gmail.com>
2024-02-07 17:30:01 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -25,8 +25,7 @@ def run():
         _ = (
             p
             | 'OpenZarrDataset' >> xr.open_zarr(INPUT_ZARR_PATH, chunks=None)
-            | xbeam.DatasetToChunks(
-          forecast,
+            | xbeam.DatasetToChunks(forecast,
           self.input_chunks,
           split_vars=False,
       )

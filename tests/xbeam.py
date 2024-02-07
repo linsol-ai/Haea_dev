eb32873b203af29726bf81c356b9357f53ef4ce9
wooj1nBot <1403jung@gmail.com>
2024-02-07 19:05:26 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -22,6 +22,7 @@ def save_chunk(dataset):
     return xarray_beam.ChunksToZarr(dataset, OUTPUT_ZARR_PATH)
 
 def preprocess_dataset(ds):
+    ds 
     ds_filtered = ds.sel(time=slice('2023-01-01', '2023-01-31'), lat=slice(30, 50), lon=slice(-130, -60))
     return ds_filtered
 

ad09bf89dcfc86eabe106c83390af0610511bbf3
wooj1nBot <1403jung@gmail.com>
2024-02-07 09:55:32 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -59,7 +59,7 @@ def main():
         # 데이터셋을 Beam PCollection으로 로드
         dataset = (
             p 
-            | "Read Dataset" >> xbeam.DatasetToChunks(source_dataset, {'time': 10}, split_vars=False,)
+            | "Read Dataset" >> xbeam.DatasetToChunks(source_dataset, {'time': 10, ''}, split_vars=False,)
             | "PreprocessData" >> beam.MapTuple(PreprocessData)
             | print
         )

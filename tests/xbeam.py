bc124d7a650799c455c2b82b11ebf11f4e0c4807
wooj1nBot <1403jung@gmail.com>
2024-02-07 19:53:49 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -28,10 +28,6 @@ def preprocess_dataset(element):
 
 def run():
     dataset = xarray.open_zarr(INPUT_ZARR_PATH, chunks=None)
-    template = (
-      xarray_beam.make_template(dataset)
-      .sel(time=slice('2023-01-01', '2023-01-31'), latitude=slice(32.2, 39.0), longitude=slice(124.2, 131))
-   ) 
     with beam.Pipeline(options=options) as p:
         _ = (
             p

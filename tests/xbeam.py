74c606d7679fc9be864d7c6b516266912ca8db42
wooj1nBot <1403jung@gmail.com>
2024-02-07 09:51:42 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -52,6 +52,18 @@ def main():
     lon_min, lon_max = 124.2, 131
     source_dataset, source_chunks = xbeam.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr')
 
+    dataset = dataset[variable]
+    start_date = pd.to_datetime('2021-01-01')
+    end_date = pd.to_datetime('2021-02-01')
+   
+
+    dataset = dataset.sel(time=slice(start_date, end_date))
+    lat_min, lat_max = 32.2, 39.0
+    lon_min, lon_max = 124.2, 131
+
+    # isel 함수 대신 sel 함수를 사용하여 경위도 범위를 필터링
+    dataset = dataset.sel(latitude=slice(lat_max, lat_min), longitude=slice(lon_min, lon_max))
+
     print(source_chunks)
 
     # Apache Beam 파이프라인 설정

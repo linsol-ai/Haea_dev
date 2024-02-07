c58329dc06ac6eabdfb39f50cb62647e890fdf8c
wooj1nBot <1403jung@gmail.com>
2024-02-07 19:07:34 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -23,7 +23,7 @@ def save_chunk(dataset):
 
 def preprocess_dataset(element):
     ds = element[1] 
-    ds_filtered = ds.sel(time=slice('2023-01-01', '2023-01-31'), lat=slice(30, 50), lon=slice(-130, -60))
+    ds_filtered = ds.sel(time=slice('2023-01-01', '2023-01-31'), latitude=slice(30, 50), lon=slice(-130, -60))
     return ds_filtered
 
 def run():

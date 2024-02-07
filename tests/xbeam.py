4bd54e1eceed0ab73e83015e9419c6bcc8414cc1
wooj1nBot <1403jung@gmail.com>
2024-02-07 14:26:15 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -35,7 +35,6 @@ def proprocess_data(
     start_date = pd.to_datetime('2021-01-01')
     end_date = pd.to_datetime('2021-02-01')
    
-
     dataset = dataset.sel(time=slice(start_date, end_date))
     lat_min, lat_max = 32.2, 39.0
     lon_min, lon_max = 124.2, 131

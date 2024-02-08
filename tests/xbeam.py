4ac040924621fe8dd4d75a998aa8237184219a6a
wooj1nBot <1403jung@gmail.com>
2024-02-08 13:24:23 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -2,7 +2,7 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 import xarray
 import xarray_beam
-
+import pandas as pd
 
 # GCS 경로 설정
 GCS_BUCKET = 'dataflow_preprocess'

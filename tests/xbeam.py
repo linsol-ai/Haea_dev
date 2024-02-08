9e808c8cafc25062a48381da4df8967bae3334e6
wooj1nBot <1403jung@gmail.com>
2024-02-08 17:04:39 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -13,7 +13,7 @@ import pandas as pd
 INPUT_PATH = 'gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr'
 OUTPUT_PATH = 'gs://dataflow_preprocess/test/test.zarr'
 RUNNER = 'DirectRunner'
-VARIABLE = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
+ = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
 
 # pylint: disable=expression-not-assigned
 
@@ -28,7 +28,7 @@ def rekey_chunk_on_month_hour(
 
 def main(argv):
   
-  c, source_chunks = xbeam.open_zarr(INPUT_PATH)
+  source_dataset, source_chunks = xbeam.open_zarr(INPUT_PATH)
 
   start_date = pd.to_datetime('2021-01-01')
   end_date = pd.to_datetime('2021-08-01')

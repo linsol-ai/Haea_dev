e894961f01b591c1485e0c90bc13287a79f5a5ea
wooj1nBot <1403jung@gmail.com>
2024-02-08 16:47:20 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -10,7 +10,7 @@ variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_
 # 파이프라인 실행
 if __name__ == '__main__':
     start = time.time()
-    ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr', 
+    ds = xarray.open_zarr('ㅓ', 
                           consolidated=True, 
                           chunks={'time':10},
                           drop_variables=drop_vars

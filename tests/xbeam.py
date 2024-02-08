259a417816a82f056d64167698ec89edfa33694d
wooj1nBot <1403jung@gmail.com>
2024-02-08 20:13:33 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -25,7 +25,7 @@ VARIABLE = HAS_LEVEL_VARIABLE + NONE_LEVEL_VARIABLE
 
 # pylint: disable=expression-not-assigned
 
-START_DATE = flags.DEFINE_string('start_date', None, help='Input Zarr path')
+START_DATE = flags.DEFINE_string('start_date', None, help='start_date')
 END_DATE = flags.DEFINE_string('end_date', None, help='Output Zarr path')
 NUM_WORKER = 
 

a319cf82d7bf8663e9219da9e9b2d4c50484f982
wooj1nBot <1403jung@gmail.com>
2024-02-08 17:04:21 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -13,7 +13,8 @@ import pandas as pd
 INPUT_PATH = 'gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr'
 OUTPUT_PATH = 'gs://dataflow_preprocess/test/test.zarr'
 RUNNER = 'DirectRunner'
-variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
+
+ = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
 
 # pylint: disable=expression-not-assigned
 

f01b82d5d765d8ba78209e2eef7bc842c41b92fa
wooj1nBot <1403jung@gmail.com>
2024-02-08 21:07:09 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -72,7 +72,8 @@ def main(argv):
         region='us-central1',
         requirements_file='/workspace/Haea/req.txt',
         num_workers=FLAGS.num_worker,
-        usePublicIps=False
+        usePublicIps=False,
+        autoscaling_algorithm
   )
 
 

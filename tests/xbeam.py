50bdb3470275047bfd13cb65ea588a870f5c8d89
wooj1nBot <1403jung@gmail.com>
2024-02-08 16:59:08 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 
 INPUT_PATH = 'gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr'
-OUTPUT_PATH = 'gs://dataflow_preprocess/'
+OUTPUT_PATH = 'gs://dataflow_preprocess/test/test.zarr'
 RUNNER = flags.DEFINE_string('runner', None, 'beam.runners.Runner')
 
 

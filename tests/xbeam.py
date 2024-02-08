54797cd5625a8fc5309cd2979607a5a9e3f37df2
wooj1nBot <1403jung@gmail.com>
2024-02-08 17:19:03 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -51,7 +51,7 @@ def main(argv):
         runner='DataflowRunner',
         project='your-gcp-project',
         temp_location=f'gs://era5_preprocess/temp',
-        region='your-gcp-region',
+        region='era5_preprocess',
   )
 
 

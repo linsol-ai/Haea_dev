fc2b089e9864ab43d91c56295a8f7fdbaee74166
wooj1nBot <1403jung@gmail.com>
2024-02-08 16:55:22 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -43,8 +43,8 @@ def main(argv):
   lon_min, lon_max = 124.2, 131
 
   # 해당 범위에 속하는 위도와 경도의 인덱스 찾기
-  lat_indices = np.where((ds.latitude >= lat_min) & (ds.latitude <= lat_max))[0]
-  lon_indices = np.where((ds.longitude >= lon_min) & (ds.longitude <= lon_max))[0]
+  lat_indices = np.where((source_dataset.latitude >= lat_min) & (ds.latitude <= lat_max))[0]
+  lon_indices = np.where((source_dataset.longitude >= lon_min) & (ds.longitude <= lon_max))[0]
 
   output_chunks = {'time': 1, 'month': 1}
 

e19f2c62da4d6aef934b4624f7b5a3168ea803ac
wooj1nBot <1403jung@gmail.com>
2024-02-08 18:23:52 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 INPUT_PATH = 'gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr'
-OUTPUT_PATH = 'gs://era5_preprocess/test/test.zarr'
+OUTPUT_PATH = 'gs://era5_preprocess/d/test.zarr'
 RUNNER = 'DirectRunner'
 HAS_LEVEL_VARIABLE = [
   'geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']

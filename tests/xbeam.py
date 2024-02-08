901db067c0d740f196e41d7e87a6a9848b03a689
wooj1nBot <1403jung@gmail.com>
2024-02-08 20:12:41 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -2,7 +2,7 @@
 from typing import Tuple
 from apache_beam.options.pipeline_options import PipelineOptions
 from absl import app
-END_DATE
+from absl import flags
 import apache_beam as beam
 import numpy as np
 import xarray

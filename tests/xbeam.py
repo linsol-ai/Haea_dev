1b7b51df18bdc59f855a1ec39345e5f18c6d85e3
wooj1nBot <1403jung@gmail.com>
2024-02-08 14:54:31 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -28,7 +28,7 @@ def rekey_chunk_on_month_hour(
 def main(argv):
   variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
   source_dataset, source_chunks = xbeam.open_zarr(INPUT_PATH)
-  source_dataset = source_dataset[]
+  source_dataset = source_dataset[variable]
   template = (
       xbeam.make_template(source_dataset)
       .isel(time=0, drop=True)

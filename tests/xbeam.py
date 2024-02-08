7a8379036925dc3ed9b158d4bd9b67d07f92bb92
wooj1nBot <1403jung@gmail.com>
2024-02-08 17:05:05 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -29,7 +29,7 @@ def rekey_chunk_on_month_hour(
 def main(argv):
   
   source_dataset, source_chunks = xbeam.open_zarr(INPUT_PATH)
-  source_dataset =
+  source_dataset = 
   start_date = pd.to_datetime('2021-01-01')
   end_date = pd.to_datetime('2021-08-01')
   source_dataset = source_dataset.sel(time=slice(start_date, end_date))

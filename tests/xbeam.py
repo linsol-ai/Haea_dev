ab60a4372e8a94f79ee2a94280ef8837091c7329
wooj1nBot <1403jung@gmail.com>
2024-02-08 22:26:30 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -71,6 +71,7 @@ def main(argv):
         temp_location='gs://era5_preprocess/temp',
         region='us-central1',
         requirements_file='/workspace/Haea/req.txt',
+        --num_workers
   )
 
 

0ea25071251343614d61f4e3773f3f4b223ad39b
wooj1nBot <1403jung@gmail.com>
2024-02-08 16:55:39 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -39,13 +39,6 @@ def main(argv):
       .expand_dims(month=np.arange(1, max_month + 1), hour=np.arange(24))
   )
 
-  lat_min, lat_max = 32.2, 39.0
-  lon_min, lon_max = 124.2, 131
-
-  # 해당 범위에 속하는 위도와 경도의 인덱스 찾기
-  lat_indices = np.where((source_dataset.latitude >= lat_min) & (source_dataset.latitude <= lat_max))[0]
-  lon_indices = np.where((source_dataset.longitude >= lon_min) & (source_dataset.longitude <= lon_max))[0]
-
   output_chunks = {'time': 1, 'month': 1}
 
   with beam.Pipeline(runner=RUNNER.value, argv=argv) as root:

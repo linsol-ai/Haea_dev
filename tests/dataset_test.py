0ffe882fa4b166e467d4876248bc2fb28d873116
wooj1nBot <1403jung@gmail.com>
2024-02-08 17:43:39 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -11,7 +11,7 @@ variable = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_
 if __name__ == '__main__':
 
     start = time.time()
-    ds = xarray.open_zarr('gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721.zarr', 
+    ds = xarray.open_zarr('gs://era5_preprocess/test/test.zarr', 
                           consolidated=True, 
                           chunks=None,
                           )

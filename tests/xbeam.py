4af228a5685c0fa09f0801606062820fd9862116
wooj1nBot <1403jung@gmail.com>
2024-02-08 14:27:21 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -54,7 +54,7 @@ def main(argv):
         | xbeam.SplitChunks({'time': 1})
         | beam.MapTuple(rekey_chunk_on_month_hour)
         | xbeam.Mean.PerKey()
-        | xbeam.ChunksToZarr(OUTPUT_PATH, template, output_chunks)
+        | xbeam.ChunksToZarr(OUTPUT_PATH, template, None)
     )
 
 

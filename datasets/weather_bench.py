f84608d5e2e5b546ca3b80e46729c10c63376ca0
wooj1nBot <1403jung@gmail.com>
2024-02-09 16:39:11 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -225,7 +225,7 @@ class WeatherDataset:
 
         # wind.shape => (level * 3, time, h, w)
         wind_dataset = torch.stack(wind_dataset, dim=0)
-        wind_dataset = wind_dataset.view(-1, wind_dataset.shape[])
+        wind_dataset = wind_dataset.view(-1, wind_dataset.shape[2:])
         
         # shape => (time, level, 3, h, w)
         wind_dataset = torch.swapaxes(wind_dataset, 0, 1)

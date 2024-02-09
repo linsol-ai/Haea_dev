86726a527ef5cfe7999355c620dbe71a574986d6
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:33:44 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -128,7 +128,7 @@ class WeatherDataset:
 
     def load_variable(self, key, level=None):
         arr = self.ds[key]
-        if l
+        if level is 
         data = arr.sel(level=level)
         data = data.to_numpy()
         data = remove_missing_values(data)

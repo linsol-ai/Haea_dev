adc2e90ff9bd4be09d3a625c4d6c648b17fd189f
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:20:30 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -114,7 +114,7 @@ class WeatherDataset:
 
     
 
-    def load_dataset(self):
+    def load_dataset(datasets):
         print("데이터셋 불러오는 중...")
         ds, chunks = xr.open_zarr(self.url)
         self.ds = ds

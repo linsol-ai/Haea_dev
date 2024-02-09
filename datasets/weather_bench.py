fb57e5bc3470ca93a734bfc966fc8a7524c6ae94
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:32:17 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -132,7 +132,7 @@ class WeatherDataset:
         data = data.to_numpy()
         data = remove_missing_values(data)
         data = torch.from_numpy(data)
-        # data.shape
+        # data.shape = (time, width, height)
         data = data.flatten()
         return data
 

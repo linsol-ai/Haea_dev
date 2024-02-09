3ff99eebde4d2b9a20c290f4430be8b964b67d02
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:36:30 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -150,7 +150,7 @@ class WeatherDataset:
             for val in self.HAS_LEVEL_VARIABLE:
                 result[val] = {}
                 for level in levels:
-                    key = executor.submit(self.load_level_val, val, level, start_date, end_date, val not in wind_keys)
+                    key = executor.submit(self.load_level_val, val, level)
                     futures[key] = (val, level)
 
 

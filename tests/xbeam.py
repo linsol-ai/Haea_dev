4b28babd70aefd2ee89baf8db7b9cb3ae517da69
wooj1nBot <1403jung@gmail.com>
2024-02-09 23:41:56 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -52,7 +52,7 @@ def main(argv):
   START_DATE = f'{FLAGS.start}-12-31'
   END_DATE = f'{FLAGS.end}-12-31'
   print('Preprocess Data: ', START_DATE, 'to', END_DATE)
-  OUTPUT_PATH = f'gs://era5_preprocess/240x121/{START_DATE}_{END_DATE}.zarr'
+  OUTPUT_PATH = f'gs://era5_preprocess/{}/{START_DATE}_{END_DATE}.zarr'
 
   source_dataset, source_chunks = xbeam.open_zarr(INPUT_PATHS[FLAGS.type])
   source_dataset = source_dataset[VARIABLE]

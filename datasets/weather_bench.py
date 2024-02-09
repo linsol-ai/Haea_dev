0d8e71c2e4c0a402b0fe896194825fe9cfc46e9c
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:19:36 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -110,7 +110,7 @@ class WeatherDataset:
         self.end = pd.to_datetime(f'{end}-01-01')
         self.datasets = [
             f'gs://era5_preprocess/1440x720/{start}-01-01_{end}-01-01.zarr',
-            f'gs://era5_preprocess/64x32/{start}-01-01_{end}-01-01.zarr',
+            f'gs://era5_preprocess/240x121/{start}-01-01_{end}-01-01.zarr',
             f'gs://era5_preprocess/64x32/{start}-01-01_{end}-01-01.zarr'
         ]
 

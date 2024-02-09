5b1365dfadcf7e16db82adbb0590532bc4e0d548
wooj1nBot <1403jung@gmail.com>
2024-02-09 17:18:51 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -5,11 +5,14 @@ import time
 import xarray_beam
 
 
+HAS_LEVEL_VARIABLE = [ 'geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
+
+    NONE_LEVEL_VARIABLE = ['2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'sea_surface_temperature', 'total_cloud_cover', 'total_precipitation_24hr', 'total_precipitation_6hr']
 
 # 파이프라인 실행
 if __name__ == '__main__':
 
     start = time.time()
     ds = xarray.open_zarr('gs://era5_preprocess/1440x720/2018-01-01_2023-01-01.zarr', chunks={'time':10})
-    
+
     print(ds)

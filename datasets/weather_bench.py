a71e495d75420c347c217b74bc127ab91ddf0005
wooj1nBot <1403jung@gmail.com>
2024-02-09 16:28:45 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -146,7 +146,8 @@ class WeatherDataset:
             result = self.load_data(dataset, wind_batch)
             var_dataset.append(result[0])
             wind_dataset.append(result[1])
-            
+        
+        
     
     def load_data(self, dataset:xr.Dataset, wind_batch=256) -> Tuple[torch.Tensor, torch.Tensor]:
         start = time.time()

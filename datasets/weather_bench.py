9bf948d1b8e8aa88a1c3ab25e69a033fc9a5d5a2
wooj1nBot <1403jung@gmail.com>
2024-02-09 13:53:09 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -109,7 +109,7 @@ class WeatherDataset:
     
     def load_bart(self, variables, start_date, end_date, lat, lon, wind_batch, device):
         wind_keys = ['u_component_of_wind', 'v_component_of_wind']
-        levels = ds.level.values
+        levels = self.ds.level.values
         result = {}
         start = time.time()
         with ThreadPoolExecutor() as executor:

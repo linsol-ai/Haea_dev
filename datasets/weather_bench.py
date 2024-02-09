babff6cebd4aeee14a2079cac9d1e4b54ff340bc
wooj1nBot <1403jung@gmail.com>
2024-02-09 16:41:01 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -147,7 +147,7 @@ class WeatherDataset:
             var_dataset.append(result[0])
             wind_dataset.append(result[1])
         
-        # var_dataset.shape = (time, var * level, w)
+        # var_dataset.shape = (time, var * level, w*h)
         var_dataset = torch.stack(var_dataset, dim=0)
 
     

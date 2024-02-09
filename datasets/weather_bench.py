542653ede2fffd7beab213d9cc9bdf7e52891752
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:21:23 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -105,12 +105,12 @@ class WeatherDataset:
     def __init__(self, start:int, end:int):
         self.start = pd.to_datetime(f'{start}-01-01')
         self.end = pd.to_datetime(f'{end}-01-01')
-        datasets = [
+        dataset_urls = [
             f'gs://era5_preprocess/1440x720/{start}-01-01_{end}-01-01.zarr',
             f'gs://era5_preprocess/240x121/{start}-01-01_{end}-01-01.zarr',
             f'gs://era5_preprocess/64x32/{start}-01-01_{end}-01-01.zarr'
         ]
-        self.load_dataset(datasets)
+        self.load_dataset(dataset_urls)
 
     
 

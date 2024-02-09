5022346b875ba09e6a1e75bb81c4d0ab78c1b09a
wooj1nBot <1403jung@gmail.com>
2024-02-09 23:21:38 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -51,7 +51,7 @@ def main(argv):
   end_date = pd.to_datetime(END_DATE)
   source_dataset = source_dataset.sel(time=slice(start_date, end_date))
 
-  lat_min, lat_max = 20, 
+  lat_min, lat_max = 20, 70
   lon_min, lon_max = 124.2, 131
 
   # 해당 범위에 속하는 위도와 경도의 인덱스 찾기

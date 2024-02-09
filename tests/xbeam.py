00968c5d9759661f0bdcc758144585b0608dadfc
wooj1nBot <1403jung@gmail.com>
2024-02-09 21:03:07 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -77,7 +77,7 @@ def main(argv):
   with beam.Pipeline(options=pipeline_options) as root :
     (
         root
-        | xbeam.DatasetToChunks(source_dataset, source_chunks
+        | xbeam.DatasetToChunks(source_dataset, source_chunks)
         | beam.MapTuple(rekey_chunk_on_month_hour, lat_indices=lat_indices, lon_indices=lon_indices)
         | xbeam.ConsolidateChunks({'time': 108})
         | xbeam.ChunksToZarr(OUTPUT_PATH, template, {'time': 108})

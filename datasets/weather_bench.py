e19de41f7d4578b3a3f0c83182c19637a99f9d51
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:17:35 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -107,7 +107,7 @@ class WeatherDataset:
 
     def __init__(self, start:int, end:int):
         self.start = pd.to_datetime(f'{start}-01-01')
-        self.end = end
+        self.end = pd.to_datetime(f'{start}-01-01')
 
     
 

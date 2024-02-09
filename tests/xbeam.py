506b59cd727eb9c001cdace8df9324e75a611e8a
wooj1nBot <1403jung@gmail.com>
2024-02-09 22:29:38 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -71,7 +71,6 @@ def main(argv):
         project='genfit-7ba0d',
         temp_location='gs://era5_preprocess/temp',
         requirements_file='/workspace/Haea/req.txt',
-        region='us-central1',
         worker_zone='us-central1-f',
         machine_type='n2-standard-4'
   )

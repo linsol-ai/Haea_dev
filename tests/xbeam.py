8a291c9e0eed11852f76b5c97edaf033759fb273
wooj1nBot <1403jung@gmail.com>
2024-02-09 19:29:43 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -63,7 +63,7 @@ def main(argv):
       .isel(latitude=lat_indices, longitude=lon_indices)
   )
 
-  output_chunks = {'time': 100}
+  output_chunks = {'time': 108}
 
   pipeline_options = PipelineOptions(
         runner='DataflowRunner',

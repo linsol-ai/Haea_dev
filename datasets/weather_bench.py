63d4e4dc25f299ee3f7003565a44e40aa3142f99
wooj1nBot <1403jung@gmail.com>
2024-02-09 15:34:10 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -127,9 +127,9 @@ class WeatherDataset:
 
 
     def load_variable(self, key, level=None):
-        arr = self.ds[key]
+        data = self.ds[key]
         if level:
-            data = arr.sel(level=level)
+            data = data.sel(level=level)
         data = data.to_numpy()
         data = remove_missing_values(data)
         data = torch.from_numpy(data)

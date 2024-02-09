2aac3f1bd59bce509c920f88699faa53292e2761
wooj1nBot <1403jung@gmail.com>
2024-02-09 16:17:06 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -194,6 +194,7 @@ class WeatherDataset:
         for val in (self.HAS_LEVEL_VARIABLE + self.NONE_LEVEL_VARIABLE):
             if val in (self.HAS_LEVEL_WIND_VAR + self.NONE_LEVEL_WIND_VAR):
                 continue
+            if 
             for level in levels:
                 data = result[val][level]
                 dataset.append(data) 

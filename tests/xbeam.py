7cebde650cfb3c13bd49e1d50ad15e2628e2eb1c
wooj1nBot <1403jung@gmail.com>
2024-02-09 20:55:47 +0000

    Auto commit

--- tests/xbeam.py
+++ tests/xbeam.py
@@ -79,7 +79,6 @@ def main(argv):
         root
         | xbeam.DatasetToChunks(source_dataset, source_chunks)
         | beam.MapTuple(rekey_chunk_on_month_hour, lat_indices=lat_indices, lon_indices=lon_indices)
-        | xbeam.SplitChunks(output_chunks)
         | xbeam.ChunksToZarr(OUTPUT_PATH, template, output_chunks)
     )
 

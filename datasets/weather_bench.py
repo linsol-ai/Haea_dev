1b453f7e49640e4e413e94d940b718028f6239bf
wooj1nBot <1403jung@gmail.com>
2024-02-10 06:44:32 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -208,7 +208,7 @@ class WeatherDataset:
 
             data = result[val]
             if len(data.shape) == 3:
-                 for lv in data.
+                 for lv in data.size(0)
 
 
         dataset = torch.stack(dataset, dim=0)

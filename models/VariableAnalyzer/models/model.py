1b318bf4c5dfb74f0746d50732af3ec436ebd064
wooj1nBot <1403jung@gmail.com>
2024-02-10 22:48:40 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -40,8 +40,6 @@ class SourceEmbedding(nn.Module):
         """
         super().__init__()
         self.variable = VariableEmbedding(var_len, embed_size)
-        self.position = PositionalEmbedding(time_len, embed_size)
-        self.dropout = nn.Dropout(p=dropout)
         self.embed_size = embed_size
 
     def forward(self, tgt, position_seq, variable_seq):

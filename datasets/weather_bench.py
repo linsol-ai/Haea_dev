e0caa0841aa1d3c8781a87c19803598b7f0c101c
wooj1nBot <1403jung@gmail.com>
2024-02-10 07:53:34 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -188,11 +188,11 @@ class WeatherDataset:
         with ThreadPoolExecutor() as executor:
             futures = {}
             v1 = result[self.HAS_LEVEL_WIND_VAR]
-            k1 = executor.submit(self.calculate_wind, v1[0], v1[1], wind_batch, self.device)
+            k1 = executor.submit(self.calculate_wind, v1[0], v1[1], self.device)
             futures[k1] = 1
 
             v2 = result[self.NONE_LEVEL_WIND_VAR]
-            k2 = executor.submit(self.calculate_wind, v2[0], v2[1], wind_batch, self.device)
+            k2 = executor.submit(self.calculate_wind, v2[0], v2[1], self.device)
             futures[k2] = 0
 
             for future in tqdm(as_completed(futures), desc="Processing futures"):

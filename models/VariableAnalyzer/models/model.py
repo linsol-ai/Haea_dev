109b1ddcba3bb0ce22e0dc0bc7c9eb44da25ab59
wooj1nBot <1403jung@gmail.com>
2024-02-10 22:49:45 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -67,7 +67,7 @@ class VariableAnalyzer(nn.Module):
             batch_first=True
         )
         self.tgt_embedding = TargetEmbedding(var_len, time_len, dim_model, dropout)
-        self.src_embedding = VariableEmbedding(var_len, dim_model)
+        self.src_embedding = SourceEmbedding(var_len, dim_model)
         self.out = nn.Linear(dim_model, dim_model)
 
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):

1ea4c646158763e29414c065e68a06948e1c79cf
wooj1nBot <1403jung@gmail.com>
2024-02-10 21:26:38 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -25,7 +25,7 @@ def _main() -> None:
     try:
         with open(config_path) as f:
             config_dict = yaml.safe_load(f)
-            config: TrainingRunConfig = TrainingRunConfig.parse_obj(config_dict)
+            config: TrainingRunConfig = .parse_obj(config_dict)
     except FileNotFoundError:
         logging.error(f"Config file {config_path} does not exist. Exiting.")
     except yaml.YAMLError:

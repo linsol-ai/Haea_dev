5d61e34ff91b75d881be4ac0b530b37ac2ef3ca1
wooj1nBot <1403jung@gmail.com>
2024-02-10 07:43:38 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -142,7 +142,7 @@ class WeatherDataset:
         # data.shape = (time, width * height)
 
         if len(data.shape) == 4:
-            data = data.reshape(data.shape[:2])
+            data = data.reshape(data.shape[:2], -1)
         else:
             data = data.reshape(1)
 

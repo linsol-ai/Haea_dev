274364ae84c907743e642450a3e986dadf97aca3
wooj1nBot <1403jung@gmail.com>
2024-02-10 18:43:34 +0000

    Auto commit

--- models/VariableAnalyzer/training/configs.py
+++ models/VariableAnalyzer/training/configs.py
@@ -6,7 +6,6 @@ class LinearSchedulerConfig(BaseModel):
     """The final value of the parameter."""
 
     warmup: float = Field(ge=0, le=1, default=0.0)
-    """Fraction of the total training steps for the warmup phase."""
 
     warmup_step: int = Field(ge=0, le=1, default=0.0)
 

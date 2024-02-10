3dc1df2e9e10567a0d4430b11ead42d5274be3e4
wooj1nBot <1403jung@gmail.com>
2024-02-10 21:43:15 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -30,7 +30,7 @@ def get_dataset(year_offset: int, time_len: int):
     weather = WeatherDataset(year_offset, device=device)
     # dataset.shape:  torch.Size([7309, 100, 1450])
     original = weather.load()
-    dataset = CustomDataset(original, )
+    dataset = CustomDataset(original, time_len)
 
         
 

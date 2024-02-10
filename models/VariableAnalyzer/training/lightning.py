4ebb86b470aaa18d0672e06bd85ced70b60aebff
wooj1nBot <1403jung@gmail.com>
2024-02-10 20:29:59 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -5,7 +5,7 @@ from einops import rearrange
 from models.model import VariableAnalyzer
 from training.configs import TrainingConfig
 from training.params_schedule import CosineWarmupScheduler
-from typing import List
+from typing import List, T
 
 class TrainModule(pl.LightningModule):
     """A PyTorch Lightning training module for the `DVAE`."""

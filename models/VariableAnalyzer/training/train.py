a20f49aeab5f2a5b7350f47aacc9c73757f72ea7
wooj1nBot <1403jung@gmail.com>
2024-02-10 21:54:14 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -50,8 +50,8 @@ def _main() -> None:
     else:
         pl.seed_everything(config.seed)
 
-
         
+
         logger = WandbLogger(save_dir=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'tb_logs'), name="my_model")
         model = VariableAnalyzer(
             

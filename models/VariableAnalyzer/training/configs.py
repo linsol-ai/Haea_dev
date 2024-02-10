afc07c0d70e8f5399dea4244417c201659e151b7
wooj1nBot <1403jung@gmail.com>
2024-02-10 18:55:41 +0000

    Auto commit

--- models/VariableAnalyzer/training/configs.py
+++ models/VariableAnalyzer/training/configs.py
@@ -20,7 +20,7 @@ class DVAETrainingConfig(BaseModel):
 
     warmup_step: int = Field(ge=1, default=4000)
 
-    weight_decay: float = 
+    weight_decay: float = Field(ge=0, default=0.1)
 
     batch_size: int = Field(ge=0, default=256)
     """"The batch size."""

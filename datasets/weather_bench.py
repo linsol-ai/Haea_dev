13d7e9899ef3ad49accd6937b8914ed8ddd15cb6
wooj1nBot <1403jung@gmail.com>
2024-02-10 08:18:16 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -200,7 +200,7 @@ class WeatherDataset:
                 # shape => (3, time, h * w) or (level, 3, time, h * w)
                 data = future.result()
                 if len(data.shape) == 4:
-                    data = data.view(-1, data.size())
+                    data = data.view(-1, data.size(2), data.size(3))
                 wind_result[level] = data
 
             del result[self.HAS_LEVEL_WIND_VAR[0]]

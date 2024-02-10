20d4e15364169c8c97cc51e79a111a198a00fec3
wooj1nBot <1403jung@gmail.com>
2024-02-10 17:15:21 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -237,6 +237,7 @@ class WeatherDataset:
         has_nan = torch.isnan(dataset).any()
         if has_nan:
             print("dataset has nan")
+            
         has_nan = torch.isnan(dataset).any()
         if has_nan:
             print("dataset has nan")

f31c7da2dc687f1999b8f6dce457358d18d2ac50
wooj1nBot <1403jung@gmail.com>
2024-02-10 06:09:59 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -182,7 +182,7 @@ class WeatherDataset:
         with ThreadPoolExecutor() as executor:
             futures = {}
             v1 = result[self.HAS_LEVEL_WIND_VAR]
-            key = executor.submit(self.calculate_wind, u_wind, v_wind, wind_batch, self.device)
+            key = executor.submit(self.calculate_wind, v1[0], v_wind, wind_batch, self.device)
             futures[key] = 0
 
             

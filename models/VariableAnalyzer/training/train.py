3d159c0e24c1ab11b1e52f020fc2a017e312fc4f
wooj1nBot <1403jung@gmail.com>
2024-02-10 21:27:27 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -18,7 +18,6 @@ import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
 from datasets.weather_bench import WeatherDataset
 from models.VariableAnalyzer.models.model import VariableAnalyzer
-from training.callbacks import SaveValVisualizationCallback
 from training.config import TrainingRunConfig
 from training.lightning import DVAETrainModule
         

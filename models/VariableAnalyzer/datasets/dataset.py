de45b3e52b7f93b9c7d5d5bcf84a4ecbb4859d18
wooj1nBot <1403jung@gmail.com>
2024-02-10 22:27:12 +0000

    Auto commit

--- models/VariableAnalyzer/datasets/dataset.py
+++ models/VariableAnalyzer/datasets/dataset.py
@@ -25,7 +25,8 @@ class CustomDataset(Dataset):
                 dataset.append(self.dataset[t])
 
         # dataset.shape = (time_len, var, hidden)
-        dataset = 
+        dataset = torch.stack(dataset, dim=0)
+        
 
     def make_dataset(self):
         dataset = []

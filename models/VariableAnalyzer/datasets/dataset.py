dc6ce02ac0f1a0d0e6adc1bb5d0b0e3fc8e3db53
wooj1nBot <1403jung@gmail.com>
2024-02-10 22:13:52 +0000

    Auto commit

--- models/VariableAnalyzer/datasets/dataset.py
+++ models/VariableAnalyzer/datasets/dataset.py
@@ -11,6 +11,7 @@ class CustomDataset(Dataset):
         self.dataset = dataset
         self.var_len = dataset.size(1)
         self.time_len = time_len
+        self.
         self.make_dataset()
 
     def __len__(self):

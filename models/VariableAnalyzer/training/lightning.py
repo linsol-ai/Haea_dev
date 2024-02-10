954078ac1b8e19c3796de257268b00dfc9abb295
wooj1nBot <1403jung@gmail.com>
2024-02-10 20:09:19 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -30,10 +30,7 @@ class DVAETrainModule(pl.LightningModule):
 
     def configure_optimizers(self) -> tuple[list[AdamW], list[ExponentialLR]]:  # noqa: D102
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.config.learning_rate)
-        scheduler = ExponentialLR(
-            optimizer,
-            gamma= self.config.lr_decay_rate
-        )
+        scheduler = CosineWarmupScheduler
         return [optimizer], [scheduler]
 
     def _step(self, batch: torch.Tensor, mode: str) -> torch.Tensor:

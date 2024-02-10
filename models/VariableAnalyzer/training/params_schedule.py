c057b40210926d34a754da71c20045e97d93577b
wooj1nBot <1403jung@gmail.com>
2024-02-10 20:02:26 +0000

    Auto commit

--- models/VariableAnalyzer/training/params_schedule.py
+++ models/VariableAnalyzer/training/params_schedule.py
@@ -57,28 +57,3 @@ class TransformerScheduler(ParameterScheduler):
         return self._value
 
 
-class ExponentialScheduler(ParameterScheduler):
-    def __init__(
-        self, start: float, min: float, anneal_rate: float = 1e-6
-    ) -> None:
-        """Init the linear scheduler.
-
-        Args:
-            start: The start value of the parameter.
-            end: The end value of the parameter.
-            steps: The number of steps to take.
-            warmup: Fraction of steps to warm up. The scheduler will start
-                interpolating from `start` to `end` at `int(warmup * steps)` step.
-            cooldown: Fraction of steps to cool down. The scheduler will be
-                interpolating from `end` to `start` till `1 - int(cooldown * steps)` step.
-        """
-        super().__init__()
-        self._value = start
-        self._min = min
-        self._anneal_rate = anneal_rate
-
-    def step(self, global_steps) -> None:  # noqa: D102
-        self._value = max(self._value * math.exp(-self._anneal_rate * global_steps), self._min)
-
-    def get_value(self) -> float:  # noqa: D102
-        return self._value

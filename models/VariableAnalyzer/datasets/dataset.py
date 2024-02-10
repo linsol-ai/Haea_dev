094012ae947f572faefa14a0e905acbfe874f66f
wooj1nBot <1403jung@gmail.com>
2024-02-10 22:24:21 +0000

    Auto commit

--- models/VariableAnalyzer/datasets/dataset.py
+++ models/VariableAnalyzer/datasets/dataset.py
@@ -17,7 +17,7 @@ class CustomDataset(Dataset):
         return self.dataset.size(0)
 
     def get_data(self, indicate):
-        
+        datae
 
     def make_dataset(self):
         dataset = []

571174a1d75b583d2b0c1e2a1d67e9f3755464f8
wooj1nBot <1403jung@gmail.com>
2024-02-10 06:04:16 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -174,7 +174,7 @@ class WeatherDataset:
                     key = executor.submit(self.load_variable, dataset, val, level)
                     futures[key] = (val, level)
 
-            for val in (self.NONE_LEVEL_VARIABLE + self.HAS):
+            for val in (self.NONE_LEVEL_VARIABLE + self.HAS_LEVEL_VARIABLE):
                 key = executor.submit(self.load_variable, dataset, val)
                 futures[key] = (val, -1)
 

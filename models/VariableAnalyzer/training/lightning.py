453cc15554506c42ee80717eca97a586dfed5a19
wooj1nBot <1403jung@gmail.com>
2024-02-10 20:36:30 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -42,7 +42,7 @@ class TrainModule(pl.LightningModule):
         return self._step(batch, "train")
 
     def on_train_epoch_end(self) -> None:  # noqa: D102
-        self._kl_div_weight_scheduler.step()
+    
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], _: int) -> torch.Tensor:  # noqa: D102
         return self._step(batch, "val")

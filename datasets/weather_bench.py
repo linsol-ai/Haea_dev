5d47ca089473bafc55591113805ec79f8d91f2bf
wooj1nBot <1403jung@gmail.com>
2024-02-10 07:51:32 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -130,13 +130,6 @@ class WeatherDataset:
     def load_variable(self, dataset, key):
         data = dataset[key]
         data = data.to_numpy()
-        if len(data.shape) == 4:
-            removed = np.zeros_like(data)
-            for i in range(data.shape[0]):
-                removed[i] = remove_missing_values(data[i])
-            data = removed
-        else:
-            data = remove_missing_values(data)
 
         data = torch.from_numpy(data)
         # data.shape = (time, width, height)

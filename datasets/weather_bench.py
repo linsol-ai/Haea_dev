e17fcb4bfcd89343ae50956007ccff6e86ff016e
wooj1nBot <1403jung@gmail.com>
2024-02-10 06:38:19 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -242,7 +242,7 @@ class WeatherDataset:
 
         res = preprocess_wind_data(u_wind, v_wind, device).cpu()
 
-        return torch.concat(output, dim=1)
+        return torch.concat(res, dim=1)
 
 
 

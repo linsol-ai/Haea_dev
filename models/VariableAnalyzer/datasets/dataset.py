06fe3b483aca74c767e4994aaa39a8bb8101aaf3
wooj1nBot <1403jung@gmail.com>
2024-02-10 22:14:01 +0000

    Auto commit

--- models/VariableAnalyzer/datasets/dataset.py
+++ models/VariableAnalyzer/datasets/dataset.py
@@ -11,7 +11,7 @@ class CustomDataset(Dataset):
         self.dataset = dataset
         self.var_len = dataset.size(1)
         self.time_len = time_len
-        self.
+        self.padding_vec = to
         self.make_dataset()
 
     def __len__(self):

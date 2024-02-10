3af72e8863d6649febfda166b6b4f1a3fade8f03
wooj1nBot <1403jung@gmail.com>
2024-02-10 19:13:59 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -1,7 +1,8 @@
 import pytorch_lightning as pl
 import torch.optim.optimizer
 from torch.optim import AdamW
-from torch.optim.lr_scheduler import ExponentialLR, LinearLR, Cos
+from torch.optim.lr_scheduler import ExponentialLR, LinearLR
+fr
 from einops import rearrange
 import pytorch_lightning
 from models.dalle import DiscreteVAE

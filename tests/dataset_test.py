0d4f5acfffc6897c5ff7d8f807d43ea162ded215
wooj1nBot <1403jung@gmail.com>
2024-02-10 06:32:43 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -12,14 +12,7 @@ NONE_LEVEL_VARIABLE = ['2m_temperature', '10m_u_component_of_wind', '10m_v_compo
 # 파이프라인 실행
 if __name__ == '__main__':
 
-    start = time.time()
-    import xarray_beam as xbeam
-    import xarray
-    from apache_beam.options.pipeline_options import PipelineOptions
-    import apache_beam as beam
-    import pandas as pd
-    import numpy as np   
-
+    start = time.time() 
     ds = xarray.open_zarr('gs://era5_preprocess/1440x720/2016-12-31_2021-12-31.zarr')
     print(ds.nbytes / (10**9))
     print(ds)

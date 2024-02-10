18093c1e8a034a4e65b166f730d76e97d4d1c7e8
wooj1nBot <1403jung@gmail.com>
2024-02-10 00:36:38 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -195,8 +195,7 @@ class WeatherDataset:
                 key = executor.submit(self.calculate_wind, u_wind, v_wind, wind_batch, self.device)
                 futures[key] = level
             
-            u_wind, v_wind = result[self.NONE_LEVEL_WIND_VAR]
-            key = executor.submit(self.calculate_wind, u_wind, v_wind, wind_batch, self.device)
+            u_wind, v_wind = result[self.NONE_LEVEL_WIND_VAR] key = executor.submit(self.calculate_wind, u_wind, v_wind, wind_batch, self.device)
             futures[key] = -1
 
             for future in tqdm(as_completed(futures), desc="Processing futures"):

92361d2bde880cb722e9fe53b9fc4c16c62fd9fa
wooj1nBot <1403jung@gmail.com>
2024-02-10 07:44:53 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -197,7 +197,7 @@ class WeatherDataset:
         print(dataset)
 
         for val in (self.NONE_LEVEL_VARIABLE + self.HAS_LEVEL_VARIABLE):
-            result[val] = self.load_variable_chunk()
+            result[val] = self.load_variable_chunk(data)
 
         with ThreadPoolExecutor() as executor:
             futures = {}

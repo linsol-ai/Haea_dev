f0fbc6133e83e43ebae11777d60488a5f716e90a
wooj1nBot <1403jung@gmail.com>
2024-02-10 22:07:06 +0000

    Auto commit

--- models/VariableAnalyzer/datasets/dataset.py
+++ models/VariableAnalyzer/datasets/dataset.py
@@ -19,7 +19,7 @@ class CustomDataset(Dataset):
     def make_dataset(self):
         dataset = []
         for t in range(self.dataset.size(0)):
-            src = [(t, v) for v in range(self.var_len)]
+            src = {}
             tgt = []
             for t_n in range(t, min(self.dataset.size(0), t + self.time_len)):
                 tgt.extend(

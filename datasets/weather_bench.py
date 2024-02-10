184ecf164b2103f07a6cee26cea65c0e7e2292f0
wooj1nBot <1403jung@gmail.com>
2024-02-10 06:08:47 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -183,7 +183,7 @@ class WeatherDataset:
             futures = {}
             u_wind, v_wind = result[self.HAS_LEVEL_WIND_VAR]
             key = executor.submit(self.calculate_wind, u_wind, v_wind, wind_batch, self.device)
-                futures[key] = level
+            futures[key] = level
             for level in levels:
                 u_wind, v_wind = result[self.HAS_LEVEL_WIND_VAR][level]
                 key = executor.submit(self.calculate_wind, u_wind, v_wind, wind_batch, self.device)

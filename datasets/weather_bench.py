34ffe37d7031ee2ece38d266112cf3b129880285
wooj1nBot <1403jung@gmail.com>
2024-02-10 06:15:34 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -173,7 +173,7 @@ class WeatherDataset:
 
             for future in tqdm(as_completed(futures), desc="Processing futures"):
                 val = futures[future]
-                # shape => (level, time, h * w) or (time, h * w)
+                # shape => (time, h * w) or (time, h * w)
                 data = future.result()
                 result[val] = data
 

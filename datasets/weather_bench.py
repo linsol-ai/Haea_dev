efdf14bc15deb23971a21559e9f762ae0669384b
wooj1nBot <1403jung@gmail.com>
2024-02-10 07:56:51 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -203,8 +203,7 @@ class WeatherDataset:
                     data = data.swapaxes(0, 1)
                 wind_result[level] = data
 
-            del result[self.HAS_LEVEL_WIND_VAR[0]]
-            del result[self.HAS_LEVEL_WIND_VAR[1]]
+            del result[self.HAS_LEVEL_WIND_VAR[0]] del result[self.HAS_LEVEL_WIND_VAR[1]]
             del result[self.NONE_LEVEL_WIND_VAR[0]]
             del result[self.NONE_LEVEL_WIND_VAR[1]]
 

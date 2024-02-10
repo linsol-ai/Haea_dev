eb7db61e79fa09a817a19cab47043f9ecbe4bb9b
wooj1nBot <1403jung@gmail.com>
2024-02-10 21:33:56 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -198,7 +198,7 @@ class WeatherDataset:
         
         end = time.time()
         print(f"{end - start:.5f} sec")
-        return dataset, wind_dataset
+        return torch.stack(wind_dataset, dim=0)dataset, wind_dataset
 
 
     def calculate_wind(self, u_wind, v_wind, device):

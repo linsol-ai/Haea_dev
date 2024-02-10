fc97139c655e140437ee34eb12ea45baf02c6bd9
wooj1nBot <1403jung@gmail.com>
2024-02-10 18:10:33 +0000

    Auto commit

--- models/VariableAnalyzer/training/configs.py
+++ models/VariableAnalyzer/training/configs.py
@@ -19,19 +19,6 @@ class DVAETrainingConfig(BaseModel):
     """The learning rate."""
     lr_decay_rate: float = 0.98
 
-    """The minimum factor value for the learning rate scheduler."""
-    kl_div_weight_scheduler: LinearSchedulerConfig = Field(
-        default_factory=lambda: LinearSchedulerConfig(start=0, end=1e-4, warmup=0.1, cooldown=0.2)
-    )
-    """A config specification of the KL Divergence weight scheduler."""
-
-    temperature_scheduler: ExponentialSchedulerConfig = Field(
-        default_factory=lambda: ExponentialSchedulerConfig(
-            start=1., min=0.5, anneal_rate=1e-6 
-        )
-    )
-    """A config specification of the temperature scheduler."""
-
     batch_size: int = Field(ge=0, default=256)
     """"The batch size."""
 

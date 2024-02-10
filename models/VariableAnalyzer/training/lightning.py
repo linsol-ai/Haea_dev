9c644793c00f1c8b055e4458f3114575c781ded9
wooj1nBot <1403jung@gmail.com>
2024-02-10 20:15:03 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -31,7 +31,6 @@ class TrainModule(pl.LightningModule):
         return [optimizer], [scheduler]
 
 
-
     def _step(self, batch: torch.Tensor, mode: str) -> torch.Tensor:
         temperature = self._temperature_scheduler.get_value()
         kl_div_weight = self._kl_div_weight_scheduler.get_value()

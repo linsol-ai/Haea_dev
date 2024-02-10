cc65266c4d9c0f87f08e6c7e90487f47c881a0ac
wooj1nBot <1403jung@gmail.com>
2024-02-10 17:19:03 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -176,7 +176,8 @@ class WeatherDataset:
                 val = futures[future]
                 # shape => (level, time, h * w) or (time, h * w)
                 data = future.result()
-                
+                has_nan = torch.isnan(dataset).any()
+                    
                 if len(data.shape) == 3:
                     data = data.swapaxes(0, 1)
                 result[val] = data

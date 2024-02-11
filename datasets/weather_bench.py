79dd25f17bc36373840fa1ca8b235533c5b5da13
wooj1nBot <1403jung@gmail.com>
2024-02-11 19:52:30 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -190,7 +190,7 @@ class WeatherDataset:
             nan_indices = torch.isnan(data)
             data[nan_indices] = 0
 
-        unnormalized = d
+        unnormalized = data.
         normalized = normalize_tensor(data)
 
         if normalize:

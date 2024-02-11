64a3bccdf8ce83e03c0e745ab7fd2b058c4260fa
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:24:20 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -78,6 +78,7 @@ def preprocess_wind_data(u, v, device, normalize):
 
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
+    def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
     # The ID of your GCS bucket
     # bucket_name = "your-bucket-name"
@@ -87,6 +88,23 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
 
     # The path to which the file should be downloaded
     # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, destination_file_name
+        )
+    )
 
     storage_client = storage.Client()
 

e14249e31546b9ccdabe9ed9a835bde55d217d7a
wooj1nBot <1403jung@gmail.com>
2024-02-11 19:59:40 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -76,6 +76,8 @@ def preprocess_wind_data(u, v, device):
         sin_encoded = sin_encoded.cpu()
         cos_encoded = cos_encoded.cpu()
 
+        
+
         wind_speed = normalize_tensor(wind_speed)
         sin_encoded = normalize_tensor(sin_encoded)
         cos_encoded = normalize_tensor(cos_encoded)

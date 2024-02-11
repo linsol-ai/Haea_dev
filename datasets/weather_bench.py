db4814b934b29a9a8e52b11c74cda5e7d3c3722d
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:27:36 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -125,7 +125,7 @@ class WeatherDataset:
         self.normalize = normalize
         dataset_path = self.check_dataset(self.RESOLUTION, start, end)
         
-        self.load_dataset(dataset_urls)
+        self.load_dataset(dataset_path)
 
     
     def check_dataset(self, resolutions, start, end):

a5169e1436ff5687d805b01252f0af5732c81ebb
wooj1nBot <1403jung@gmail.com>
2024-02-11 13:29:04 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -72,7 +72,7 @@ def _main() -> None:
             n_decoder_layers=config.model_config.n_decoder_layers,
             dropout=config.model_config.dropout
         )
-        model_pl = TrainModule(model=model, max_iters=config.training.max_epochs, config=config.training)
+        model_pl = TrainModule(model=model, max_iters=config.training.max_epochs*, config=config.training)
         summary = ModelSummary(model_pl, max_depth=-1)
         print(summary)
 

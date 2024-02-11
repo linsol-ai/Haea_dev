e044b02bac365da6ff104072a623def586bc19f9
wooj1nBot <1403jung@gmail.com>
2024-02-11 10:36:16 +0000

    Auto commit

--- models/VariableAnalyzer/datasets/dataset.py
+++ models/VariableAnalyzer/datasets/dataset.py
@@ -32,7 +32,7 @@ class CustomDataset(Dataset):
 
     def make_dataset(self):
         dataset_inc = []
-        for t in range(self.dataset.size(0)-self.time_len-1):
+        for t in range(self.dataset.size(0)-self.time_len):
             src = [t]
             tgt = range(t+1, t+1 + self.time_len)
             dataset_inc.append((src, tgt))

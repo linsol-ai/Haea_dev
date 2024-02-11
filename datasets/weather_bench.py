ab225ff35a7d49e1a5ed971a1c1d4bdcbbee91bd
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:25:40 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -141,7 +141,7 @@ class WeatherDataset:
             dataset_path.append(file_path)
 
             if not file_path.is_file():
-                download_blob(self.GCS_BUCKET, resol / file_name, )
+                download_blob(self.GCS_BUCKET, resol / file_name, folder / resol)
 
             if download_list:
                 download_many_blobs_with_transfer_manager(self.GCS_BUCKET, )

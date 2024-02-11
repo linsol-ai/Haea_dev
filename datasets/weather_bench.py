a50487d8e57cf07cbab6d7bc48f7c1cb04e7c80b
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:46:50 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -137,6 +137,7 @@ class WeatherDataset:
             dataset_path.append(file_path)
 
             if not file_path.is_file():
+                gcs_path = 
                 zarr = xr.open_zarr()
                 zarr
 

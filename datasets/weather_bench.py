3d5d25e96ec69f7e7daeb8d39d9c298a11f39fa4
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:04:56 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -90,7 +90,7 @@ class WeatherDataset:
 
     GCS_ADDESS = 'gs://era5_preprocess'
 
-    DIR_NAME = 'r'
+    DIR_NAME = 'reso'
 
 
     def __init__(self, year_offset:int, device:torch.device, normalize=True):

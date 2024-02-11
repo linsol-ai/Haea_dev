6382770dd07fce8c971703a45bb52492080d408f
wooj1nBot <1403jung@gmail.com>
2024-02-11 13:42:26 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -17,7 +17,7 @@ from pytorch_lightning.utilities.model_summary import ModelSummary
 import sys,os
 current_directory = os.getcwd()
 print(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(current_directory))))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset
 from models.VariableAnalyzer.datasets.dataset import CustomDataset
 from models.VariableAnalyzer.models.model import VariableAnalyzer

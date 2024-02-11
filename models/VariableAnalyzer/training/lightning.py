90ce6507d059c94a0044d8361d79d895629d4a7e
wooj1nBot <1403jung@gmail.com>
2024-02-11 12:44:35 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -27,7 +27,7 @@ class TrainModule(pl.LightningModule):
     def _step(self, batch: torch.Tensor, mode: str) -> torch.Tensor:
         src = batch[:self.var_len]
         tgt = batch[self.var_len:]
-        
+        output = self.model(src, tgt)
         loss = F.mse_loss(tgt_b, output)
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")
         return loss

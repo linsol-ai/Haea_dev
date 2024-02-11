29fa72aba779e394bdbc2375bd966e3934744ef4
wooj1nBot <1403jung@gmail.com>
2024-02-11 18:52:05 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -20,7 +20,7 @@ class TrainModule(pl.LightningModule):
 
 
     def configure_optimizers(self) -> tuple[list[AdamW], list[CosineWarmupScheduler]]:  # noqa: D102
-        print()
+        print("lr: ", )
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.config.learning_rate)
         scheduler = CosineWarmupScheduler(optimizer=optimizer, warmup=self.config.warmup_step, max_iters=self.max_iters)
         return [optimizer]

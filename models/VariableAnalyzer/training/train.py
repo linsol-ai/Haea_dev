82e9919bae17232e2a52452b8de2a680838e94f7
wooj1nBot <1403jung@gmail.com>
2024-02-11 12:30:49 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -67,7 +67,7 @@ def _main() -> None:
             var_len=shape[1],
             time_len=time_len,
             dim_model=shape[2],
-            num_heads=config.model_config
+            num_heads=config.model_config.num
 
         )
         model_pl = DVAETrainModule(dvae=model, config=config.training)

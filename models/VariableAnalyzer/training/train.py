bec378011638fccd01ccbe75dc1778bbef07bc92
wooj1nBot <1403jung@gmail.com>
2024-02-11 14:33:25 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -62,7 +62,7 @@ def _main(args) -> None:
         # shape = (time, var, hidden)
         dataset, shape, dims = get_dataset(train_offset, time_len)
 
-        print("s")
+        print("DATASET ")
 
         logger = WandbLogger(save_dir=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'tb_logs'), name="my_model")
         model = VariableAnalyzer(

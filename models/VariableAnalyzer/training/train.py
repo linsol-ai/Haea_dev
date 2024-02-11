5be4cd73a19ab8d60337c60a1f423d2fa01b1509
wooj1nBot <1403jung@gmail.com>
2024-02-11 13:56:09 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -25,7 +25,7 @@ from models.VariableAnalyzer.training.lightning import TrainModule
 FLAGS = flags.FLAGS
 YEAR_OFFSET = flags.DEFINE_multi_integer('train_offset', None, help='training year')
 TIME_LENGTH = flags.DEFINE_multi_integer('time_len', None, help='TIME_LENGTH')
-flags.mark_flag_as_required("train_year")
+flags.mark_flag_as_required("train_offset")
 flags.mark_flag_as_required("time_len")
 
 

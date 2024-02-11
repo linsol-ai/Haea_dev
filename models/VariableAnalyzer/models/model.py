abe31e48060c0f99d0bffc4fcf9f82af27aca738
wooj1nBot <1403jung@gmail.com>
2024-02-11 22:06:02 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -71,6 +71,9 @@ class VariableAnalyzer(nn.Module):
         self.tgt_mask = self.get_tgt_mask()
         self.src_var_seq, self.tgt_var_seq = self.get_var_seq(batch_size, device)
         self.time_seq = self.get_time_seq(batch_size, device)
+    
+
+    def init
 
 
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):

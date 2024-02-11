b2899014756c72c3eca179e97047106c0c5883cf
wooj1nBot <1403jung@gmail.com>
2024-02-11 16:58:19 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -79,9 +79,6 @@ def preprocess_wind_data(u, v, device, normalize):
         return torch.stack([wind_speed, sin_encoded, cos_encoded], dim=0)
 
 
-
-
-# Custom DoFn to update progress bar
 class ProgressUpdater(beam.DoFn):
 
     def __init__(self):

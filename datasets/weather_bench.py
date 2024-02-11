9ab743baf5ceb0cb2b92c553b257d6811ab49ae4
wooj1nBot <1403jung@gmail.com>
2024-02-11 16:50:24 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -81,6 +81,10 @@ def preprocess_wind_data(u, v, device, normalize):
 
 def download_zarr(source):
     source_dataset, source_chunks = xb.open_zarr(source)
+    template = (
+      xbeam.make_template(source_dataset)
+      .isel(latitude=lat_indices, longitude=lon_indices)
+  )
     with beam.Pipeline() as root :
         (
             root

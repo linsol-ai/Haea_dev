8c6a37f65c8624b59a96abae29d074afbd1fcdb2
wooj1nBot <1403jung@gmail.com>
2024-02-11 20:07:31 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -287,7 +287,7 @@ class WeatherDataset:
                     unnormalized_dataset.append(unnormalized[i])
             else:
                 normalized_dataset.append(normalized)
-                unnormalized_dataset.append()
+                unnormalized_dataset.append(un)
 
         normalized_dataset = torch.stack(normalized_dataset, dim=0)
         # dataset.shape => (time, var, h * w)

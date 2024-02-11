ed7a2d59d0ea5f152355dfc46cd8b6a684f74de0
wooj1nBot <1403jung@gmail.com>
2024-02-11 12:32:30 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -70,7 +70,7 @@ def _main() -> None:
             num_heads=config.model_config.num_heads,
             n_encoder_layers=config.model_config.n_encoder_layers,
             n_decoder_layers=config.model_config.n_decoder_layers,
-            dr
+            dropout=
 
         )
         model_pl = DVAETrainModule(dvae=model, config=config.training)

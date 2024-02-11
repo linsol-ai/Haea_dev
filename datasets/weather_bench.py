a6915a0ce0a8218ae905a43408adf7db1e144b4b
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:30:12 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -82,7 +82,7 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
 
     storage_client = storage.Client()
 
-    print()
+    print(so)
 
     bucket = storage_client.bucket(bucket_name)
 

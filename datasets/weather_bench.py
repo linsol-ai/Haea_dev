7c8e8997b8728d4d5aa69fe0f9543c27c5ff19f7
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:59:30 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -141,6 +141,7 @@ class WeatherDataset:
                 gcs_path = self.GCS_BUCKET + "/" + resol + "/" + file_name
                 ds = xr.open_zarr(gcs_path)
                 ds.to_zarr(file_path, mode='w', consolidated=True, compute=False)
+                write_job = write_job.persist()
                 distributed.progress(write_job, notebook=False)
                 ds.close()
                 

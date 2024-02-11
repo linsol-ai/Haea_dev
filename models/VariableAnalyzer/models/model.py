b3e792c9bd9aa19b7fea055ba2bf636c17ed8afd
wooj1nBot <1403jung@gmail.com>
2024-02-11 14:52:03 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -97,7 +97,7 @@ class VariableAnalyzer(nn.Module):
         
             time_seq.append(seq)
         
-        return torch.tensor(time_seq, device=src.de)
+        return torch.tensor(time_seq, device=src.device)
     
 
     def get_tgt_mask(self) -> torch.tensor:

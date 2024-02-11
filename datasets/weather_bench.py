a0ccb365edd29abf41b3425304e0612ee45da67d
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:37:14 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -86,7 +86,7 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     print(destination_file_name)
 
     bucket = storage_client.bucket(bucket_name)
-    era5_preprocess/1440x720/2011-12-31_2016-12-31.zarr
+    
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(destination_file_name)
 

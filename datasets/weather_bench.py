288c88b4ec56a2ee2c4e586525e9dcbb65ae6a42
wooj1nBot <1403jung@gmail.com>
2024-02-11 09:56:25 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -13,7 +13,7 @@ import torch
 def min_max_scaling(tensor):
     # 각 차원별 최소값과 최대값 계산
     min_vals = np.min(tensor, axis=(2, 3), keepdims=True)
-    max_vals = np.max(tensor, axis=(2, 1), keepdims=True)
+    max_vals = np.max(tensor, axis=(2, 3), keepdims=True)
     
     # 최대-최소 스케일링을 적용하여 정규화
     scaled_tensor = (tensor - min_vals) / (max_vals - min_vals)

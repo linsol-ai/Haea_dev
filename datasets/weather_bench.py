c743ab1da272f9e9537ff912b34c32b82a9c7964
wooj1nBot <1403jung@gmail.com>
2024-02-11 20:11:14 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -275,7 +275,7 @@ class WeatherDataset:
 
         # dataset.shape => (var*level, time, h * w)
         input_dataset = []
-        targetdataset = []
+        target_dataset = []
 
         for val in (self.HAS_LEVEL_VARIABLE + self.NONE_LEVEL_VARIABLE):
             if val in (self.HAS_LEVEL_WIND_VAR + self.NONE_LEVEL_WIND_VAR):

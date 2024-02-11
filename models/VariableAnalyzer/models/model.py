08dc09bdef7f6d9a8621003185baa41b531bc803
wooj1nBot <1403jung@gmail.com>
2024-02-11 14:17:57 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -82,6 +82,8 @@ class VariableAnalyzer(nn.Module):
 
         transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=None, tgt_key_padding_mask=None)
         out = self.out(transformer_out)
+
+        
         return out
 
 

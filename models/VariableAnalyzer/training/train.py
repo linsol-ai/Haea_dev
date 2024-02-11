4d9e98d8e4efdc93fbd0b88440b2d966fc13c989
wooj1nBot <1403jung@gmail.com>
2024-02-11 12:32:13 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -69,7 +69,7 @@ def _main() -> None:
             dim_model=shape[2],
             num_heads=config.model_config.num_heads,
             n_encoder_layers=config.model_config.n_encoder_layers,
-            nd
+            n_decoder_layers=
 
         )
         model_pl = DVAETrainModule(dvae=model, config=config.training)

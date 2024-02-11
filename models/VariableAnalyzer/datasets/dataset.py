8509539a7326083a2cfab6d797134fe9ead60ae1
wooj1nBot <1403jung@gmail.com>
2024-02-11 10:23:37 +0000

    Auto commit

--- models/VariableAnalyzer/datasets/dataset.py
+++ models/VariableAnalyzer/datasets/dataset.py
@@ -45,7 +45,7 @@ class CustomDataset(Dataset):
     def __getitem__(self, item):
         src, tgt = self.dataset_inc[item]
         src = self.get_data(src)
-        tgt = 
+        tgt = self.get_data(tgt)
         res = torch.concat([self.get_data(src), self.get_data(tgt)], dim=0)
         return res
 

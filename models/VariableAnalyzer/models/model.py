ec4646cba637fa9dd4476e5d5511a5b89bcc0021
wooj1nBot <1403jung@gmail.com>
2024-02-11 11:32:11 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -90,7 +90,7 @@ class VariableAnalyzer(nn.Module):
         return torch.tensor([time_seq for _ in range(src.size(0))])
     
     def get_tgt_mask(self, src) -> torch.Tensor:
-        matrix = torch.zeros(src., src.size(1))
+        matrix = torch.zeros(, src.size(1))
         for i in range(0, src.size(1)):
             for j in range(0, min(self.var_len*(i+1), src.size(1))):
                 matrix[i, j] = 1

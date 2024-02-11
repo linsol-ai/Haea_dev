eb90ee6fca19d6e64b6c4217c582496452a6eb3a
wooj1nBot <1403jung@gmail.com>
2024-02-11 12:03:24 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -85,7 +85,7 @@ class VariableAnalyzer(nn.Module):
 
 
     def get_time_seq(self, src: torch.Tensor):
-        time_seq = torch.z
+        time_seq = torch.zeros()
         for i in range(self.time_len):
             seq = [i for _ in range(self.var_len)]
             time_seq.extend(seq)

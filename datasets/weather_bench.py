73976839cc16a3523c27feafec2edbbc946b698a
wooj1nBot <1403jung@gmail.com>
2024-02-11 17:29:17 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -86,7 +86,7 @@ def preprocess_wind_data(u, v, device, normalize):
         v = v.cpu()
         del u
         del v
-        del 
+        del wind_direction
 
         return torch.stack([wind_speed, sin_encoded, cos_encoded], dim=0)
 

99969a126c45bc89994abcf2a6a8b0cec4624d22
wooj1nBot <1403jung@gmail.com>
2024-02-11 22:11:33 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -1,6 +1,6 @@
 import pytorch_lightning as pl
 import torch.optim.optimizer
-import typing.
+
 from torch.optim import AdamW
 import torch.nn.functional as F
 from torch.optim.lr_scheduler import ExponentialLR

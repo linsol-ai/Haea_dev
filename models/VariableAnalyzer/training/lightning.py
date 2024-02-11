8db46873b0aea90485c1fca5f0f0b5066bc8d22a
wooj1nBot <1403jung@gmail.com>
2024-02-11 22:07:20 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -20,7 +20,7 @@ class TrainModule(pl.LightningModule):
 
 
     def on_test_start(self) -> None:
-        self.model.init_seq(se)
+        self.model.init_seq(self.device)
 
     def configure_optimizers(self) -> AdamW:  # noqa: D102
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.config.learning_rate)

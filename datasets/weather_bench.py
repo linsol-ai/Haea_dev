2f6794036a3a32e0f212a2887724841ef46b47ee
wooj1nBot <1403jung@gmail.com>
2024-02-11 17:26:07 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -292,6 +292,7 @@ class WeatherDataset:
 
     def calculate_wind(self, u_wind, v_wind, device):
         res = preprocess_wind_data(u_wind, v_wind, device, self.normalize)
+        
         return res
 
 

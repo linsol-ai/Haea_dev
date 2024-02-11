e17d2de937738137452479af49d12e51e34cba13
wooj1nBot <1403jung@gmail.com>
2024-02-11 18:19:44 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -90,7 +90,7 @@ class VariableAnalyzer(nn.Module):
     
 
     def get_var_seq(self, src: torch.Tensor, tgt: torch.Tensor):
-        src =
+        src_seq = torch.tensor([self.var_seq for _ in range(src.size(0))], device=src.device)
         time_seq = []
         for _ in range(src.size(0)):
             seq = []

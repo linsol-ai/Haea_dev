a03567821b4c2b59224c3f70d17fdc687feb203e
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:11:18 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -112,7 +112,7 @@ class WeatherDataset:
     
     def check_dataset(self, resolutions, start, end):
         file_name = f'{start}-12-31_{end}-12-31.zarr'
-        dataset_dir = 
+        dataset_path = []
         for resol in resolutions:
             folder = Path(self.DIR_NAME)
             file_path = folder / resol / file_name

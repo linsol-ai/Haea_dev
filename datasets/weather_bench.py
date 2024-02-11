5bc0a6329f88a4353a057fd3f3905ba89b62813b
wooj1nBot <1403jung@gmail.com>
2024-02-11 17:03:37 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -293,5 +293,5 @@ if __name__ == '__main__':
     device = ("cuda" if torch.cuda.is_available() else "cpu" )
     device = torch.device(device)
 
-    weather = WeatherDataset(1, device=device)
+    weather = WeatherDataset(0, device=device)
     weather.load()

68f45101bcfdc93c6faec04df2740e29fc3f5962
wooj1nBot <1403jung@gmail.com>
2024-02-11 16:51:12 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -136,7 +136,7 @@ class WeatherDataset:
                 gcs_path = self.GCS_BUCKET + "/" + resol + "/" + file_name
                 print("DOWNLOAD: ", gcs_path)
 
-                download_zarr(gcs_path, )
+                download_zarr(gcs_path, file_path)
         
         return dataset_path
     

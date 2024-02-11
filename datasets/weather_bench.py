db4f26de801a24a970b45de22e6a1b4a26a1890b
wooj1nBot <1403jung@gmail.com>
2024-02-11 15:10:13 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -115,7 +115,7 @@ class WeatherDataset:
         for resol in resolutions:
             folder = Path(self.DIR_NAME)
             file_path = folder / resol / file_name
-            if file_path.is_file
+            if file_path.is_file():
 
     
     def load_dataset(self, dataset_urls):

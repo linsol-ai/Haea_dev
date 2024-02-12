f4b5c3e4569dda93a5cde7b744cdbb98eb64ff9f
wooj1nBot <1403jung@gmail.com>
2024-02-12 10:05:31 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -215,7 +215,7 @@ class WeatherDataset:
             input_dataset.append(input)
             target_dataset.append(target)
             # shape = (var * level, 2)
-            print(min_max)
+            print(min_max.shape)
             min_max_data.append(min_max)
             dims.append(input.size(2))
         

29d09633bceb4cf2ce8fd294ae7bba5102b79c78
wooj1nBot <1403jung@gmail.com>
2024-02-12 15:44:10 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pytorch_lightning as pl
 import torch
-import torch
 from pytorch_lightning import Callback
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import Dataset

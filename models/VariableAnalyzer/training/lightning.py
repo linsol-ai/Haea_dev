5639630250a4f44d1847d5b403669b0687ce6c1b
wooj1nBot <1403jung@gmail.com>
2024-02-12 12:20:43 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -46,7 +46,7 @@ class TrainModule(pl.LightningModule):
         return loss
     
     def calculate_loss(self, predict, label):
-        # predict.shape = (2800, )
+        # predict.shape = (batch, 2)
 
 
 

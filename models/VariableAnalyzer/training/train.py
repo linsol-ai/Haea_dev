3d918bd6877b7d2bf7cd8198e1dc27ea78fe91d3
wooj1nBot <1403jung@gmail.com>
2024-02-12 17:25:51 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -105,11 +105,7 @@ def _main(args) -> None:
             callbacks=[
                 LearningRateMonitor(logging_interval="step"),
                 VariableVaildationCallback(
-                
                     
-                    log_every_n_step=config.training.log_every_n_step,
-                    dataset=train_ds,
-                    logger=logger,
                 ),
             ],
             precision="bf16"

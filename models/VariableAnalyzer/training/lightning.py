85c5af5d0ce24658c205eb474b7ce9d8098b0778
wooj1nBot <1403jung@gmail.com>
2024-02-12 17:52:34 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -57,7 +57,7 @@ class TrainModule(pl.LightningModule):
         return loss
 
 
-    def calculate_loss(self, predict: torch.Tensor, label: torch.Tensor, reduction: str | 'mean' ='):
+    def calculate_loss(self, predict: torch.Tensor, label: torch.Tensor, reduction: str | 'mean' = 'mean''):
         # predict.shape = (batch, time_len * var_len, 1450) -> not nomalized
         predict = predict.view(predict.size(0), -1, self.var_len, predict.size(2))
         # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized

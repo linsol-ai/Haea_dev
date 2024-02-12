396f69503cd5c06634a445719d10e635215d376f
wooj1nBot <1403jung@gmail.com>
2024-02-12 17:53:12 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -95,7 +95,7 @@ class VariableVaildationCallback(Callback):
         )
         predict = pl_module.model(src, tgt)
         loss = pl_module.calculate_loss(predict[:, :, :pl_module.predict_dim], label[:, :, :pl_module.predict_dim], reduction='none')
-        print(loss)
+        print(loss.shape)
         # loss.shape = (batch, time_len, var_len, 1450)
         loss = loss.view(loss.size(0), -1, loss.size(3))
         # loss.shape = (batch, var_len, time_len, 1450)

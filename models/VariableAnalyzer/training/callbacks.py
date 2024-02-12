fc2081ad1878ae98001cd20c628130d3dee35b4e
wooj1nBot <1403jung@gmail.com>
2024-02-12 15:45:11 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset
 from torchvision.utils import make_grid
 import wandb
 from typing import List
-from models.VariableAnalyzer.training.lightning import TrainingConfig
+from models.VariableAnalyzer.training.lightning import T
 
 
 class VariableVaildationCallback(Callback):

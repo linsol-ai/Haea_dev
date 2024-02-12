510ebf699c7b2d00048a779008f385bf7dffc6a8
wooj1nBot <1403jung@gmail.com>
2024-02-12 15:55:50 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -47,10 +47,10 @@ class VariableVaildationCallback(Callback):
         
     
     def validate_loss(self, pl_module: TrainModule):
-        val_data = torch.stack([self._dataset[i] for i in range(self.val_batch)], dim=0).to(
+        val_data = torch.stack([self._dataset[i][0] for i in range(self.val_batch)], dim=0).to(
             pl_module.device  # type: ignore[arg-type]
         )
-        
+
 
         return loss
 

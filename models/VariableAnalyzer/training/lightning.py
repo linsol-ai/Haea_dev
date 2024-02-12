f03f7328323bb4fad535e1b869fd8caf91efd84c
wooj1nBot <1403jung@gmail.com>
2024-02-12 16:17:04 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -63,7 +63,6 @@ class TrainModule(pl.LightningModule):
         # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized
         predict = predict.permute(0, 2, 1, 3)
         # predict.shape = (batch, var_len, time_len, 1450) -> not nomalized
-        
         min_max = self.min_max_data[0]
         reversed_predict = denormalize(predict, min_max)
         reversed_predict = reversed_predict.permute(0, 2, 1, 3)

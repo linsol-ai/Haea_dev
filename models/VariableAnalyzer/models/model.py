c7ac4a40bd11ad22ff7b0b6c8f4a5e28f6073fcc
wooj1nBot <1403jung@gmail.com>
2024-02-12 18:54:25 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -86,7 +86,7 @@ class VariableAnalyzer(nn.Module):
 
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):
         # src.shape = (batch, 1, 99, 1450), tgt.shape = (batch, time_len, 99, 1450)
-        src, tgt = src.squeeze(1), tgt.view(tgt.size(0), -1, t)
+        src, tgt = src.squeeze(1), tgt.view(tgt.size(0), -1, tgt.size())
         src = self.src_embedding(src, self.src_var_seq) * math.sqrt(self.dim_model)
         tgt = self.tgt_embedding(tgt, self.time_seq, self.tgt_var_seq) * math.sqrt(self.dim_model)
         tgt_mask = self.tgt_mask.to(src.device)

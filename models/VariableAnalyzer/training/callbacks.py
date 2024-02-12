52272fe0d90cbba584aa02e9516028c882a06d1a
wooj1nBot <1403jung@gmail.com>
2024-02-12 15:44:59 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset
 from torchvision.utils import make_grid
 import wandb
 from typing import List
-
+from models.VariableAnalyzer.training.configs import TrainingConfig
 
 class VariableVaildationCallback(Callback):
     """Callback to save visualizations of a dataset throughout training.

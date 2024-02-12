636321e073ac1a7e62d5cf3b0b21ad703420dbbf
wooj1nBot <1403jung@gmail.com>
2024-02-12 09:30:01 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -32,7 +32,7 @@ def normalize_tensor(tensor):
     # 원래 차원으로 되돌림
     scaled_tensor = scaled_tensor.view(shape)
     
-    return scaled_tensor
+    return scaled_tensor, min_val
 
 
 def calculate_wind_speed(u, v):

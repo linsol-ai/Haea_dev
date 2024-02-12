9bec9485eff171f01634689368515257dc06913f
wooj1nBot <1403jung@gmail.com>
2024-02-12 17:24:55 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -56,7 +56,7 @@ def _main(args) -> None:
         time_len = 4 * FLAGS.time_len
 
         # shape = (time, var, hidden)
-        weather_dataset, shape, min_max, dims = get_dataset(train_offset, time_len)
+        dataset_info, dataset, shape, min_max, dims = get_dataset(train_offset, time_len)
 
         print("DATASET SHAPE: " , shape)
 

7befd8c48b6e21fa926a8815ae4575eb0c2323e6
wooj1nBot <1403jung@gmail.com>
2024-02-12 16:56:12 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -31,6 +31,7 @@ class VariableVaildationCallback(Callback):
         """
         self.level_var = level_var
         self.non_level_var = non_level_var
+        self.
         self.val_batch = val_batch
         self.log_every_n_step = log_every_n_step
         self._dataset = dataset

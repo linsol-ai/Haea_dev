764831d740e0358c87e5f45cbe7aeca9b9676c0f
wooj1nBot <1403jung@gmail.com>
2024-02-12 17:42:52 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -83,7 +83,6 @@ class VariableAnalyzer(nn.Module):
         self.time_seq = self.get_time_seq(batch, device)
 
 
-
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):
         # src.shape = (batch, 99, 1450), tgt.shape = (batch, 99 * time_len, 1450)
         src = self.src_embedding(src, self.src_var_seq) * math.sqrt(self.dim_model)

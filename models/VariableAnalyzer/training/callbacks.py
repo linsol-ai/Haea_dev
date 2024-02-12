d7d8210618471be8be3f2a508a6b2ecbe3fd4a7b
wooj1nBot <1403jung@gmail.com>
2024-02-12 16:46:04 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -51,6 +51,7 @@ class VariableVaildationCallback(Callback):
             end = start + 13
             name = self.level_var[i]
             loss = level_loss[start:end]
+            
             self._logger.lo({"my_custom_id" : wandb.plot.line_series(
                        xs=[0, 1, 2, 3, 4], 
                        ys=[[10, 20, 30, 40, 50], [0.5, 11, 72, 3, 41]],

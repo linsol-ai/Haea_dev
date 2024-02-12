60df6ede17fa0d8c70b6cbbd6c1ac1090b7660df
wooj1nBot <1403jung@gmail.com>
2024-02-12 17:17:43 +0000

    Auto commit

--- models/VariableAnalyzer/training/configs.py
+++ models/VariableAnalyzer/training/configs.py
@@ -29,6 +29,8 @@ class TrainingConfig(BaseModel):
     gradient_clip_val: float | None = None
     """The value to clip the gradients to."""
 
+    log_every_n_step
+
     log_batch: int = Field(ge=0, default=256)
 
 

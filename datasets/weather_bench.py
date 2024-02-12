4eeac434b74866c818041b7da4923d0706c34142
wooj1nBot <1403jung@gmail.com>
2024-02-12 20:31:29 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -377,7 +377,7 @@ class WeatherDataset:
         input_dataset = torch.swapaxes(input_dataset, 0, 1)
         target_dataset = torch.swapaxes(target_dataset, 0, 1)
 
-        print(dataset['geopotential'])
+        print(dataset['geopotential'][])
         
         end = time.time()
         print(f"{end - start:.5f} sec")

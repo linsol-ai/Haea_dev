49404ef3336d4f47635114b1a971ae8679341329
wooj1nBot <1403jung@gmail.com>
2024-02-12 17:42:53 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -77,6 +77,7 @@ class VariableAnalyzer(nn.Module):
         self.src_var_seq, self.tgt_var_seq = self.get_var_seq(self.batch_size, device)
         self.time_seq = self.get_time_seq(self.batch_size, device)
 
+
     def change_seq(self, batch, device):
         self.tgt_mask = self.get_tgt_mask()
         self.src_var_seq, self.tgt_var_seq = self.get_var_seq(batch, device)

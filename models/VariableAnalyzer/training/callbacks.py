fdca9c5e316396975092966663d410d3cc63bd0b
wooj1nBot <1403jung@gmail.com>
2024-02-12 16:22:56 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -58,6 +58,7 @@ class VariableVaildationCallback(Callback):
         predict = pl_module.model(src, tgt)
         loss = pl_module.calculate_loss(predict[:, :, :self.predict_dim], label[:, :, :self.predict_dim])
 
+        #
         loss = loss.view(loss.size(0), -1, loss.size(3))
 
 

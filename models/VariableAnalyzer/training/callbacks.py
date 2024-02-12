c82ee3f83106c84eace287326e0e14e29039ebdb
wooj1nBot <1403jung@gmail.com>
2024-02-12 16:56:04 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -19,7 +19,7 @@ class VariableVaildationCallback(Callback):
     """
 
     def __init__(
-        self, level_var: List, non_level_var: List, levelval_batch: int, log_every_n_step: int, dataset: Dataset, logger: WandbLogger
+        self, level_var: List, non_level_var: List, level_info, val_batch: int, log_every_n_step: int, dataset: Dataset, logger: WandbLogger
     ) -> None:
         """Init the callback.
 

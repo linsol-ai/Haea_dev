9de44474a15d25d0dc5e9e499c47f82dd69b4f49
wooj1nBot <1403jung@gmail.com>
2024-02-12 16:36:13 +0000

    Auto commit

--- models/VariableAnalyzer/training/callbacks.py
+++ models/VariableAnalyzer/training/callbacks.py
@@ -71,7 +71,7 @@ class VariableVaildationCallback(Callback):
         loss = loss.view(loss.size(0), -1, loss.size(3))
         # loss.shape = (batch, var_len, time_len, 1450)
         loss = loss.permute(0, 2, 1, 3)
-        
+        # loss.shape = (batch, var_len, time_len, 1450)
         loss = torch.sum(loss, dim=3)
 
         level_loss = loss[:, :13 * len(self.level_var)]

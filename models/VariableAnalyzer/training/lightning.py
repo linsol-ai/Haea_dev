490850bdfb31baf9d7b359cf96147b0e031ada79
wooj1nBot <1403jung@gmail.com>
2024-02-13 04:02:52 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -62,7 +62,7 @@ class TrainModule(pl.LightningModule):
         label = batch[2]
         label = label.view(label.size(0), -1, label.size(3))
         predict = self.model(src, tgt)
-        loss = self.calculate_rmse_loss(predict[:, :-1, :self.predict_dim], label[:, :, :self.predict_dim])
+        loss = self.calculate_rmse_loss(predict[:, :, :self.predict_dim], label[:, :, :self.predict_dim])
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")
         return loss
 

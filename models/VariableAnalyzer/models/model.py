f94ad514d02101a541e9d70c64ec12e69e865093
wooj1nBot <1403jung@gmail.com>
2024-02-13 08:27:48 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -136,7 +136,8 @@ class VariableAnalyzer(nn.Module):
         Input arguments same as the forward pass.
         """
         x = x.squeeze(1)
-        if self.src_var_seq
+        if self.src_var_seq is None:
+            
         x = self.src_embedding(x, self.src_var_seq) * math.sqrt(self.dim_model)
 
         attention_maps = []

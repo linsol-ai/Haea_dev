b37d8812ebd5f024cfb7c9a47fa5143a7e57f0b2
wooj1nBot <1403jung@gmail.com>
2024-02-13 09:38:07 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -111,6 +111,8 @@ class VariableAnalyzer(nn.Module):
             src_seq = [], tgt_seq = []
             for i in range(0, self.src_time_len):
                 src_seq.extend([ i for _ in range(self.var_len)])
+                for i in range(0, self.src_time_len):
+                src_seq.extend([ i for _ in range(self.var_len)])
         
             src_time_seq.append(src_seq)
         

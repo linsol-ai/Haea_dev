979ad2ddc479376a9c2825ae661ecde63bf48671
wooj1nBot <1403jung@gmail.com>
2024-02-13 01:49:58 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -75,7 +75,7 @@ class TrainModule(pl.LightningModule):
         self.lr_scheduler.step()  # Step per iteration
     
 
-    def validation(self, ):
+    def validation(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]):
 
 
         

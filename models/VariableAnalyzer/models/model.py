11ead5f7eb1eedbdc277ac81138833c245ab9757
wooj1nBot <1403jung@gmail.com>
2024-02-13 09:22:23 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -68,7 +68,7 @@ class VariableAnalyzer(nn.Module):
             batch_first=True
         )
         
-        self.tgt_embedding = Embedding(var_len, time_len, dim_model, dropout)
+        self.tgt_embedding = Embedding(var_len, src_time_len, dim_model, dropout)
         self.src_embedding = Embedding(var_len, dim_model)
         self.out = nn.Linear(dim_model, predict_dim)
     

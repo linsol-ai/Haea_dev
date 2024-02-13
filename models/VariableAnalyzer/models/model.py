05ae174f5895ba21df846a3988d9af404ffe42d0
wooj1nBot <1403jung@gmail.com>
2024-02-13 09:37:49 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -108,7 +108,7 @@ class VariableAnalyzer(nn.Module):
     def get_time_seq(self, batch_size, device):
         src_time_seq = []
         for _ in range(batch_size):
-            src_seq = []
+            src_seq = [], tgt_seq = []
             for i in range(0, self.src_time_len):
                 src_seq.extend([ i for _ in range(self.var_len)])
         

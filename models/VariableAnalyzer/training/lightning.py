670db1a51fcea98477a32832ab542749353a59ee
wooj1nBot <1403jung@gmail.com>
2024-02-13 02:08:44 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -26,7 +26,7 @@ def rmse_loss(x, y):
 class TrainModule(pl.LightningModule):
 
     def __init__(self, *, model: VariableAnalyzer, mean_std: torch.Tensor, var_len: int, 
-                 predict_dim: int, max_iters: int, var_lv:List, var_nlv, levels, config: TrainingConfig | None = None):
+                 predict_dim: int, max_iters: int, var_lv: List, var_nlv, levels, config: TrainingConfig | None = None):
         
         super().__init__()
         self.var_len = var_len

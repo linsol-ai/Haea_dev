34614d67d1b9f7d0dd4f10eea21e6b669b38c951
wooj1nBot <1403jung@gmail.com>
2024-02-13 01:56:13 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -83,7 +83,7 @@ class TrainModule(pl.LightningModule):
         predict = predict.view(predict.size(0), -1, self.var_len, predict.size(2))
         mean_std = self.mean_std[0]
         reversed_predict = denormalize(predict, mean_std)
-        
+        reversed_predict = reversed_predict.view(reversed_predict.size(0), -1, reversed_predict.size(3))
 
 
         

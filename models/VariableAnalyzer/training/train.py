4f60ca12b04e7fed158f2aa3036c4fba1db49a77
wooj1nBot <1403jung@gmail.com>
2024-02-13 19:35:58 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -44,7 +44,7 @@ def _main(args) -> None:
     else:
         pl.seed_everything(config.seed)
 
-        train_offset = FLAGS.train_offset
+        train_offset = 
         time_len = 4 * FLAGS.time_len
 
         # shape = (time, var, hidden)

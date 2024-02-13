a8affb3e051f513c8e9eefc1d3b646522df188b5
wooj1nBot <1403jung@gmail.com>
2024-02-13 01:29:49 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -56,7 +56,7 @@ def main(argv):
   END_DATE = f'{FLAGS.end}-12-31'
   print('Preprocess Data: ', START_DATE, 'to', END_DATE)
   folder = Path(self.DIR_NAME)
-            file_path = folder / resol / file_name
+  file_path = folder / resol / file_name
   OUTPUT_PATH = f'gs://era5_preprocess/{FOLDER_NAME[FLAGS.type]}/{START_DATE}_{END_DATE}.zarr'
 
   source_dataset, source_chunks = xbeam.open_zarr(INPUT_PATHS[FLAGS.type])

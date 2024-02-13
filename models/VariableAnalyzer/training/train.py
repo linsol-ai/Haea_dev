dcb04edfc75b10aeccc6ac89523c67679cb0e149
wooj1nBot <1403jung@gmail.com>
2024-02-13 19:37:14 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -49,7 +49,7 @@ def _main(args) -> None:
         tgt_time_len = 4 * config.training.tgt_time_len
 
         # shape = (time, var, hidden)
-        dataset_info, dataset, shape, mean_std, dims = get_dataset(train_offset, src)
+        dataset_info, dataset, shape, mean_std, dims = get_dataset(train_offset, src_time_len, tg)
 
         print("DATASET SHAPE: " , shape)
 

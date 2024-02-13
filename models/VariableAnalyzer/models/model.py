b2602cc93bfea072398716bdd150e777361a41e1
wooj1nBot <1403jung@gmail.com>
2024-02-13 08:28:00 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -137,7 +137,8 @@ class VariableAnalyzer(nn.Module):
         """
         x = x.squeeze(1)
         if self.src_var_seq is None:
-            self.init_seq(x.dev)
+            self.init_seq(x.device)
+            
         x = self.src_embedding(x, self.src_var_seq) * math.sqrt(self.dim_model)
 
         attention_maps = []

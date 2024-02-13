757d902317e4a78c3914696a4692f66dd0d5d718
wooj1nBot <1403jung@gmail.com>
2024-02-13 03:27:39 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -113,7 +113,7 @@ class WeatherDataset:
 
     HAS_LEVEL_VARIABLE = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
 
-NONE_LEVEL_VARIABLE = [
+    NONE_LEVEL_VARIABLE = [
   'geopotential_at_surface', 'toa_incident_solar_radiation', '2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'sea_surface_temperature', 'total_cloud_cover', 'total_precipitation_6hr'
 ]
 

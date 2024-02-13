80580d7aca4a2e641b2dddcede57411cf35f6f21
wooj1nBot <1403jung@gmail.com>
2024-02-13 03:06:20 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -136,7 +136,7 @@ class TrainModule(pl.LightningModule):
         loss = torch.sum(loss, dim=-1) / hidden
         # loss.shape = (var_len, batch, time_len)
         loss = loss.permute(1, 0, 2)
-        
+        batch
         loss = torch.sum(loss, dim=1) / 
 
         level_loss = loss[:, :, :13 * len(self.var_lv)]

a5a1fbdd8f2704ebcd478726e90e3662dd867607
wooj1nBot <1403jung@gmail.com>
2024-02-13 19:34:20 +0000

    Auto commit

--- models/VariableAnalyzer/training/configs.py
+++ models/VariableAnalyzer/training/configs.py
@@ -29,7 +29,7 @@ class TrainingConfig(BaseModel):
     gradient_clip_val: float | None = None
     """The value to clip the gradients to."""
 
-    src_time_len: int = Field(ge=0, default=4000)
+    src_time_len: int = Field(ge=0, default=2)
 
 
 

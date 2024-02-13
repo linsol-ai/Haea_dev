63693a42da10c70b1afa367ea3b9faaebe91211e
wooj1nBot <1403jung@gmail.com>
2024-02-13 21:11:49 +0000

    Auto commit

--- models/VariableAnalyzer/models/model.py
+++ models/VariableAnalyzer/models/model.py
@@ -14,7 +14,7 @@ class PositionalEmbedding(nn.Embedding):
         super().__init__(time_len, embed_size)
 
 
-class TargetEmbedding(nn.Module):
+class Embedding(nn.Module):
     def __init__(self, var_len, time_len, embed_size, dropout=0.1):
         """
         :param vocab_size: total vocab size
@@ -27,35 +27,21 @@ class TargetEmbedding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
         self.embed_size = embed_size
 
-    def forward(self, tgt, position_seq, variable_seq):
-        x = tgt + self.position(position_seq) + self.variable(variable_seq)
-        return self.dropout(x)
-    
-class SourceEmbedding(nn.Module):
-    def __init__(self, var_len, embed_size, dropout=0.1):
-        """
-        :param vocab_size: total vocab size
-        :param embed_size: embedding size of token embedding
-        :param dropout: dropout rate
-        """ 
-        super().__init__()
-        self.variable = VariableEmbedding(var_len, embed_size)
-        self.dropout = nn.Dropout(p=dropout)
-        self.embed_size = embed_size
-
-    def forward(self, src, variable_seq):
-        x = src + self.variable(variable_seq)
+    def forward(self, x, position_seq, variable_seq):
+        x = x + self.position(position_seq) + self.variable(variable_seq)
         return self.dropout(x)
 
 
 class VariableAnalyzer(nn.Module):
-    def __init__(self, var_len, time_len, dim_model, predict_dim, batch_size, num_heads=12, n_encoder_layers=3, n_decoder_layers=3, dropout=0.1):
+    def __init__(self, var_len, src_time_len, tgt_time_len, dim_model, predict_dim, batch_size, max_len=4*10, num_heads=12, n_encoder_layers=3, n_decoder_layers=3, dropout=0.1):
         super().__init__()
         self.var_seq = range(var_len)
         self.var_len = var_len
-        self.time_len = time_len
+        self.src_time_len = src_time_len
+        self.tgt_time_len = tgt_time_len
         self.dim_model = dim_model
         self.batch_size = batch_size
+
         self.transformer = nn.Transformer(
             d_model=dim_model,
             nhead=num_heads,
@@ -66,8 +52,7 @@ class VariableAnalyzer(nn.Module):
             batch_first=True
         )
         
-        self.tgt_embedding = TargetEmbedding(var_len, time_len, dim_model, dropout)
-        self.src_embedding = SourceEmbedding(var_len, dim_model)
+        self.embedding = Embedding(var_len, max_len, dim_model, dropout)
         self.out = nn.Linear(dim_model, predict_dim)
     
 
@@ -75,20 +60,21 @@ class VariableAnalyzer(nn.Module):
         print("init", device)
         self.tgt_mask = self.get_tgt_mask()
         self.src_var_seq, self.tgt_var_seq = self.get_var_seq(self.batch_size, device)
-        self.time_seq = self.get_time_seq(self.batch_size, device)
+        self.src_time_seq, self.tgt_time_seq = self.get_time_seq(self.batch_size, device)
 
 
     def change_seq(self, batch, device):
         self.tgt_mask = self.get_tgt_mask()
         self.src_var_seq, self.tgt_var_seq = self.get_var_seq(batch, device)
-        self.time_seq = self.get_time_seq(batch, device)
+        self.src_time_seq, self.tgt_time_seq = self.get_time_seq(batch, device)
 
 
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):
-        # src.shape = (batch, 1, 99, 1450), tgt.shape = (batch, time_len, 99, 1450)
-        src, tgt = src.squeeze(1), tgt.view(tgt.size(0), -1, tgt.size(3))
-        src = self.src_embedding(src, self.src_var_seq) * math.sqrt(self.dim_model)
-        tgt = self.tgt_embedding(tgt, self.time_seq, self.tgt_var_seq) * math.sqrt(self.dim_model)
+        # src.shape = (batch, src_time_len, 99, 1450), tgt.shape = (batch, tgt_time_len, 99, 1450)
+        src, tgt = src.view(src.size(0), -1, src.size(3)), tgt.view(tgt.size(0), -1, tgt.size(3))
+
+        src = self.embedding(src, self.src_time_seq, self.src_var_seq) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt, self.tgt_time_seq, self.tgt_var_seq) * math.sqrt(self.dim_model)
         tgt_mask = self.tgt_mask.to(src.device)
 
         transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=None, tgt_key_padding_mask=None)
@@ -97,35 +83,74 @@ class VariableAnalyzer(nn.Module):
     
 
     def get_var_seq(self, batch_size, device):
-        src_seq = torch.tensor([self.var_seq for _ in range(batch_size)], device=device)
+        src_seq = []
         tgt_seq = []
+
         for _ in range(batch_size):
-            seq = []
-            for _ in range(0, self.time_len):
-                seq.extend(self.var_seq)
-            tgt_seq.append(seq)
+            t_seq = []
+            s_seq = []
+
+            for _ in range(0, self.tgt_time_len):
+                t_seq.extend(self.var_seq)
+
+            for _ in range(0, self.src_time_len):
+                s_seq.extend(self.var_seq)
+
+            tgt_seq.append(t_seq)
+            src_seq.append(s_seq)
         
         tgt_seq = torch.tensor(tgt_seq, device=device)
+        src_seq = torch.tensor(src_seq, device=device)
+
         return src_seq, tgt_seq
 
 
     def get_time_seq(self, batch_size, device):
-        time_seq = []
+        src_time_seq = []
+        tgt_time_seq = []
+
         for _ in range(batch_size):
-            seq = []
-            for i in range(0, self.time_len):
-                seq.extend([ i for _ in range(self.var_len)])
+            src_seq = []
+            tgt_seq = []
+
+            for i in range(0, self.src_time_len):
+                src_seq.extend([ i for _ in range(self.var_len)])
+
+            for i in range(0, self.tgt_time_len):
+                tgt_seq.extend([ i for _ in range(self.var_len)])
         
-            time_seq.append(seq)
+            src_time_seq.append(src_seq)
+            tgt_time_seq.append(tgt_seq)
         
-        return torch.tensor(time_seq, device=device)
+        return torch.tensor(src_time_seq, device=device), torch.tensor(tgt_time_seq, device=device)
     
 
     def get_tgt_mask(self) -> torch.tensor:
-        matrix = torch.zeros(self.var_len * self.time_len, self.var_len * self.time_len)
-        for i in range(0, self.var_len * self.time_len):
-            matrix[i, min(self.var_len*(i+1), self.var_len * self.time_len):] = float('-inf')
+        matrix = torch.zeros(self.var_len * self.tgt_time_len, self.var_len * self.tgt_time_len)
+        for i in range(0, self.var_len * self.tgt_time_len):
+            matrix[i, min(self.var_len*(i+1), self.var_len * self.tgt_time_len):] = float('-inf')
         return matrix
+
+    
+    @torch.no_grad()
+    def get_attention_maps(self, x: torch.Tensor):
+        """Function for extracting the attention matrices of the whole Transformer for a single batch.
+
+        Input arguments same as the forward pass.
+        """
+        x = x.view(x.size(0), -1, x.size(3))
+        if self.src_var_seq is None:
+            self.init_seq(x.device)
+
+        x = self.src_embedding(x, self.src_time_seq, self.src_var_seq) * math.sqrt(self.dim_model)
+
+        attention_maps = []
+        for layer in self.transformer.encoder.layers:
+            _, attn_map = layer.self_attn(query=x, key=x, value=x)
+            attention_maps.append(attn_map)
+            x = layer(x)
+
+        return attention_maps
 
     
 

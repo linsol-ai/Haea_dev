0987c7fdecb3167d3be252f391bbfd229db0c922
wooj1nBot <1403jung@gmail.com>
2024-02-13 04:40:52 +0000

    Auto commit

--- models/VariableAnalyzer/training/lightning.py
+++ models/VariableAnalyzer/training/lightning.py
@@ -140,7 +140,7 @@ class TrainModule(pl.LightningModule):
         loss = torch.sum(loss, dim=1) / n_batch
         loss = torch.sqrt(loss)
 
-        level_loss = loss[:, :len(self.levels) * len(self.var_lv)]
+        level_loss = loss[:len(self.levels) * len(self.var_lv)]
         non_level_loss = loss[:, len(self.levels) * len(self.var_nlv):]
 
         self.visualization_level(level_loss)

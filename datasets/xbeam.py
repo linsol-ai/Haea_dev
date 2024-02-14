d19b9ef7aaa1891c084674f36155de3386e12135
wooj1nBot <1403jung@gmail.com>
2024-02-14 16:31:22 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -78,7 +78,7 @@ def main(argv):
 
   pipeline_options = PipelineOptions(
         runner='DataflowRunner',
-        project='	climate-414222',
+        project='climate-414222',
         temp_location='gs://era5_climate/temp',
         requirements_file='/workspace/Haea/req.txt',
         region='us-central1',

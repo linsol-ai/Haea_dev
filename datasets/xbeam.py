b8c3cfb9dc83a8d8d44d0e48c57d15012b6377b8
wooj1nBot <1403jung@gmail.com>
2024-02-14 22:37:18 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -74,7 +74,7 @@ def main(argv):
   )
 
   output_chunks = source_chunks.copy()
-  output_chunks['time'] = 256
+  output_chunks['time'] = 128
 
   pipeline_options = PipelineOptions(
         runner='DataflowRunner',

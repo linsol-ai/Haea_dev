d88bd90e88c52ec9cde6a9a7897fcf39544c424b
Wooj1nBot <1403jung@gmail.com>
2024-02-15 18:12:34 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -140,7 +140,7 @@ class WeatherDataset:
                 file_path = folder / resol / file_name
                 dataset_path.append(file_path)
             else:
-                 
+                 dataset_path.append(file_path)
 
             if not file_path.exists():
                 print("======= DOWNLOAD Zarr FROM GCS ======")

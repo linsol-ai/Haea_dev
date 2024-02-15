2b28ef939e5cc09fd984c2eac1a6d115142f558f
Wooj1nBot <1403jung@gmail.com>
2024-02-15 18:12:21 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -137,7 +137,6 @@ class WeatherDataset:
         for resol in resolutions:
             folder = Path(self.DIR_NAME)
             file_path = folder / resol / file_name
-            
             dataset_path.append(file_path)
 
             if not file_path.exists():

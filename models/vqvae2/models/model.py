af723338cc017360b29e4cf7580cdfc87572553a
Wooj1nBot <1403jung@gmail.com>
2024-02-15 21:37:49 +0000

    Auto commit

--- models/vqvae2/models/model.py
+++ models/vqvae2/models/model.py
@@ -222,7 +222,7 @@ class VQVAE(nn.Module):
 
         return quant, diff_t + diff_b, id_t, id_b
 
-    def decode(self, quant_t, quant_b):
+    def decode(self, quant):
         upsample_t = self.upsample_t(quant_t)
         quant = torch.cat([upsample_t, quant_b], 1)
         dec = self.dec(quant)

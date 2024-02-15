ae74d1507ccb2cf6b2dafd9bb4943c5dcf734212
Wooj1nBot <1403jung@gmail.com>
2024-02-15 21:40:37 +0000

    Auto commit

--- models/vqvae2/distributed/launch.py
+++ models/vqvae2/distributed/launch.py
@@ -4,7 +4,7 @@ import torch
 from torch import distributed as dist
 from torch import multiprocessing as mp
 
-import distributed as dist_fn
+import models.distributed as dist_fn
 
 
 def find_free_port():

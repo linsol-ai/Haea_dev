a4e8c63f20f4e14e73c5e2c0199a352286d1b6ef
Wooj1nBot <1403jung@gmail.com>
2024-02-15 21:41:57 +0000

    Auto commit

--- models/vqvae2/distributed/launch.py
+++ models/vqvae2/distributed/launch.py
@@ -3,7 +3,7 @@ import os
 import torch
 from torch import distributed as dist
 from torch import multiprocessing as mp
-from models.vqvae2 import distributed as 
+from models.vqvae2 import distributed as dist_fn
 import models.vqvae2.vdistributed as dist_fn
 
 

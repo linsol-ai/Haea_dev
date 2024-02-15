b81bfc59057c4382cad4207f8cdc9906f4553ffe
Wooj1nBot <1403jung@gmail.com>
2024-02-15 21:37:33 +0000

    Auto commit

--- models/vqvae2/models/model.py
+++ models/vqvae2/models/model.py
@@ -217,6 +217,8 @@ class VQVAE(nn.Module):
         quant_b = quant_b.permute(0, 3, 1, 2)
         diff_b = diff_b.unsqueeze(0)
 
+        
+
         return quant_t, quant_b, diff_t + diff_b, id_t, id_b
 
     def decode(self, quant_t, quant_b):

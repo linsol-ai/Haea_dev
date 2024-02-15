32511205dc396eaf2f46a48fd297218ef5d4c41f
Wooj1nBot <1403jung@gmail.com>
2024-02-15 21:38:10 +0000

    Auto commit

--- models/vqvae2/models/model.py
+++ models/vqvae2/models/model.py
@@ -196,7 +196,7 @@ class VQVAE(nn.Module):
 
     def forward(self, input):
         quant, diff, _, _ = self.encode(input)
-        dec = self.decode(quant_t, quant_b)
+        dec = self.decode(quant)
 
         return dec, diff
 

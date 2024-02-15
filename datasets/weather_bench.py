4e2d11f4abd8057eb46a7b6bdb5514bcda7e4d59
Wooj1nBot <1403jung@gmail.com>
2024-02-15 22:57:57 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -91,7 +91,7 @@ def download_zarr(source, output_path):
         (
             root
             | "Read from Source Dataset" >> xb.DatasetToChunks(source_dataset, source_chunks)
-            | "Write to Zarr" >> xb.ChunksToZarr(output_path, template, source_dataset)
+            | "Write to Zarr" >> xb.ChunksToZarr(output_path, template, source_chunks)
         )
         
 

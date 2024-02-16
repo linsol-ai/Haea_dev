ba37ea6c63afeafb19787e5397ab8a47fe400f65
Wooj1nBot <1403jung@gmail.com>
2024-02-16 02:58:46 +0000

    Auto commit

--- models/dVAE/training/lightning.py
+++ models/dVAE/training/lightning.py
@@ -13,7 +13,7 @@ import wandb
 class TrainModule(pl.LightningModule):
 
     def __init__(self, *, model: VariableAnalyzer, mean_std: torch.Tensor, var_len: int, 
-                 predict_dim: int, max_iters: int, var_lv: List, var_nlv: List, levels: List, 
+               predict_dim: int, max_iters: int, var_lv: List, var_nlv: List, levels: List, 
                  config: TrainingConfig | None = None):
         
         super().__init__()

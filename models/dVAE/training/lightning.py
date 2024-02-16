064aea25b12759e83e154404a680714e411ed9d3
Wooj1nBot <1403jung@gmail.com>
2024-02-16 02:50:42 +0000

    Auto commit

--- models/dVAE/training/lightning.py
+++ models/dVAE/training/lightning.py
@@ -9,17 +9,6 @@ from models.VariableAnalyzer.training.params_schedule import CosineWarmupSchedul
 from typing import List
 import wandb
 
-def denormalize(inputs, mean_std):
-    # min_max 텐서를 적절히 재구성하여 inputs의 차원에 맞춤
-    mean = mean_std[:, 0].unsqueeze(0).unsqueeze(0).unsqueeze(-1)  # (1, 1, var_len, 1)
-    std = mean_std[:, 1].unsqueeze(0).unsqueeze(0).unsqueeze(-1)  # (1, 1, var_len, 1)
-    # 역정규화 수행
-    denormalized = (inputs * std) + mean
-    return denormalized
-
-def rmse_loss(x, y):
-    return torch.sqrt(F.mse_loss(x, y))
-
 
 class TrainModule(pl.LightningModule):
 

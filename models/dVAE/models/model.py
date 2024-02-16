db2abf56587f7516f532f31ffa6cf22e24d4c8bf
Wooj1nBot <1403jung@gmail.com>
2024-02-16 02:26:06 +0000

    Auto commit

--- models/dVAE/models/model.py
+++ models/dVAE/models/model.py
@@ -5,6 +5,11 @@ from torch import nn, einsum
 from torch.nn import functional as F
 from einops import rearrange
 
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
 
 
 class DiscreteVAE(nn.Module):

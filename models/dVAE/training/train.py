e6a2ed7e86aae105b0bb157ef24c4ae9a29f94de
Wooj1nBot <1403jung@gmail.com>
2024-02-16 05:37:14 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -64,9 +64,9 @@ def _main() -> None:
         model_pl = DVAETrainModule(dvae=model, config=config.training)
         summary = ModelSummary(model_pl, max_depth=-1)
         print(summary)
-        
+
         device = ("cuda" if torch.cuda.is_available() else "cpu" )
-    device = torch.device(device)
+         device = torch.device(device)
 
     weather = WeatherDataset(0, device=device, offline=True)
         input, _, _ = 

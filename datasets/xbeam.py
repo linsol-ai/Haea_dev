bbe51c4aef221f48cb26ff39079ec2533e79a0d7
Wooj1nBot <1403jung@gmail.com>
2024-02-16 20:54:21 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -96,7 +96,7 @@ def main(argv):
   with beam.Pipeline(options=pipeline_options) as root :
     (
         root
-        | xbeam.DatasetToChunks(source_dataset, source_chunks, split_vars=True)
+        | xbeam.DatasetToChunks(source_dataset, source_chunks)
         | beam.MapTuple(rekey_chunk_on_month_hour, level=LEVEL, lat_indices=lat_indices, lon_indices=lon_indices)
         | xbeam.ConsolidateChunks(output_chunks)
         | xbeam.ChunksToZarr(OUTPUT_PATH, template, output_chunks)

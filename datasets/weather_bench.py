700b7f18f4ae1324ff04a0590ec49d1ccd7c85c8
Wooj1nBot <1403jung@gmail.com>
2024-02-16 05:22:07 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -357,7 +357,7 @@ class WeatherDataset:
 
         for val in (self.HAS_LEVEL_VARIABLE + self.NONE_LEVEL_VARIABLE):
             input, target, mean, std = result[val]
-            
+            input_dataset[val]
             if len(input.shape) == 4:
                 for i in range(input.size(0)):
                     input_dataset.append(input[i])

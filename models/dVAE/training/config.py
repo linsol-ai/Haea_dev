1bd79f8bd979ed841119d5a0b1215f400b888458
Wooj1nBot <1403jung@gmail.com>
2024-02-16 05:40:47 +0000

    Auto commit

--- models/dVAE/training/config.py
+++ models/dVAE/training/config.py
@@ -54,7 +54,7 @@ class DVAEModelConfig(BaseModel):
 class DVAETrainingConfig(BaseModel):
     """A config specification of training the DVAE."""
 
-    train_valable = variable 
+    train_variable = variable 
 
     learning_rate: float = 5e-4
     """The learning rate."""

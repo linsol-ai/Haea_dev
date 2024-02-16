22d11dc438f488d6074c4a15873199f04e28ee7e
Wooj1nBot <1403jung@gmail.com>
2024-02-16 03:04:52 +0000

    Auto commit

---
+++ models/dVAE/training/config.py
@@ -0,0 +1,100 @@
+from pydantic import BaseModel, Field
+
+
+class LinearSchedulerConfig(BaseModel):
+    """A config specification of the parameter scheduler."""
+
+    start: float = 1.0
+    """The initial value of the parameter."""
+
+    end: float = 1.0
+    """The final value of the parameter."""
+
+    warmup: float = Field(ge=0, le=1, default=0.0)
+    """Fraction of the total training steps for the warmup phase."""
+
+    cooldown: float = Field(ge=0, le=1, default=0.0)
+    """Fraction of the total training steps for the cooldown phase."""
+
+
+class ExponentialSchedulerConfig(BaseModel):
+    """A config specification of the parameter scheduler."""
+
+    start: float = 1.0
+    """The initial value of the parameter."""
+
+    min: float = 0.5
+    """The min value of the parameter."""
+
+    anneal_rate: float = 1e-6
+    """The anneal_rate of the parameter."""
+
+class DVAEModelConfig(BaseModel):
+    """A config specification of the DVAE model."""
+
+    codebook_size: int = Field(ge=0, default=256)
+    """The number of vectors in the codebook."""
+
+    codebook_vector_dim: int = Field(ge=0, default=256)
+    """The dimension of the codebook vector."""
+
+    hidden_dim: int = Field(ge=0, default=256)
+    """The dimension of the hidden vector."""
+
+    channels: int = Field(ge=0)
+    """The number of channels in the input image."""
+
+    num_layers: int = 3
+    """The number of layers"""
+
+    num_resnet_blocks: int = 2
+    """The number of resnet blocks"""
+
+
+class DVAETrainingConfig(BaseModel):
+    """A config specification of training the DVAE."""
+
+    learning_rate: float = 5e-4
+    """The learning rate."""
+    lr_decay_rate: float = 0.98
+
+    """The minimum factor value for the learning rate scheduler."""
+    kl_div_weight_scheduler: LinearSchedulerConfig = Field(
+        default_factory=lambda: LinearSchedulerConfig(start=0, end=1e-4, warmup=0.1, cooldown=0.2)
+    )
+    """A config specification of the KL Divergence weight scheduler."""
+
+    temperature_scheduler: ExponentialSchedulerConfig = Field(
+        default_factory=lambda: ExponentialSchedulerConfig(
+            start=1., min=0.5, anneal_rate=1e-6 
+        )
+    )
+    """A config specification of the temperature scheduler."""
+
+    batch_size: int = Field(ge=0, default=256)
+    """"The batch size."""
+
+    max_epochs: int = Field(ge=0, default=100)
+    """The maximum number of training epochs."""
+
+    gradient_clip_val: float | None = None
+    """The value to clip the gradients to."""
+
+    save_vis_every_n_step: int = Field(ge=0, default=100)
+    """Save the validation visualizations every n epochs."""
+
+    num_vis: int = Field(ge=0, default=10)
+    """The number of validation visualizations to save every `save_vis_every_n_epochs` steps."""
+
+
+class TrainingRunConfig(BaseModel):
+    """A config specification of the training run."""
+
+    model: DVAEModelConfig
+    """A config specification of the model."""
+
+    training: DVAETrainingConfig
+    """A config specification of the training."""
+
+    seed: int = 123
+    """The random seed."""
--- models/dVAE/training/lightning.py
+++ models/dVAE/training/lightning.py
@@ -1,164 +1,81 @@
 import pytorch_lightning as pl
 import torch.optim.optimizer
-from typing import Tuple
-from torch.optim import Adam
-import torch.nn.functional as F
-from models.VariableAnalyzer.models.model import VariableAnalyzer
-from models.VariableAnalyzer.training.configs import TrainingConfig
-from models.VariableAnalyzer.training.params_schedule import CosineWarmupScheduler
-from typing import List
-import wandb
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import ExponentialLR
+from einops import rearrange
+import pytorch_lightning
+from models. import DiscreteVAE
+from training.config import DVAETrainingConfig
+from training.params_schedule import LinearScheduler, ExponentialScheduler
 
 
-class TrainModule(pl.LightningModule):
+class DVAETrainModule(pl.LightningModule):
+    """A PyTorch Lightning training module for the `DVAE`."""
 
-    def __init__(self, *, model: VariableAnalyzer, mean_std: torch.Tensor, var_len: int, 
-               predict_dim: int, max_iters: int, var_lv: List, var_nlv: List, levels: List, 
-                 config: TrainingConfig | None = None):
-        
+    def __init__(self, *, dvae: DiscreteVAE, config: DVAETrainingConfig | None = None):
+        """Init the DVAE training module.
+
+        Args:
+            dvae: An instance of the Discrete Variational Auto-Encoder to be used.
+            config: The DVAE's training configuration. If `None`, default configuration
+                will be used.
+        """
         super().__init__()
-        self.var_len = var_len
-        self.var_lv = var_lv
-        self.var_nlv = var_nlv
-        self.levels = levels
-        self.max_iters = max_iters
-        self.predict_dim = predict_dim
-        self.model = model
-        self.mean_std = mean_std
-        self.config = TrainingConfig() if config is None else config
-        self.save_hyperparameters()
-
-    
-    def setup(self, stage: str) -> None:
-        print(stage)
-        self.model.init_seq(self.device)
-        self.mean_std = self.mean_std.to(self.device)
-
-
-    def configure_optimizers(self) -> Adam:  # noqa: D102
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.learning_rate)
-        self.lr_scheduler = CosineWarmupScheduler(
-            optimizer, warmup=self.config.warmup_step, max_iters=self.max_iters
+        self.dvae = dvae
+        self.step = 0
+        self.config = DVAETrainingConfig() if config is None else config
+        self._temperature_scheduler = ExponentialScheduler(
+            start=self.config.temperature_scheduler.start,
+            min=self.config.temperature_scheduler.min,
+            anneal_rate=config.temperature_scheduler.anneal_rate
         )
-        return optimizer
+        self._kl_div_weight_scheduler = LinearScheduler(
+            start=self.config.kl_div_weight_scheduler.start,
+            end=self.config.kl_div_weight_scheduler.end,
+            warmup=self.config.kl_div_weight_scheduler.warmup,
+            cooldown=self.config.kl_div_weight_scheduler.cooldown,
+            steps=self.config.max_epochs,
+        )
+        self.save_hyperparameters(self.config.dict(), ignore=["dvae", "config"])
 
 
-    def _step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], mode: str) -> torch.Tensor:
-        src = batch[0]
-        tgt = batch[1]
-        label = batch[2]
-        label = label.view(label.size(0), -1, label.size(3))
-        predict = self.model(src, tgt)
-        loss = self.calculate_rmse_loss(predict[:, :, :self.predict_dim], label[:, :, :self.predict_dim])
-        self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")
-        return loss
+    def configure_optimizers(self) -> tuple[list[AdamW], list[ExponentialLR]]:  # noqa: D102
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.config.learning_rate)
+        scheduler = ExponentialLR(
+            optimizer,
+            gamma= self.config.lr_decay_rate
+        )
+        return [optimizer], [scheduler]
 
+    def _step(self, batch: torch.Tensor, mode: str) -> torch.Tensor:
+        temperature = self._temperature_scheduler.get_value()
+        kl_div_weight = self._kl_div_weight_scheduler.get_value()
 
-    def calculate_rmse_loss(self, predict: torch.Tensor, label: torch.Tensor):
-        # predict.shape = (batch, time_len * var_len, 1450) -> not nomalized
-        predict = predict.view(predict.size(0), -1, self.var_len, predict.size(2))
-        # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized
-        mean_std = self.mean_std[0]
-        reversed_predict = denormalize(predict, mean_std)
-        reversed_predict = reversed_predict.view(reversed_predict.size(0), -1, reversed_predict.size(3))
-        # reversed_predict.shape = (batch, time_len * var_len, 1450) -> nomalized
-        loss = rmse_loss(reversed_predict, label)
-        return loss
+        loss, recons = self.dvae(
+            batch,
+            return_loss = True,
+            return_recons = True,
+            temp = temperature
+        )
 
-    def optimizer_step(self, *args, **kwargs):
-        super().optimizer_step(*args, **kwargs)
-        self.lr_scheduler.step()  # Step per iteration
+        self._temperature_scheduler.step(self.step)
 
-
-    def visualization_level(self, level_loss: torch.Tensor):
-        for i in range(len(self.var_lv)):
-            start = i * len(self.levels)
-            end = start + len(self.levels)
-            name = self.var_lv[i]
-            loss = level_loss[start:end]
-
-            custom_plot = wandb.plot.line_series(
-                xs=range(loss.size(1)), 
-                ys=loss,
-                keys=self.levels,
-                title=name,
-                split_table=True,
-                xname="Time - 6Hour per"
-            )
-
-            self.logger.experiment.log({f"Atmospheric Loss/{name}": custom_plot})
-    
-
-    def visualization_non_level(self, non_level_loss):
-        for i in range(len(self.var_nlv)):
-            name = self.var_nlv[i]
-            loss = non_level_loss[i]
-
-            custom_plot = wandb.plot.line_series(
-                xs=range(loss.size(0)), 
-                ys=[loss],
-                title=name,
-                xname="Time - 6Hour per"
-            )
-
-            self.logger.experiment.log({f"Surface Loss/{name}": custom_plot})
-    
-
-    def validation(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]):
-        src = batch[0]
-        tgt = batch[1]
-        label = batch[2]
-        label = label.view(label.size(0), -1, label.size(3))
-        predict = self.model(src, tgt)
-        loss = self.calculate_sqare_loss(predict[:, :, :self.predict_dim], label[:, :, :self.predict_dim])
-        # loss.shape = (batch, time_len, var_len, 1450)
-        loss = loss.view(loss.size(0), -1, self.var_len, loss.size(2))
-        # loss.shape = (batch, var_len, time_len, 1450)
-        loss = loss.permute(0, 2, 1, 3)
-        hidden = loss.size(3)
-        # loss.shape = (batch, var_len, time_len)
-        loss = torch.sum(loss, dim=-1) / hidden
-        # loss.shape = (var_len, batch, time_len)
-        loss = loss.permute(1, 0, 2)
-        n_batch = loss.size(1)
-        # loss.shape = (var_len, time_len)
-        loss = torch.sum(loss, dim=1) / n_batch
-        loss = torch.sqrt(loss)
-
-        print(loss.shape)
-
-        level_loss = loss[:len(self.levels) * len(self.var_lv), :]
-        non_level_loss = loss[len(self.levels) * len(self.var_lv):, :]
-
-        self.visualization_level(level_loss)
-        self.visualization_non_level(non_level_loss)
-
-
+        self.log(f"{mode}/loss", loss, prog_bar=mode == "train")
+        self.log("temperature", self._temperature_scheduler.get_value())
+        self.log("kl_div_weight", self._kl_div_weight_scheduler.get_value())
         
-    
-
-    def calculate_sqare_loss(self, predict: torch.Tensor, label: torch.Tensor):
-        # predict.shape = (batch, time_len * var_len, 1450) -> not nomalized
-        predict = predict.view(predict.size(0), -1, self.var_len, predict.size(2))
-        # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized
-        mean_std = self.mean_std[0]
-        reversed_predict = denormalize(predict, mean_std)
-        reversed_predict = reversed_predict.view(reversed_predict.size(0), -1, reversed_predict.size(3))
-        # reversed_predict.shape = (batch, time_len * var_len, 1450) -> nomalized
-        loss = F.mse_loss(reversed_predict, label, reduction='none')
-        return loss
+        self.step += 1
         
+        return loss
 
-    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], _: int) -> torch.Tensor:  # noqa: D102
+    def training_step(self, batch: torch.Tensor, _: int) -> torch.Tensor:  # noqa: D102
         return self._step(batch, "train")
 
-    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:  # noqa: D102
-        if batch_idx == 0:
-            self.validation(batch)
-            return
+    def on_train_epoch_end(self) -> None:  # noqa: D102
+        self._kl_div_weight_scheduler.step()
+
+    def validation_step(self, batch: torch.Tensor, _: int) -> torch.Tensor:  # noqa: D102
         return self._step(batch, "val")
 
-    def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], _: int) -> None:  # noqa: D102
+    def test_step(self, batch: torch.Tensor, _: int) -> None:  # noqa: D102
         self._step(batch, "test")
-    
-

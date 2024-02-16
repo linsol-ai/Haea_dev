963132d1450f15f8e040b560efef944b9117d1d6
Wooj1nBot <1403jung@gmail.com>
2024-02-16 08:36:48 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -67,7 +67,7 @@ def _main() -> None:
         device = ("cuda" if torch.cuda.is_available() else "cpu" )
         device = torch.device(device)
         weather = WeatherDataset(0, device=device, offline=True)
-        input, _, _ = weather.load([])
+        input, _, _ = weather.load(variables=[])
 
         val_dataset = input[config.training.train_variable]
 

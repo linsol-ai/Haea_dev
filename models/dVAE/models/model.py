89746407a6863acb65c96d2a8bafc526695f3d9a
Wooj1nBot <1403jung@gmail.com>
2024-02-16 02:26:31 +0000

    Auto commit

--- models/dVAE/models/model.py
+++ models/dVAE/models/model.py
@@ -11,6 +11,14 @@ def exists(val):
 def default(val, d):
     return val if exists(val) else d
 
+def eval_decorator(fn):
+    def inner(model, *args, **kwargs):
+        was_training = model.training
+        model.eval()
+        out = fn(model, *args, **kwargs)
+        model.train(was_training)
+        return out
+    return inner
 
 class ResBlock(nn.Module):
     def __init__(self, chan):

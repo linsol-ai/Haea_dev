d1c44219f9e420fafdff141090bd1cc80906b145
Wooj1nBot <1403jung@gmail.com>
2024-02-16 08:38:20 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -17,7 +17,7 @@ from datasets.weather_bench import WeatherDataset
 from models.dVAE.training.callbacks import SaveValVisualizationCallback
 from models.dVAE.configs import TrainingRunConfig
 from models.dVAE.training.lightning import DVAETrainModule
-from models.model import DiscreteVAE
+from models.dVAE. import DiscreteVAE
 
 class ImageDataset(Dataset):
         def __init__(self, data_array: torch.Tensor):

4c4b2debce2869d9d7822b7b8d7fc716108a124f
Wooj1nBot <1403jung@gmail.com>
2024-02-16 03:42:19 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -187,6 +187,7 @@ class WeatherDataset:
                 inputs.append(input)
                 means.append(mean)
                 stds.append(std)
+            
 
         return input, target, mean, std
     

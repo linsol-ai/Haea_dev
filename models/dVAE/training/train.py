50cd7e43262d1b742f31caee4a85a4eba99024e8
Wooj1nBot <1403jung@gmail.com>
2024-02-16 13:12:13 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -59,7 +59,8 @@ def _main() -> None:
         logger = WandbLogger(
             save_dir=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), f'vqvae_logs/{config.training.train_variable}'), 
             name=config.training.train_variable,
-            project='vqvae'
+            project='vqvae',
+            log_model=False
             )
         model = DiscreteVAE(
             num_tokens=config.model.codebook_size,

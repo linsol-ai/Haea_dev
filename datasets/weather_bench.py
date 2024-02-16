c4f309ddda6f702897864a24f058e3c2380e976d
Wooj1nBot <1403jung@gmail.com>
2024-02-16 05:28:08 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -193,7 +193,6 @@ class WeatherDataset:
 
     def load(self):
         input, target, normalizaion = self.load_data(self.datasets)
-
         print("======= RESULT SHAPE =======")
         print("result_dataset.shape: ", input_dataset.shape)
 

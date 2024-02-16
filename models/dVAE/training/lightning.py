b42086984065d0639fd65fb1fa5ad76c6d6cb251
Wooj1nBot <1403jung@gmail.com>
2024-02-16 03:05:40 +0000

    Auto commit

--- models/dVAE/training/lightning.py
+++ models/dVAE/training/lightning.py
@@ -49,7 +49,6 @@ class DVAETrainModule(pl.LightningModule):
 
     def _step(self, batch: torch.Tensor, mode: str) -> torch.Tensor:
         temperature = self._temperature_scheduler.get_value()
-        kl_div_weight = self._kl_div_weight_scheduler.get_value()
 
         loss, recons = self.dvae(
             batch,

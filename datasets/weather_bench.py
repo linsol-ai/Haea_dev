bb0c9932a2706eccdf3e359a614721fba2aad676
Wooj1nBot <1403jung@gmail.com>
2024-02-16 03:41:58 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -186,7 +186,7 @@ class WeatherDataset:
             for i in range(data.size(1)):
                 input, mean, std = normalize_tensor(data[:, i, : , :])
                 inputs.append(input)
-                targets.append()
+                
 
         return input, target, mean, std
     

7d152ea6007c629422b5baffb7d56aa617697c72
Wooj1nBot <1403jung@gmail.com>
2024-02-16 12:32:41 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -50,7 +50,7 @@ def _main() -> None:
         pl.seed_everything(config.seed)
 
         log_path = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), f'vqvae_logs/{config.training.train_variable}')
-        if log_path.ex
+        if log_path.exists
 
         logger = WandbLogger(
             save_dir=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), f'vqvae_logs/{config.training.train_variable}'), 

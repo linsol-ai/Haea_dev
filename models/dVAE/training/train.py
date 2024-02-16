1c3aa42cfc527b54fa6fc57190642e131521b4a2
Wooj1nBot <1403jung@gmail.com>
2024-02-16 05:41:49 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -74,7 +74,7 @@ def _main() -> None:
 
         # Use a custom dataset class with proper transformations
 
-        dataset = ImageDataset(input)
+        dataset = ImageDataset(val_dataset)
         train_ds, val_ds = torch.utils.data.random_split(
             dataset,
             [0.7, 0.3],

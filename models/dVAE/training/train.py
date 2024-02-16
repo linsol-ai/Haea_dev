e1f87c667ec74ca7503646a1a7dba12ff2106b6c
Wooj1nBot <1403jung@gmail.com>
2024-02-16 05:37:26 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -68,7 +68,7 @@ def _main() -> None:
         device = ("cuda" if torch.cuda.is_available() else "cpu" )
         device = torch.device(device)
         weather = WeatherDataset(0, device=device, offline=True)
-        input, _, _ = wa
+        input, _, _ = weather.
 
         # Use a custom dataset class with proper transformations
 

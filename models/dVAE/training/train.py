cba7df6aa32b4d415397828e1d589293b17960bb
Wooj1nBot <1403jung@gmail.com>
2024-02-16 05:43:42 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -25,7 +25,7 @@ from datasets.weather_bench import WeatherDataset
 
 class ImageDataset(Dataset):
         def __init__(self, data_array: torch.Tensor):
-            
+            if 
             self.data_array = data_array
 
         def __len__(self):

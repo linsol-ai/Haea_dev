983cc3f0b3e00dc42e5df502195ecfc2a7f3bbe5
Wooj1nBot <1403jung@gmail.com>
2024-02-16 17:44:50 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -76,7 +76,7 @@ def main(argv):
       .isel(latitude=lat_indices, longitude=lon_indices)
   )
 
-  source_dataset = source_dataset.sel(time=slice(start_date, end_date)).sel()
+  source_dataset = source_dataset.sel(time=slice(start_date, end_date)).sel(level=LEVEL)
 
   output_chunks = source_chunks.copy()
   output_chunks['time'] = 256

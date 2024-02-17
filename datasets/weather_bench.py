d3e10d7e74135813ea8e6918da4488cbd2da471a
Wooj1nBot <1403jung@gmail.com>
2024-02-17 21:12:25 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -186,7 +186,7 @@ class WeatherDataset:
                 means.append(mean)
                 stds.append(std)
 
-            return torch.stack(inputs, dim=0).unsqueeze(2), target.swapaxes(0, 1).unsqueeze(2), means, stds
+            return torch.stack(inputs, dim=0).unsqueeze(2), target.swapaxes(0, 1).unsqueeze(2), torch.tensor(means, stds
 
         else:
             if lat_indices:

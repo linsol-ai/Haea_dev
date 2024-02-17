5fa70f5437f7adfd614eb5427a9cd0bee1108a02
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:32:47 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -35,7 +35,25 @@ class Embedding(nn.Module):
             return self.dropout(x)
 
 class LinearDecoder(nn.Module):
+def __init__(self, var_len, time_len, embed_size, dropout=0.1):
+        """
+        :param vocab_size: total vocab size
+        :param embed_size: embedding size of token embedding
+        :param dropout: dropout rate
+        """
+        super().__init__()
+        self.variable = VariableEmbedding(var_len, embed_size)
+        self.position = PositionalEmbedding(time_len, embed_size)
+        self.dropout = nn.Dropout(p=dropout)
+        self.embed_size = embed_size
 
+    def forward(self, x, variable_seq, position_seq=None):
+        if position_seq:
+            x = x + self.position(position_seq) + self.variable(variable_seq)
+            return self.dropout(x)
+        else:
+            x = x + self.variable(variable_seq)
+            return self.dropout(x)
 
 
 class VariableEncoder(nn.Module):

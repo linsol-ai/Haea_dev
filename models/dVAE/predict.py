625d400b8e78843af84feb4f19076468ec3c5f6b
Wooj1nBot <1403jung@gmail.com>
2024-02-17 17:43:18 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -48,4 +48,4 @@ class VariableProprecess:
         return weather.load(variables=variables)
 
     def load_models(self, variables, model_path) -> List[DVAETrainModule]:
-        for key in self.
+        for key in variables:

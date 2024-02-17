d661f56490c6e9e573411e4cd70a8d391324071b
Wooj1nBot <1403jung@gmail.com>
2024-02-17 00:38:52 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -83,8 +83,8 @@ def _main() -> None:
         vars = weather.HAS_LEVEL_VARIABLE + weather.NONE_LEVEL_VARIABLE
         input, _, _ = weather.load(variables=vars)
 
-
         
+
         val_dataset = input[config.training.train_variable]
 
         # Use a custom dataset class with proper transformations

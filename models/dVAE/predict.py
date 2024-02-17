7ab660a63f12a20995e708aadc4f7ab82478ceb9
Wooj1nBot <1403jung@gmail.com>
2024-02-17 20:56:30 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -91,7 +91,7 @@ class VariableProprecess:
         
         # dataset.shape = (vars, time, hidden)
         dataset = torch.stack(dataset, dim=0)
-        return 
+        return dataset.swapaxes(0, 1)
     
 
 

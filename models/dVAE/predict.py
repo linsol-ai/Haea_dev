3afddd48e65a4a9ec16c362e0d948f854d1d01c5
Wooj1nBot <1403jung@gmail.com>
2024-02-17 17:55:14 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -57,6 +57,7 @@ class VariableProprecess:
 
 
     def predict(self, key: str, model: DVAETrainModule, dataset: torch.Tensor):
+        print("===== PREDICT : ")
         dataset = ImageDataset(dataset)
         data_loader = DataLoader(
             dataset, batch_size=self.batch_size, num_workers=8, shuffle=False

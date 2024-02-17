2c1ee69151ea0bdb5466db3e39b832e472b30cc7
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:45:53 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -24,7 +24,7 @@ def get_dataset(year_offset: int, tgt_time_len: int, latitude, longitude):
 
     weather = WeatherDataset(year_offset, mode= WeatherDataset.RESOLUTION_MODE_BASIC_SET, device=device)
     # dataset.shape:  torch.Size([7309, 100, 1450])
-    input, target, mean_std, dims = weather.load(weather.HAS_LEVEL_VARIABLE + weather.NONE_LEVEL_VARIABLE, latitude=lat, longitude=longitude)
+    input, target, mean_std, dims = weather.load(weather.HAS_LEVEL_VARIABLE + weather.NONE_LEVEL_VARIABLE, latitude=latitude, longitude=longitude)
     dataset = CustomDataset(input, target, tgt_time_len)
     return (weather.HAS_LEVEL_VARIABLE, weather.NONE_LEVEL_VARIABLE, weather.PRESSURE_LEVELS), dataset, input.shape, mean_std, dims
 

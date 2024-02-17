7c6da0e232f25feebf3a59771ee5dc618d554e93
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:50:34 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -12,6 +12,7 @@ from pytorch_lightning.utilities.model_summary import ModelSummary
 import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset
+from da
 from models.VariableEncoder.datasets.dataset import CustomDataset
 from models.VariableEncoder.models.model import VariableEncoder
 from models.VariableEncoder.training.configs import TrainingRunConfig

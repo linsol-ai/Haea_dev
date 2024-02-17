3340f973d6dc577279235e71cf780f08396f358b
Wooj1nBot <1403jung@gmail.com>
2024-02-17 20:28:09 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -70,7 +70,7 @@ class VariableProprecess:
         
         predictions = torch.stack(predictions, dim=0)
 
-        if 
+        if len(data_array.shape) == 5:
         print(predictions.shape)
         return predictions
 

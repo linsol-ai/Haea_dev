afddfc1de790bcce6d2232a139e102ded836f2d3
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:46:23 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -57,7 +57,7 @@ def _main(args) -> None:
             var_len=shape[1],
             tgt_time_len=tgt_time_len,
             dim_model=shape[2],
-            predict_dim=dims[0],
+            out_dim=dims[0],
             batch_size=config.training.batch_size,
             num_heads=config.model.num_heads,
             n_encoder_layers=config.model.n_encoder_layers,

f2fe011f7692a457716a9eb16b95f19167b3ac55
Wooj1nBot <1403jung@gmail.com>
2024-02-17 06:39:14 +0000

    Auto commit

--- models/VariableAnalyzer/training/train.py
+++ models/VariableAnalyzer/training/train.py
@@ -108,6 +108,8 @@ def _main(args) -> None:
         trainer.fit(model_pl, train_dataloaders=train_loader, val_dataloaders=val_loader)
         trainer.test(model_pl, dataloaders=test_loader)
 
+        tra
+
 
 if __name__ == "__main__":
     app.run(_main)

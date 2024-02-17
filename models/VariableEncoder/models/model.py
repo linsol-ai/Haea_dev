819b85b15a27674ac473d560add822324edd4fd9
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:30:50 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -114,11 +114,9 @@ class VariableEncoder(nn.Module):
         for _ in range(batch_size):
             tgt_seq = []
 
-
             for i in range(0, self.tgt_time_len):
                 tgt_seq.extend([ i for _ in range(self.var_len)])
         
-            src_time_seq.append(src_seq)
             tgt_time_seq.append(tgt_seq)
         
         return torch.tensor(src_time_seq, device=device), torch.tensor(tgt_time_seq, device=device)

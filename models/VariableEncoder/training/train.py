c8541921b0fefcb38fdf32e3ddc563c94d24b187
Wooj1nBot <1403jung@gmail.com>
2024-02-17 18:21:15 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -13,7 +13,7 @@ import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset
 from models.VariableEncoder.datasets.dataset import CustomDataset
-from models.VariableEncoder.models.model import VariableAnalyzer
+from models.VariableEncoder.models.model import VariableEncoder
 from models.VariableEncoder.training.configs import TrainingRunConfig
 from models.VariableEncoder.training.lightning import TrainModule
 

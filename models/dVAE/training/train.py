53a9442ad38f9ace0f185cf6752eae76930c07f7
Wooj1nBot <1403jung@gmail.com>
2024-02-17 00:39:27 +0000

    Auto commit

--- models/dVAE/training/train.py
+++ models/dVAE/training/train.py
@@ -76,12 +76,7 @@ def _main() -> None:
         summary = ModelSummary(model_pl, max_depth=-1)
         print(summary)
 
-        device = ("cuda" if torch.cuda.is_available() else "cpu" )
-        device = torch.device(device)
-        weather = WeatherDataset(0, device=device, offline=True)
 
-        vars = weather.HAS_LEVEL_VARIABLE + weather.NONE_LEVEL_VARIABLE
-        input, _, _ = weather.load(variables=vars)
 
         
 

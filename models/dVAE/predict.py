0e33bc75700e452927ce76b07b36ef35fcc01a94
Wooj1nBot <1403jung@gmail.com>
2024-02-17 06:35:51 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -12,6 +12,13 @@ from pytorch_lightning.utilities.model_summary import ModelSummary
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks import ModelCheckpoint
 
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
+from datasets.weather_bench import WeatherDataset
 
+from models.dVAE.training.callbacks import SaveValVisualizationCallback
+from models.dVAE.training.config import TrainingRunConfig
+from models.dVAE.training.lightning import DVAETrainModule
+from models.dVAE.models.model import DiscreteVAE
 
 class VariableProprecess:

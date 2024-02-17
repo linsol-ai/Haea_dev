b110c4fb887fd295347901ef1993281c8244112c
Wooj1nBot <1403jung@gmail.com>
2024-02-17 21:07:26 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -85,7 +85,6 @@ class VariableProprecess:
         for key in self.variables:
             source_data = self.input[key]
             target_data = self.target[key]
-            
             model = self.models[key]
             # predictions.shape = (levels, time, hidden)
             predictions = self.predict_vars(key, model, source_data)

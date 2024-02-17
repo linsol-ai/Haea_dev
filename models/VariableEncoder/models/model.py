9be00e0c64ff7d6afc847d4255cf4437eb0cd8cf
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:34:36 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -45,7 +45,7 @@ class LinearDecoder(nn.Module):
         super().__init__()
         self.seq = nn.Sequential(
             nn.Linear(in_dim, out_dim),
-            
+            nn.R
         )
 
     def forward(self, x):

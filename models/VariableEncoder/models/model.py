787539b52fc4834a41e6366992141bdcfb41dcab
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:29:15 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -112,7 +112,7 @@ class VariableEncoder(nn.Module):
     
             tgt_time_seq.append(tgt_seq)
         
-        return torch.tensor(src_time_seq, device=device), torch.tensor(tgt_time_seq, device=device)
+        return torch.tensor(tgt_time_seq, device=device)
     
 
     def get_tgt_mask(self) -> torch.tensor:

a3a55a9afa1bbe3c7383fcb8abbf70e433dfe2f8
Wooj1nBot <1403jung@gmail.com>
2024-02-17 18:03:45 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -319,11 +319,11 @@ class WeatherDataset:
 
         if latitude:
 
-        lat_min, lat_max = latitude
-        lon_min, lon_max = longitude
+            lat_min, lat_max = latitude
+            lon_min, lon_max = longitude
 
-        lat_indices = np.where((dataset.latitude >= lat_min) & (dataset.latitude <= lat_max))[0]
-        lon_indices = np.where((dataset.longitude >= lon_min) & (dataset.longitude <= lon_max))[0]
+            lat_indices = np.where((dataset.latitude >= lat_min) & (dataset.latitude <= lat_max))[0]
+            lon_indices = np.where((dataset.longitude >= lon_min) & (dataset.longitude <= lon_max))[0]
 
         with ThreadPoolExecutor() as executor:
             futures = {}

c6cb93834ade8811d07232f7f0fb60c80aec68ce
Wooj1nBot <1403jung@gmail.com>
2024-02-17 20:54:42 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -83,7 +83,7 @@ class VariableProprecess:
         for key in self.variables:
             source_data = self.input[key]
             model = self.models[key]
-            #
+            # predictions.shape = ()
             predictions = self.predict_vars(key, model, source_data)
             dataset.append(predictions)
         

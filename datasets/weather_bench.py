52912e79ef5381a6db68af7978ad2cc5c167b0c6
Wooj1nBot <1403jung@gmail.com>
2024-02-17 18:03:43 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -317,7 +317,7 @@ class WeatherDataset:
 
         print("==== LOAD DATASET ====\n", dataset)
 
-        if latitude
+        if latitude:
 
         lat_min, lat_max = latitude
         lon_min, lon_max = longitude

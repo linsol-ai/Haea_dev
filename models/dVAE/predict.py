6f0dd60130cf1ae3609d14bc6f2b3001adee1765
Wooj1nBot <1403jung@gmail.com>
2024-02-17 18:13:38 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -9,8 +9,6 @@ from pydantic import ValidationError
 from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader, Dataset
-from pytorch_lightning.utilities.model_summary import ModelSummary
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pathlib import Path
 
 import sys,os

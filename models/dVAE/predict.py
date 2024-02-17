3376c2a0d3ee4edcbdb9ebac4951916291dc957c
Wooj1nBot <1403jung@gmail.com>
2024-02-17 06:44:58 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -40,4 +40,4 @@ class VariableProprecess:
         device = ("cuda" if torch.cuda.is_available() else "cpu" )
         self.device = torch.device(device)
         weather = WeatherDataset(year_offset, device=device, offline=True)
-        
+        input, _, _ = weather.load(variables=[config.training.train_variable])

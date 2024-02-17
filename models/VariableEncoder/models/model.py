3e93efabdc5a64842059af01593ca8205d61008e
Wooj1nBot <1403jung@gmail.com>
2024-02-17 23:11:25 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -94,6 +94,7 @@ class VariableEncoder(nn.Module):
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):
         # src.shape = (batch, 1, 99, 1450), tgt.shape = (batch, tgt_time_len, 99, 1450)
         src, tgt = src.squeeze(1), tgt.view(tgt.size(0), -1, tgt.size(3))
+        src_var_seq = 
 
         src = self.embedding(src, self.src_var_seq) * math.sqrt(self.dim_model)
         tgt = self.embedding(tgt, self.tgt_var_seq, self.tgt_time_seq) * math.sqrt(self.dim_model)

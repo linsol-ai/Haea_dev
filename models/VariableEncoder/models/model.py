c16a0b21a3f204acd87632760910ccb0e40edc93
Wooj1nBot <1403jung@gmail.com>
2024-02-17 22:36:11 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -45,7 +45,7 @@ class LinearDecoder(nn.Module):
         super().__init__()
         self.seq = nn.Sequential(
             nn.Linear(in_dim, out_dim),
-            nn.LayerNorm(self.hparams.model_dim),
+            nn.LayerNorm(out_dim),
             nn.ReLU(inplace=True),
             nn.Linear(out_dim, out_dim),
         )

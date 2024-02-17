131cd1ea4b82efe7dc3374f45f12a74ba9f419e6
Wooj1nBot <1403jung@gmail.com>
2024-02-17 17:52:53 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -37,7 +37,7 @@ class ImageDataset(Dataset):
             return sample
 
 class VariableProprecess:
-    def __init__(self, variables, model_path: str, year_offset: int, ):
+    def __init__(self, variables, model_path: str, year_offset: int, batch_size = 256):
         input, target, normalizaion = self.load_dataset(variables, year_offset)
         models = self.load_models(variables, model_path)
 

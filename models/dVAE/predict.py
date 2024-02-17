a15fbc8997982acb3d90c98d26912ac990865c42
Wooj1nBot <1403jung@gmail.com>
2024-02-17 20:56:09 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -89,6 +89,7 @@ class VariableProprecess:
             predictions = self.predict_vars(key, model, source_data)
             dataset.append(predictions)
         
+        #
         dataset = torch.stack(dataset, dim=0)
     
 

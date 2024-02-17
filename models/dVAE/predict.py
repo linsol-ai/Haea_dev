bdb726bcf6f33c966d3748e9f8f4946c33f0dab3
Wooj1nBot <1403jung@gmail.com>
2024-02-17 21:49:25 +0000

    Auto commit

--- models/dVAE/predict.py
+++ models/dVAE/predict.py
@@ -67,7 +67,7 @@ class VariableProprecess:
         predictions = []
         for i, batch in enumerate(tqdm.tqdm(data_loader)):
             # shape = (batch, hidden_dim)
-            predict = 
+            predict = model(batch.to(self.device))
             predictions.append(model(batch.to(self.device)))
         
         predictions = torch.cat(predictions, dim=0)

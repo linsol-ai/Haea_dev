e7ad0dd2e68712deba47bce837e0be8624fecf7e
Wooj1nBot <1403jung@gmail.com>
2024-02-18 16:40:01 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -80,7 +80,6 @@ def main(argv):
       xbeam.make_template(source_dataset)
       .sel(level=LEVEL)
       .isel(latitude=lat_indices, longitude=lon_indices)
-
   )
 
   pipeline_options = PipelineOptions(

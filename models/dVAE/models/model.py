bca1fbb6107960030295bba6abf3f3997d1f9e53
Wooj1nBot <1403jung@gmail.com>
2024-02-18 07:40:32 +0000

    Auto commit

--- models/dVAE/models/model.py
+++ models/dVAE/models/model.py
@@ -111,7 +111,7 @@ class DiscreteVAE(nn.Module):
         img_seq
     ):
         image_embeds = self.codebook(img_seq)
-        print(im)
+        print(image_embeds.shape)
         b, n, d = image_embeds.shape
         h = w = int(sqrt(n))
 

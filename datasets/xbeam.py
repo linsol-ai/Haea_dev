c6fcced14131725d63eeebc9f7a0ef922578346d
Wooj1nBot <1403jung@gmail.com>
2024-02-18 11:26:25 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -81,6 +81,7 @@ def main(argv):
 
   template = (
       xbeam.make_template(source_dataset)
+      ..sel(level=LEVEL).isel(latitude=lat_indices, longitude=lon_indices).sortby('latitude', ascending=True)
 
   )
 

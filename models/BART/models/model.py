c855f1c038ba2ffc30500a24e9de57d1c6cf3d05
Wooj1nBot <1403jung@gmail.com>
2024-02-18 18:12:47 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -96,7 +96,7 @@ class Haea(nn.Module):
         self.embedding = Embedding(max_len, dim_model, dropout)
 
         self.encoder = LinearEncoder(var_len, dropout=dropout)
-        self.decoder = LinearEncoder(var_len, dropout=dropout)
+        self.decoder = LinearDecoder(var_len, dropout=dropout)
     
 
     def init_seq(self, device):

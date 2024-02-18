b122eea2b61c3cce0b211017eae4db714aa60115
Wooj1nBot <1403jung@gmail.com>
2024-02-18 03:42:10 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -478,7 +478,7 @@ class WeatherDataset:
 
         input_dataset = torch.stack(input_dataset, dim=0)
         target_dataset = torch.stack(target_dataset, dim=0)
-        mean_std_dataset = torch.cat(mean_std_dataset)
+        mean_std_dataset = torch.cat(mean_std_dataset, dim=0)
 
         # dataset.shape => (time, var, h * w)
         input_dataset = torch.swapaxes(input_dataset, 0, 1)

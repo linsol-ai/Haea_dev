dd86428a304f3c4de574afdc3756aca51139f59c
Wooj1nBot <1403jung@gmail.com>
2024-02-18 11:44:36 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -80,13 +80,6 @@ def main(argv):
   lat_min, lat_max = LAT[FLAGS.type]
   lon_min, lon_max = LON[FLAGS.type]
 
-  # 해당 범위에 속하는 위도와 경도의 인덱스 찾기
-  lat_indices = np.where((source_dataset.latitude >= lat_min) & (source_dataset.latitude <= lat_max))[0]
-  lon_indices = np.where((source_dataset.longitude >= lon_min) & (source_dataset.longitude <= lon_max))[0]
-
-
-  source_dataset = source_dataset.sel(time=slice(start_date, end_date)).sel(level=LEVEL).isel(latitude=lat_indices, longitude=lon_indices)
-
   output_chunks = source_chunks.copy()
   output_chunks['time'] = 256
 

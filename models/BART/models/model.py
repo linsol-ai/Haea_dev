1a53e60529e2b1b3e012c12551646857ecd09629
Wooj1nBot <1403jung@gmail.com>
2024-02-18 18:09:42 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -43,7 +43,7 @@ class LinearEncoder(nn.Module):
             nn.LayerNorm(mid_dim),
             nn.ReLU(inplace=True),
             nn.Dropout(dropout),
-            nn.Linear(out_dim, out_dim),
+            nn.Linear(mid_dim, 1),
         )
 
     def forward(self, x):

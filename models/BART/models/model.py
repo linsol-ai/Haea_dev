8688d1925bbad055a8aff6989992557bcbab18a3
Wooj1nBot <1403jung@gmail.com>
2024-02-18 17:55:29 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -22,11 +22,7 @@ class Embedding(nn.Module):
         self.embed_size = embed_size
 
     def forward(self, x, variable_seq):
-        if position_seq is not None:
-            x = x + self.position(position_seq) + self.variable(variable_seq)
-            return self.dropout(x)
-        else:
-            x = x + self.variable(variable_seq)
+        x = x + self.variable(variable_seq)
             return self.dropout(x)
 
 

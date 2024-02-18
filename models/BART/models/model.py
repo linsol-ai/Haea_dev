a102d0d7327038d71ec7388a30fb8e887be69bb5
Wooj1nBot <1403jung@gmail.com>
2024-02-18 17:59:38 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -49,7 +49,7 @@ class LinearDecoder(nn.Module):
 class Haea(nn.Module):
     def __init__(self, dim_model, out_dim, batch_size, max_len=4*10, num_heads=12, n_encoder_layers=3, n_decoder_layers=3, dropout=0.1):
         super().__init__()
-        self.
+        self.max_len = max_len
         self.dim_model = dim_model
         self.batch_size = batch_size
 

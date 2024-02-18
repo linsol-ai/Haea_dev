93c4bc41f9d00942045dd3780192ecedd1d6ba8a
Wooj1nBot <1403jung@gmail.com>
2024-02-18 03:55:10 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -206,7 +206,8 @@ class WeatherDataset:
             if lat_indices is not None:
                 source = source[:, :, lat_indices, :][:, :, :, lon_indices]
                 target = source.clone().detach()
-            
+            else:
+                
 
             inputs = []
             means = []

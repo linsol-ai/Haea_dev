d32f291f934892b13fb7c29b8568f085387e39d7
Wooj1nBot <1403jung@gmail.com>
2024-02-18 03:32:55 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -126,6 +126,7 @@ def _main(args) -> None:
                 LearningRateMonitor(logging_interval="step"),
             
             ],
+            precision="bf16-mixed"
         )
 
         trainer.fit(model_pl, train_dataloaders=train_loader, val_dataloaders=val_loader)

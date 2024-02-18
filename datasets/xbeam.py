8143544b457c761b31ae53865d3a4d17b0e8fe2d
Wooj1nBot <1403jung@gmail.com>
2024-02-18 16:47:26 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -73,8 +73,8 @@ def main(argv):
   lon_indices = np.where((source_dataset.longitude >= lon_min) & (source_dataset.longitude <= lon_max))[0]
 
   source_dataset = source_dataset.sel(time=slice(start_date, end_date))
-  source_chunks['time'] = 2
-
+  source_chunks['time'] = 4
+  
   output_chunks = source_chunks.copy()
   output_chunks['time'] = 256
 

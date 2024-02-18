8172c00124f641f19d865e1e62285694c3dea525
Wooj1nBot <1403jung@gmail.com>
2024-02-18 23:02:59 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -71,8 +71,9 @@ def main(argv):
 
   if type == 0:
       source_dataset = source_dataset.sel(time=slice(start_date, end_date)).sel(level=LEVEL).isel(latitude=lat_indices, longitude=lon_indices).sortby('latitude', ascending=True)
+  elif type == 1:
+     
 
-  
   output_chunks = source_chunks.copy()
   output_chunks['time'] = 256
 

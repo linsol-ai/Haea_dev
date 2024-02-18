cb5ce3a0601df2786ef0028c866a7a1f11d31d92
Wooj1nBot <1403jung@gmail.com>
2024-02-18 00:25:32 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -57,6 +57,13 @@ class TrainModule(pl.LightningModule):
         src = batch[0]
         tgt = batch[1]
         label = batch[2]
+                has_nan = torch.isnan(predict).any()
+        if has_nan:
+            print("nan warn")
+
+                    has_nan = torch.isnan(predict).any()
+        if has_nan:
+            print("nan warn")
         label = label.view(label.size(0), -1, label.size(3))
         predict = self.model(src, tgt)
         loss = self.calculate_rmse_loss(predict, label)

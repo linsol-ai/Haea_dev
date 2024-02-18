b78492563c51df1df46bd4900ff6eb0be2b6190a
Wooj1nBot <1403jung@gmail.com>
2024-02-18 07:21:51 +0000

    Auto commit

--- models/dVAE/models/model.py
+++ models/dVAE/models/model.py
@@ -102,6 +102,7 @@ class DiscreteVAE(nn.Module):
     @eval_decorator
     def get_codebook_indices(self, images) -> torch.Tensor:
         logits = self(images, return_logits = True)
+        print(log)
         codebook_indices = logits.argmax(dim = 1).flatten(1)
         return codebook_indices
 

1910a7721b36f17091a3335911444d598e53960c
Wooj1nBot <1403jung@gmail.com>
2024-02-18 16:40:51 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -74,6 +74,7 @@ def main(argv):
 
   source_dataset = source_dataset.sel(time=slice(start_date, end_date))
   source_dataset['time'] = 8
+  
   output_chunks = source_chunks.copy()
   output_chunks['time'] = 256
 

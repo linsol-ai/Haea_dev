fd883eb066a20f02b79b613a8ff36c4a4f8cf6a0
Wooj1nBot <1403jung@gmail.com>
2024-02-18 11:24:46 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -50,6 +50,7 @@ def rekey_chunk_on_month_hour(
     if 'level' in dataset.dims:
         dataset = dataset.sel(level=level)
     new_dataset = dataset.isel(latitude=lat_indices, longitude=lon_indices)
+    
     return key, new_dataset
 
 

57d89476ba4aaad6f43db1df507445e4c25c5716
Wooj1nBot <1403jung@gmail.com>
2024-02-18 18:15:58 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -72,7 +72,7 @@ def main(argv):
   source_dataset = source_dataset.sel(time=slice(start_date, end_date)).sel(level=LEVEL).isel(latitude=lat_indices, longitude=lon_indices)
 
   output_chunks = source_chunks.copy()
-  output_chunks['time'] = 512
+  output_chunks['time'] = 256
 
   template = (
       xbeam.make_template(source_dataset)

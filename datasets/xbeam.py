46c048b8158927481cfc4d9071792df1b8b4b5db
Wooj1nBot <1403jung@gmail.com>
2024-02-18 11:28:54 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -72,6 +72,8 @@ def main(argv):
 
   start_date = pd.to_datetime(START_DATE)
   end_date = pd.to_datetime(END_DATE)
+
+  
   source_dataset = source_dataset.sel(time=slice(start_date, end_date))
 
   output_chunks = source_chunks.copy()

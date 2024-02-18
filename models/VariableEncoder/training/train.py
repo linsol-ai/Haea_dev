6d6dfa500a205350014d8cd6e731e9a24fcf3f70
Wooj1nBot <1403jung@gmail.com>
2024-02-18 00:05:17 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -39,7 +39,12 @@ def get_dataset(model_path, year_offset: int, tgt_time_len: int, latitude, longi
 
 
 def get_normal_dataset(year_offset: int, tgt_time_len: int, latitude, longitude):
-    
+    device = ("cuda" if torch.cuda.is_available() else "cpu" )
+    device = torch.device(device)
+
+    weather = WeatherDataset(year_offset, mode= WeatherDataset.RESOLUTION_MODE_BASIC_SET, device=device)
+    # dataset.shape:  torch.Size([7309, 100, 1450])
+    input, target, mean_std = weather.load(weather.HAS_LEVEL_VARIABLE + weather.NONE_LEVEL_VARIABLE, latitude=latitude, longitude=longitude)
     dataset = CustomDataset(source, target, tgt_time_len)
 
 

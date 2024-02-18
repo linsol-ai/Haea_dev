91f4ee242c7f12a021a8a7a30a6876e42ddfe7f1
Wooj1nBot <1403jung@gmail.com>
2024-02-18 08:08:18 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -215,8 +215,6 @@ class WeatherDataset:
             return torch.stack(inputs, dim=0).flatten(2), target.swapaxes(0, 1).flatten(2), torch.tensor([means, stds])
 
         else:
-            target = source.clone().detach()
-
             input, mean, std = normalize_tensor(source)
             return input.flatten(1), target.flatten(1), torch.tensor([mean, std])
     

0ebe5e191f94aa5cbbd03b6783834eea5d1328c9
Wooj1nBot <1403jung@gmail.com>
2024-02-18 11:46:54 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -107,7 +107,7 @@ def main(argv):
     processed_data = (
         root
         | beam.Create([source_dataset])
-        | 'PreprocessData' >> beam.Map(preprocess_data, level=LEVEL, start_date=start_date, end_date=end_date, LAT[FLAGS.type], )
+        | 'PreprocessData' >> beam.Map(preprocess_data, level=LEVEL, start_date=start_date, end_date=end_date, LAT[FLAGS.type], LON[FLAGS.type])
         # 필요한 추가 변환 단계
     )
     (

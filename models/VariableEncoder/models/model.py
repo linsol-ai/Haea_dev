d7f588c309e27129436d09f91be047d0f44dc0c6
Wooj1nBot <1403jung@gmail.com>
2024-02-19 00:54:31 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -105,7 +105,7 @@ class VariableEncoder(nn.Module):
         src, tgt = src.squeeze(1), tgt.view(tgt.size(0), -1, tgt.size(3))
         src_var_seq = torch.tensor([self.var_seq for _ in range(self.batch_size)], device=src.device)
 
-        src = self.embedding(src, src_var_seq) * math.sqrt(self.dim_model)
+        src = self.src_embedding(src, src_var_seq) * math.sqrt(self.dim_model)
         tgt = self.embedding(tgt, self.tgt_var_seq, self.tgt_pos_seq) * math.sqrt(self.dim_model)
         tgt_mask = self.tgt_mask.to(src.device)
 

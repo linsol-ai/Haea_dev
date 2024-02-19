381c53a1ed2e3e6955e34f437d734a6fe7840e06
Wooj1nBot <1403jung@gmail.com>
2024-02-19 02:33:49 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -146,7 +146,7 @@ class TrainModule(pl.LightningModule):
 
         print(loss.shape)
 
-        level_loss = loss[:self.levels * len(self.config.atmospheric_variable), :]
+        level_loss = loss[:self.levels * len(self.config.air_variable), :]
         non_level_loss = loss[self.levels * len(self.config.atmospheric_variable):, :]
 
         self.visualization_level(level_loss)

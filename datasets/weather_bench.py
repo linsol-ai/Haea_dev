c88adecb15bccf9bbef501048ab370e2c02d2f64
Wooj1nBot <1403jung@gmail.com>
2024-02-19 01:54:00 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -101,6 +101,8 @@ class WeatherDataset:
 
     NONE_LEVEL_VARIABLE = ['2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'total_cloud_cover', 'total_precipitation']
 
+    
+    
     PRESSURE_LEVELS = [50, 100, 150, 200, 250, 300, 400, 500, 600, 700, 850, 925, 1000]
     
     HAS_LEVEL_WIND_VAR = ['u_component_of_wind', 'v_component_of_wind']

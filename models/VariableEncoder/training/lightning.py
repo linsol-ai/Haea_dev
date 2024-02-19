826456fcb391ce1aeacfda376a2df628776a3596
Wooj1nBot <1403jung@gmail.com>
2024-02-19 02:05:51 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -130,9 +130,9 @@ class TrainModule(pl.LightningModule):
         label = label.view(label.size(0), -1, label.size(3))
         predict = self.model(src, tgt)
         loss = self.calculate_sqare_loss(predict, label, var_len)
-        
+
         # loss.shape = (batch, time_len, var_len, 1450)
-        loss = loss.view(loss.size(0), -1, self.var_len, loss.size(2))
+        loss = loss.view(loss.size(0), -1, var_len, loss.size(2))
         # loss.shape = (batch, var_len, time_len, 1450)
         loss = loss.permute(0, 2, 1, 3)
         hidden = loss.size(3)

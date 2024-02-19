4562571e403bb00cd542f3d1f2f7702843964066
Wooj1nBot <1403jung@gmail.com>
2024-02-19 01:07:32 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -28,7 +28,7 @@ def get_normal_dataset(year_offset: int, tgt_time_len: int):
     source, label, mean_std = weather.load(weather.HAS_LEVEL_VARIABLE + weather.NONE_LEVEL_VARIABLE)
 
     dataset = CustomDataset(source, label, tgt_time_len)
-    return (WeatherDataset.HAS_LEVEL_VARIABLE, WeatherDataset.NONE_LEVEL_VARIABLE, WeatherDataset.PRESSURE_LEVELS), dataset, source.shape, mean_std, target.size(-1)
+    return (WeatherDataset.HAS_LEVEL_VARIABLE, WeatherDataset.NONE_LEVEL_VARIABLE, WeatherDataset.PRESSURE_LEVELS), dataset, source.shape, mean_std, label.size(-1)
 
 
         

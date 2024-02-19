0becd38125eb96c9b8e114323fa2405bc1bc59aa
Wooj1nBot <1403jung@gmail.com>
2024-02-19 03:01:35 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -19,7 +19,7 @@ from models.VariableEncoder.training.configs import TrainingRunConfig
 from models.VariableEncoder.training.lightning import TrainModule
 
 
-def get_normal_dataset(air_variable: List[str], air_variable: List[str], year_offset: int, tgt_time_len: int):
+def get_normal_dataset(air_variable: List[str], surface_variable: List[str], year_offset: int, tgt_time_len: int):
     device = ("cuda" if torch.cuda.is_available() else "cpu" )
     device = torch.device(device)
 

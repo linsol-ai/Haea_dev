83b2bb8fb95ab2cb36660db2ad7a9cec80dfa967
Wooj1nBot <1403jung@gmail.com>
2024-02-19 00:52:46 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -44,7 +44,7 @@ class TargetEmbedding(nn.Module):
         self.embed_size = embed_size
 
     def forward(self, x, variable_seq, position_seq):
-        x = x + self.position(position_seq) + self.variable(variable_seq)
+        x = x + self.position(position_seq)
         return self.dropout(x)
 
 

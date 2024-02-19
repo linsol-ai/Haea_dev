cb1b34ba626341174ba542924c1dd64bb0640e85
Wooj1nBot <1403jung@gmail.com>
2024-02-19 01:55:25 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -26,7 +26,7 @@ def get_normal_dataset(variables:List[str], year_offset: int, tgt_time_len: int)
 
     weather = WeatherDataset(year_offset, device=device)
     # dataset.shape:  torch.Size([7309, 100, 1450])
-    source, label, mean_std = weather.load(weather.HAS_LEVEL_VARIABLE + weather.NONE_LEVEL_VARIABLE)
+    source, label, mean_std = weather.load(variables)
 
     dataset = CustomDataset(source, label, tgt_time_len)
     return (WeatherDataset.HAS_LEVEL_VARIABLE, WeatherDataset.NONE_LEVEL_VARIABLE, WeatherDataset.PRESSURE_LEVELS), dataset, source.shape, mean_std, label.size(-1)

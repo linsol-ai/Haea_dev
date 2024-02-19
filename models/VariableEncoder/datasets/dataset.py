c131553198b8fd1a5dad03bc4b8c25bc4e4637d0
Wooj1nBot <1403jung@gmail.com>
2024-02-19 01:05:51 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -35,8 +35,8 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, item):
         src_ind, tgt_ind = self.dataset_inc[item]
-        src = self.get_data(src_ind, self.input_dataset)
-        tgt = self.get_data(tgt_ind, self.input_dataset)
+        src = self.get_data(src_ind, self.source_dataset)
+        tgt = self.get_data(tgt_ind, self.source_dataset)
         label = self.get_data(tgt_ind, self.tar_dataset)
         return src, tgt, label
 

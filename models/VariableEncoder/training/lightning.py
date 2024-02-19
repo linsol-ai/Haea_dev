ecf93393815ccb328e519e3e9cac23313ada774d
Wooj1nBot <1403jung@gmail.com>
2024-02-19 02:31:43 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -86,7 +86,7 @@ class TrainModule(pl.LightningModule):
 
 
     def visualization_level(self, level_loss: torch.Tensor):
-        for i, name in range(len(self.config.atmospheric_variable)):
+        for i, name in enumerate(self.):
             start = i * len(self.levels)
             end = start + len(self.levels)
             name = self.var_lv[i]

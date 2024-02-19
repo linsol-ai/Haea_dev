494babbf7119ca7acc0b53d7384d4e386e75226f
Wooj1nBot <1403jung@gmail.com>
2024-02-19 02:05:18 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -125,6 +125,7 @@ class TrainModule(pl.LightningModule):
         src = batch[0]
         tgt = batch[1]
         label = batch[2]
+        var_len = label.size(2)
         
         label = label.view(label.size(0), -1, label.size(3))
         predict = self.model(src, tgt)

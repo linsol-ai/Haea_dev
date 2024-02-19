d3054f16de51d94c329e48d64920bbef788bbd73
Wooj1nBot <1403jung@gmail.com>
2024-02-19 02:03:05 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -71,7 +71,7 @@ class TrainModule(pl.LightningModule):
         self.model.load_state_dict(checkpoint['model_state'])
 
 
-    def calculate_rmse_loss(self, predict: torch.Tensor, label: torch.Tensor):
+    def calculate_rmse_loss(self, predict: torch.Tensor, label: torch.Tensor, var_len: int):
         # predict.shape = (batch, time_len * var_len, 1450) -> not nomalized
         predict = predict.view(predict.size(0), -1, self.var_len, predict.size(2))
         # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized

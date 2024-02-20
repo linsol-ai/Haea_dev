544d292ddf700562dd64cbc82eeb6dda04943039
wooj1nBot <1403jung@gmail.com>
2024-02-20 08:47:50 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -100,7 +100,7 @@ def main(argv):
     (
         root
         | xbeam.DatasetToChunks(source_dataset, source_chunks)
-        | beam.MapTuple(rekey_chunk_on_month_hour, )
+        | beam.MapTuple(rekey_chunk_on_month_hour, FLAGS.type)
         | xbeam.ConsolidateChunks(output_chunks)
         | xbeam.ChunksToZarr(OUTPUT_PATH, template, output_chunks)
     )

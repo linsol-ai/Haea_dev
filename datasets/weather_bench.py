9d6ba923399361e284e77e66ed2d8a071b07f88e
wooj1nBot <1403jung@gmail.com>
2024-02-20 06:35:34 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -254,4 +254,4 @@ if __name__ == '__main__':
     weather = WeatherDataset(0, device=device, offline=True)
     lat = (32, 39.3)
     lon = (124, 131.4)
-    print(weather.load(weather.)[0].shape)
+    print(weather.load(weather.V)[0].shape)

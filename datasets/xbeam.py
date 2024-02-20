5252591c6c3dc77b7de4b743a5dbc7e0ae34218c
wooj1nBot <1403jung@gmail.com>
2024-02-20 06:41:50 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -43,7 +43,7 @@ flags.mark_flag_as_required("type")
 
 
 def rekey_chunk_on_month_hour(
-    key: xbeam.Key, dataset: xarray.Dataset, lat_indices=None, lon_indices=None) -> Tuple[xbeam.Key, xarray.Dataset]:
+    key: xbeam.Key, dataset: xarray.Dataset, type, lat_indices=None, lon_indices=None) -> Tuple[xbeam.Key, xarray.Dataset]:
     if FLAGS.type == 0:
     source_dataset = source_dataset.sel(time=slice(start_date, end_date)).isel(latitude=lat_indices, longitude=lon_indices).sortby('latitude', ascending=True)
   elif FLAGS.type == 1:

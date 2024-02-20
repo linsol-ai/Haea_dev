9dee36012eaa87f8c995bdfa5b615329810bbf7b
wooj1nBot <1403jung@gmail.com>
2024-02-20 06:36:06 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -109,7 +109,7 @@ class WeatherDataset:
 
     GCS_BUCKET = 'gcs://era5_climate'
 
-    RESOLUTION = ['1440x720', '240x']
+    RESOLUTION = ['1440x720', '240x120']
 
     DIR_NAME = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'resource')
 

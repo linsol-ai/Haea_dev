121d8f7e219295bf20afd0311e8a3fdf3754a066
wooj1nBot <1403jung@gmail.com>
2024-02-21 08:05:07 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -126,7 +126,7 @@ def main(argv):
   with beam.Pipeline(options=pipeline_options) as root :
     (
         root
-        | xbeam.DatasetToChunks(ds_stacked, split_vars=True)
+        | xbeam.DatasetToChunks(ds_stacked, {},split_vars=True)
         | xbeam.Rechunk(  # pytype: disable=wrong-arg-types
             source_dataset.sizes,
             source_chunks,

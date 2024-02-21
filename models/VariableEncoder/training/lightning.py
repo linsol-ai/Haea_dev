4d9de488ede52382b28fd42c5a1a3679707a5143
wooj1nBot <1403jung@gmail.com>
2024-02-21 16:54:51 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -135,9 +135,7 @@ class TrainModule(pl.LightningModule):
     def validation(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]):
         src = batch[0]
         tgt = batch[1]
-     
-
-        label = label.view(label.size(0), -1, label.size(3))
+    
         predict = self.model(src, tgt)
         loss = self.calculate_sqare_loss(predict, label, var_len)
 

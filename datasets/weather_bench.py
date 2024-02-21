6b37a5840c2ce659e15740a3e6a9525a76ed48db
wooj1nBot <1403jung@gmail.com>
2024-02-21 16:28:24 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tqdm import tqdm
-from typing import Tuple, List
+from typing import Tuple,
 import numpy as np
 import xarray_beam as xb
 import xarray as xr
@@ -214,7 +214,7 @@ class WeatherDataset:
 
 
 
-    def load_data(self, dataset:xr.Dataset, variables=List[str], only_input_variable=List[str]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def load_data(self, dataset:xr.Dataset, variables=List, ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         start = time.time()
         result = {}
 
@@ -224,7 +224,7 @@ class WeatherDataset:
             futures = {}
 
             for val in variables:
-                key = executor.submit(self.load_variable_optimized, dataset[val], val in only_input_variable)
+                key = executor.submit(self.load_variable_optimized, dataset[val])
                 futures[key] = val
 
             for future in tqdm(as_completed(futures), desc="Processing futures"):

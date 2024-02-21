2dd69f1f8eb5d258aac605a2a379b21627c5bd0e
wooj1nBot <1403jung@gmail.com>
2024-02-21 16:15:15 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -224,7 +224,7 @@ class WeatherDataset:
             futures = {}
 
             for val in variables:
-                key = executor.submit(self.load_variable_optimized, dataset[val])
+                key = executor.submit(self.load_variable_optimized, dataset[val], )
                 futures[key] = val
 
             for future in tqdm(as_completed(futures), desc="Processing futures"):

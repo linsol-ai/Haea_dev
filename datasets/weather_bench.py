b6c20941db09a941923205adca2b2cd8e62962a9
wooj1nBot <1403jung@gmail.com>
2024-02-21 16:43:13 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -244,7 +244,6 @@ class WeatherDataset:
 
         # dataset.shape => (time, var, h * w)
         input_dataset = torch.swapaxes(input_dataset, 0, 1)
-        target_dataset = torch.swapaxes(target_dataset, 0, 1)
         
         end = time.time()
         print(f"{end - start:.5f} sec")

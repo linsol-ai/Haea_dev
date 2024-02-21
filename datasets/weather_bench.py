00ccf94b42a76dd412c2feb25915cada80c0910b
wooj1nBot <1403jung@gmail.com>
2024-02-21 14:08:32 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -209,7 +209,7 @@ class WeatherDataset:
         else:
             input, mean, std = normalize_tensor(source)
             if source_only:
-                
+                return input.flatten(1)
             else:
                 return input.flatten(1), source.flatten(1), torch.tensor([mean, std])
 

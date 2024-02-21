69d65c7bebb168227d99fcb6034c9b41a204700f
wooj1nBot <1403jung@gmail.com>
2024-02-21 09:11:15 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -155,7 +155,8 @@ class TrainModule(pl.LightningModule):
         # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized
         reversed_predict = denormalize(predict, self.mean_std)
         reversed_predict = reversed_predict[:, :, :-1, :]
-        reversed_predict = reversed_predict.view(reversed_predict.size(0), -1, reversed_predict.size(3))
+        reversed_predict = reversed_predict.reshape(reversed_predict.size(0), -1, reversed_predict.size(3))
+
         # reversed_predict.shape = (batch, time_len * var_len, 1450) -> nomalized
         loss = F.mse_loss(reversed_predict, label, reduction='none')
         return loss

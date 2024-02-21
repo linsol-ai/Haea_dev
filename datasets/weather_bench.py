6ef70bd6a446a5ffcd19868f0eb5ff3edaab383a
wooj1nBot <1403jung@gmail.com>
2024-02-21 14:09:12 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -215,7 +215,7 @@ class WeatherDataset:
 
 
 
-    def load_data(self, dataset:xr.Dataset, variables) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def load_data(self, dataset:xr.Dataset, variables, ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         start = time.time()
         result = {}
 

e364cdb1c025efe82b9b38b16b32c2ae536c3822
wooj1nBot <1403jung@gmail.com>
2024-02-21 05:36:08 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -230,7 +230,7 @@ class WeatherDataset:
     def load_data_chunk(self, dataset: xr.Dataset, air_variable, surface_variable) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         start = time.time()
         result = {}
-        
+        air_dataset = 
 
 
 

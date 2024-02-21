f1a1f2f05e68210d35f7ad7568822ea5234488a8
wooj1nBot <1403jung@gmail.com>
2024-02-21 17:23:13 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -29,9 +29,6 @@ def get_normal_dataset(config: TrainingConfig, year_offset: int, tgt_time_len: i
     # dataset.shape:  torch.Size([7309, 100, 1450])
     source, label, mean_std = weather.load(config.air_variable, config.surface_variable)
 
-    print(source.shape)
-    print(label.shape)
-
     dataset = CustomDataset(source, label, tgt_time_len)
     return dataset, source.shape, mean_std, label.size(-1)
 

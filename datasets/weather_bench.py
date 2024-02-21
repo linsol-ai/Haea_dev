8d9c897721d8505cccec2435cf5692b108906be5
wooj1nBot <1403jung@gmail.com>
2024-02-21 05:12:49 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -180,6 +180,7 @@ class WeatherDataset:
     def load(self, air_variable, surface_variable):
         variables = air_variable + surface_variable
         source_t, label, mean_std = self.load_data(self.datasets[0], variables)
+        gc.collect()
         source_b, _, _ = self.load_data(self.datasets[1], variables)
         
         # var_dataset.shape = (time, var * level, h * w)

dcd7c89b5d5823c65f96cebefc5e9e01f664b0fe
wooj1nBot <1403jung@gmail.com>
2024-02-21 04:59:33 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -187,7 +187,7 @@ class WeatherDataset:
                 inputs.append(input.unsqueeze(0))
                 stats[:, i] = torch.tensor([mean.item(), std.item()])
 
-            inputs = torch.stack(inputs, dim=0)  # dim=0 대신 dim=1을 사용하여 stack
+            inputs = torch.cat(inputs, dim=0)  # dim=0 대신 dim=1을 사용하여 stack
             return inputs.flatten(2), source.permute(1, 0, 2, 3).flatten(2), stats
 
         else:

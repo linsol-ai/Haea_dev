ca5a3a45c6122111c012238c7c4dba706fae1a99
wooj1nBot <1403jung@gmail.com>
2024-02-21 16:44:42 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -6,7 +6,6 @@ class CustomDataset(Dataset):
     def __init__(self, source_dataset: torch.Tensor, tgt_time_len: int):
         # dataset.shape = (time, var_len, hidden)
         self.source_dataset = source_dataset
-        self.label_dataset = label_dataset
         self.tgt_time_len = tgt_time_len
         self.make_dataset()
 

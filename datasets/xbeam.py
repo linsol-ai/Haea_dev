078a19a1b4de86c7b70d87b29616a5e5f6009034
wooj1nBot <1403jung@gmail.com>
2024-02-21 02:04:17 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -105,6 +105,7 @@ def main(argv):
     (
         root
         | xbeam.DatasetToChunks(source_dataset, source_chunks)
+        
         | xbeam.ConsolidateChunks( out_chunks )
         | xbeam.ChunksToZarr(OUTPUT_PATH, template)
     )

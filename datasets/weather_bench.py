a23d2a34415b2b29960e04326a030b7556bdbc45
wooj1nBot <1403jung@gmail.com>
2024-02-22 13:09:31 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -131,7 +131,7 @@ class WeatherDataset:
             code.extend(air_list)
 
         for sur in surface_var:
-            idx = self.AIR_VARIABLE.index(air) + 1
+            idx = self.SURFACE_VARIABLE.index(air) + 1
         
 
     

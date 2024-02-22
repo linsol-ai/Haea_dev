956c5a55e0b34cccd80c5c383886d7a9f967b5cf
wooj1nBot <1403jung@gmail.com>
2024-02-22 12:05:19 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -113,6 +113,7 @@ class VariableEncoder(nn.Module):
             tgt_seq.append(t_seq)
         
         tgt_seq = torch.tensor(tgt_seq, device=device)
+        torch.tensor([self.src_var_list for _ in range(self.batch_size)], device=device)
         return tgt_seq
 
 

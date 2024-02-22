f4bba96f5013912672505f459f81fa20d9ec880f
wooj1nBot <1403jung@gmail.com>
2024-02-22 13:03:33 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -127,7 +127,7 @@ class WeatherDataset:
         code = []
         for air in air_var:
             idx = self.VARIABLES.index(air) + 1
-            air_list = [(idx-1) + i for i in range(level)]
+            air_list = [(idx-1)* + i for i in range(level)]
     
 
     def check_dataset(self, start, end):

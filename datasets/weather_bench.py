a649061981e984503325000d6a00e6ec4e197c3d
wooj1nBot <1403jung@gmail.com>
2024-02-22 15:52:59 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -255,6 +255,14 @@ class WeatherDataset:
         input_dataset = torch.cat(input_dataset, dim=0)
         mean_std_dataset = torch.cat(mean_std_dataset, dim=0)
 
+        static_dataset = []
+        for val in static_variables:
+            input, mean_std = self.load_variable_optimized(dataset[val].unsqueeze(0))
+            static_dataset.append(input)
+            mean_std_dataset.append(mean_std.swapaxes(0, 1))
+
+        static_dataset = torch.cat(static_dataset, dim=0)
+        
 
 
         # dataset.shape => (time, var, h * w)

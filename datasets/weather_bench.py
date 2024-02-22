822392b890e929e47e2e577d279aa0c52df7fc53
wooj1nBot <1403jung@gmail.com>
2024-02-22 12:29:12 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -126,7 +126,7 @@ class WeatherDataset:
     def get_var_code(self, air_var, surface_var):
         code = []
         for air in air_var:
-            idx = self.VARIABLES[
+            idx = self.VARIABLES.index(ai)
             air_list = []
     
 

4c20c12d456bd74afd92b988d83868524100fbb9
wooj1nBot <1403jung@gmail.com>
2024-02-22 12:12:47 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -122,7 +122,7 @@ class WeatherDataset:
         dataset_path = self.check_dataset(start, end)
         self.load_dataset(dataset_path)
     
-    def get_var_code
+    def get_var_code()
 
     
     def check_dataset(self, start, end):

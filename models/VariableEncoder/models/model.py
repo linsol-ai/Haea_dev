737655d425138ba16aafb7efa38a77d14a6250b2
wooj1nBot <1403jung@gmail.com>
2024-02-22 12:01:56 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -61,7 +61,7 @@ class VariableEncoder(nn.Module):
                  dim_model: int, out_dim: int, batch_size: int, 
                  max_len=4*10, num_heads=12, n_encoder_layers=3, n_decoder_layers=3, dropout=0.1):
         super().__init__()
-        self.src_var_seq = range(var_len)
+        self.src_var_seq = src_var_seq
         self.var_len = var_len
         self.tgt_time_len = tgt_time_len
         self.dim_model = dim_model

2a02ff09d680c0e77efdde58317f28049539b9b0
wooj1nBot <1403jung@gmail.com>
2024-02-22 09:03:44 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -83,7 +83,7 @@ def _main(args) -> None:
         )   
 
         train_loader = DataLoader(
-            train_ds, batch_size=config.training.batch_size, shuffle=True, drop_last=True, num_workers=4
+            train_ds, batch_size=config.training.batch_size, shuffle=True, drop_last=True, num_workers=2
         )
         test_loader = DataLoader(test_ds, batch_size=config.training.batch_size, drop_last=True, num_workers=2)
         val_loader = DataLoader(val_ds, batch_size=config.training.batch_size, drop_last=True, num_workers=2)

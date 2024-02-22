9d7dafc076e6291bb2e2dda29d80cebd88bc5e8e
wooj1nBot <1403jung@gmail.com>
2024-02-22 12:04:41 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -84,7 +84,7 @@ class VariableEncoder(nn.Module):
     def init_seq(self, device):
         print("init", device)
         self.tgt_mask = self.get_tgt_mask()
-        
+
         self.tgt_var_seq = self.get_var_seq(self.batch_size, device)
         self.tgt_pos_seq = self.get_pos_seq(self.batch_size, device)
 
@@ -92,7 +92,6 @@ class VariableEncoder(nn.Module):
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):
         # src.shape = (batch, 1, 99, 1450), tgt.shape = (batch, tgt_time_len, 99, 1450)
         src, tgt = src.squeeze(1), tgt.view(tgt.size(0), -1, tgt.size(3))
-        src_var_seq = torch.tensor([self.src_var_list for _ in range(self.batch_size)], device=src.device)
 
         src = self.embedding(src, src_var_seq) * math.sqrt(self.dim_model)
         tgt = self.embedding(tgt, self.tgt_var_seq, self.tgt_pos_seq) * math.sqrt(self.dim_model)

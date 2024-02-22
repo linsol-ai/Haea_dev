3c27e5dc974ef9caf2bc2773f5a350c6e0b543f6
wooj1nBot <1403jung@gmail.com>
2024-02-22 10:26:27 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -18,7 +18,7 @@ class CustomDataset(Dataset):
         result = []
         for t in indicate:
             data = dataset[t]
-            if 
+            if source
             result.append(data)
 
         # dataset.shape = (time_len, var, hidden)

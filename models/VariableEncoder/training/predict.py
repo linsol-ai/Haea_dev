73587869963788b3e7ff85bc2761135f020d246c
wooj1nBot <1403jung@gmail.com>
2024-02-23 17:03:23 +0000

    Auto commit

--- models/VariableEncoder/training/predict.py
+++ models/VariableEncoder/training/predict.py
@@ -12,7 +12,8 @@ from training.lightning import TrainModule
 class VariablePredictor:
     def __init__(self, model_path: str, batch_size: int):
         self.model_path = model_path
-        self.batch_size = 
+        self.batch_size = batch_size
+        self.load_models(model_path)
     
     def load_models(self, model_path):
         folder_path = Path(model_path)

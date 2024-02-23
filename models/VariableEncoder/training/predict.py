222ebe22c2c4788517375ec425c61c87f3047d54
wooj1nBot <1403jung@gmail.com>
2024-02-23 16:57:49 +0000

    Auto commit

--- models/VariableEncoder/training/predict.py
+++ models/VariableEncoder/training/predict.py
@@ -4,4 +4,17 @@ class VariablePredictor:
         self.model_path = model_path
         self.batch_size
     
-    
+    def load_models(self, variables, model_path):
+        models = {}
+        for key in variables:
+            folder_path = Path(os.path.join(model_path, key))
+            first_file = next(folder_path.iterdir(), None)
+            if first_file:
+                print(f"====== LOAD MODELS : {key} =======")
+                model = DVAETrainModule.load_from_checkpoint(first_file)
+                models[key] = model
+            else:
+                print("변수 폴더가 비어있습니다.")
+                break
+
+        return models

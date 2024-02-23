88db9c914cb8c24ed3f0ea75b8c2136916332918
wooj1nBot <1403jung@gmail.com>
2024-02-23 09:52:50 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -25,7 +25,7 @@ def get_normal_dataset(config: TrainingConfig, year_offset: int, tgt_time_len: i
     device = ("cuda" if torch.cuda.is_available() else "cpu" )
     device = torch.device(device)
 
-    weather = WeatherDataset(config.train, device=device)
+    weather = WeatherDataset(config.train_offset, device=device)
     # dataset.shape:  torch.Size([7309, 100, 1450])
     source, label, mean_std = weather.load(config.air_variable, config.surface_variable, config.only_input_variable, config.constant_variable)
     dataset = CustomDataset(source, label, tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))

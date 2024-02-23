be56881d1197254958f3bdabc2e9c4aed9abd197
wooj1nBot <1403jung@gmail.com>
2024-02-23 07:55:54 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -130,7 +130,7 @@ class Haea(nn.Module):
         return torch.tensor(tgt_pos_seq, device=device)
     
 
-     def get_tgt_mask(self, size) -> torch.tensor:
+    def get_tgt_mask(self, size) -> torch.tensor:
         mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
         mask = mask.float()
         mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf

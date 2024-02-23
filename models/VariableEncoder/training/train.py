ae4f7205dae2d245204f37dd7efe7d059c73a1d6
wooj1nBot <1403jung@gmail.com>
2024-02-23 09:51:34 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -34,7 +34,31 @@ def get_normal_dataset(config: TrainingConfig, year_offset: int, tgt_time_len: i
     return dataset, mean_std, (src_var_list, tgt_var_list)
 
 
+class MNISTDataModule(L.LightningDataModule):
+    def __init__(self, data_dir: str = "path/to/dir", batch_size: int = 32):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
 
+    def setup(self, stage: str):
+        self.mnist_test = MNIST(self.data_dir, train=False)
+        self.mnist_predict = MNIST(self.data_dir, train=False)
+        mnist_full = MNIST(self.data_dir, train=True)
+        self.mnist_train, self.mnist_val = random_split(
+            mnist_full, [55000, 5000], generator=torch.Generator().manual_seed(42)
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=self.batch_size)
+
+    def predict_dataloader(self):
+        return DataLoader(self.mnist_predict, batch_size=self.batch_size)
 
 
 config_path = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'configs/train_config.yaml')

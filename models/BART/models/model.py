48e73cd7c3cd37831ec77be06fe0feb91f2ef665
wooj1nBot <1403jung@gmail.com>
2024-02-23 08:09:09 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -49,6 +49,7 @@ class LinearEncoder(nn.Module):
     def forward(self, x: torch.Tensor):
        # x.shape = (batch, time_len, var_len, hidden)
        x = x.swapaxes(2, 3)
+       # x.shape = (batch, time_len, var_len, hidden)
        return self.seq(x).unsqueeze(-1)
     
 

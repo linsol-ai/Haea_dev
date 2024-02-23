f11e6c614fe3c626640f5d456a34fe3d6f2e8ce7
wooj1nBot <1403jung@gmail.com>
2024-02-23 09:16:16 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -22,16 +22,16 @@ from models.VariableEncoder.training.lightning import TrainModule
 
 
 def get_normal_dataset(config: TrainingConfig, year_offset: int, tgt_time_len: int):
-device = ("cuda" if torch.cuda.is_available() else "cpu" )
-device = torch.device(device)
+    device = ("cuda" if torch.cuda.is_available() else "cpu" )
+    device = torch.device(device)
 
-weather = WeatherDataset(year_offset, device=device)
-# dataset.shape:  torch.Size([7309, 100, 1450])
-source, label, mean_std = weather.load(config.air_variable, config.surface_variable, config.only_input_variable, config.constant_variable)
-dataset = CustomDataset(source, label, tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))
-src_var_list = weather.get_var_code(config.air_variable, config.surface_variable + config.only_input_variable+config.constant_variable)
-tgt_var_list = weather.get_var_code(config.air_variable, config.surface_variable)
-return dataset, mean_std, (src_var_list, tgt_var_list)
+    weather = WeatherDataset(year_offset, device=device)
+    # dataset.shape:  torch.Size([7309, 100, 1450])
+    source, label, mean_std = weather.load(config.air_variable, config.surface_variable, config.only_input_variable, config.constant_variable)
+    dataset = CustomDataset(source, label, tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))
+    src_var_list = weather.get_var_code(config.air_variable, config.surface_variable + config.only_input_variable+config.constant_variable)
+    tgt_var_list = weather.get_var_code(config.air_variable, config.surface_variable)
+    return dataset, mean_std, (src_var_list, tgt_var_list)
 
 
 config_path = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'configs/train_config.yaml')

1d7b2a098eb2118c7b85ee5a5b8e5e62a5d12ab0
wooj1nBot <1403jung@gmail.com>
2024-02-23 12:42:13 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -223,7 +223,7 @@ class WeatherDataset:
         
     def load_data_single(self, dataset: xr.Dataset, variables, constant_variables):
         start = time.time()
-        result = {}
+        result = []
 
         print("==== LOAD DATASET ====\n", dataset)
 

1830a6a645d990dbed638921fce94218c150f7f2
wooj1nBot <1403jung@gmail.com>
2024-02-23 17:18:18 +0000

    Auto commit

--- models/VariableEncoder/training/predict.py
+++ models/VariableEncoder/training/predict.py
@@ -35,4 +35,4 @@ class VariablePredictor:
     
     def predict(self, dataset: torch.Tensor) -> torch.Tensor:
         dataset = EncoderDataset(dataset)
-        encoder = self.model.model.transformer.encoder
+        encoder = self.model.model.transformer.encoder()

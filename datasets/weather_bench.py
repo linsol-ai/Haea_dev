d428df0e7de4bd1003e0e87db6b9cb40c7bcf636
wooj1nBot <1403jung@gmail.com>
2024-02-23 12:37:33 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -191,8 +191,8 @@ class WeatherDataset:
 
     def load(self, air_variable, surface_variable, only_input_variable=[], constant_variables=[]):
         variables = air_variable + surface_variable + only_input_variable
-        source_t, mean_std = self.load_data(self.datasets[0], variables, constant_variables)
-        source_b, _ = self.load_data(self.datasets[1], variables, constant_variables)
+        source_t, mean_std = self.load_data_single(self.datasets[0], variables, constant_variables)
+        source_b, _ = self.load_data_single(self.datasets[1], variables, constant_variables)
         
         # var_dataset.shape = (time, var * level, h * w)
         source = torch.cat([source_t, source_b], dim=2)

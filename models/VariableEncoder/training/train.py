2187c8a5056c1f3e85a3fc6e2a1ffb300217b5fe
wooj1nBot <1403jung@gmail.com>
2024-02-23 09:55:55 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -40,7 +40,7 @@ class MNISTDataModule(pl.LightningDataModule):
         super().__init__()
         self.config = config
         self.batch_size = batch_size
-        self.dataset, self.mean_std, var_list = get_normal_dataset(self.config)
+        self.dataset, self.mean_std, self.var_list = get_normal_dataset(self.config)
 
     def setup(self, stage: str):
         dataset, mean_std, var_list = get_normal_dataset(self.config)

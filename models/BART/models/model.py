cec629adf79aa6ac70df8b136e2c9ce0dc57f393
wooj1nBot <1403jung@gmail.com>
2024-02-23 08:11:22 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -78,7 +78,7 @@ class LinearDecoder(nn.Module):
        x = x.unsqueeze(-1)
        # x.shape = (batch, time_len, hidden, var_len)
        x = self.seq(x)
-       return x.swapaxes()
+       return x.swapaxes(2, 3)
 
 
 class Haea(nn.Module):

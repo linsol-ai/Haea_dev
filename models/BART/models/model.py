97f40f027eb287548f94adf4ca6a1b17439fce91
wooj1nBot <1403jung@gmail.com>
2024-02-23 07:58:24 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -4,9 +4,28 @@ from torch.nn import functional as F
 import math
 
 
-class PositionalEmbedding(nn.Embedding):
-    def __init__(self, time_len, embed_size=768):
-        super().__init__(time_len, embed_size, padding_idx=0)
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, dropout_p, max_len):
+        super().__init__()
+        
+        self.dropout = nn.Dropout(dropout_p)
+ 
+        # Encoding - From formula
+        pos_encoding = torch.zeros(max_len, dim_model)
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
+        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
+ 
+        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
+        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
+ 
+        # Saving buffer (same as parameter without gradients needed)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding", pos_encoding)
+ 
+    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
+        # Residual connection + pos encoding
+        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
+
 
 
 class Embedding(nn.Module):

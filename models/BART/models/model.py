1d2ba39010c588c63592ae5a7f106e7a26fb6eef
wooj1nBot <1403jung@gmail.com>
2024-02-23 11:43:11 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -120,7 +120,7 @@ class Haea(nn.Module):
         tgt_mask = self.tgt_mask.to(src.device)
 
         transformer_out = self.bart(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=None, tgt_key_padding_mask=None)
-        out = self.out(transformer_out)
+        out = self.decoder()
         return out
     
 

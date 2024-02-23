541248c29337f2e0bc575f5d797e7d996afc850c
wooj1nBot <1403jung@gmail.com>
2024-02-23 09:25:18 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -89,27 +89,27 @@ test_loader = DataLoader(test_ds, batch_size=config.training.batch_size, drop_la
 val_loader = DataLoader(val_ds, batch_size=config.training.batch_size, drop_last=True, num_workers=2)
 
 if __name__=='__main__':
-print("setting lr rate: ", config.training.learning_rate)
+    print("setting lr rate: ", config.training.learning_rate)
 
-model_pl = TrainModule(model=model, mean_std=mean_std, max_iters=config.training.max_epochs*len(train_loader), 
-   pressure_level=WeatherDataset.PRESSURE_LEVEL, config=config.training)
+    model_pl = TrainModule(model=model, mean_std=mean_std, max_iters=config.training.max_epochs*len(train_loader), 
+    pressure_level=WeatherDataset.PRESSURE_LEVEL, config=config.training)
 
-summary = ModelSummary(model_pl, max_depth=-1)
-print(summary)
+    summary = ModelSummary(model_pl, max_depth=-1)
+    print(summary)
 
-trainer = pl.Trainer(
-    accelerator="auto",
-    devices=-1,
-    strategy="ddp",
-    max_epochs=config.training.max_epochs,
-    logger=logger,
-    gradient_clip_val=config.training.gradient_clip_val,
-    callbacks=[
-    LearningRateMonitor(logging_interval="step"),
+    trainer = pl.Trainer(
+        accelerator="auto",
+        devices=-1,
+        strategy="ddp",
+        max_epochs=config.training.max_epochs,
+        logger=logger,
+        gradient_clip_val=config.training.gradient_clip_val,
+        callbacks=[
+        LearningRateMonitor(logging_interval="step"),
 
-    ],
-    precision="bf16-mixed"
-)
+        ],
+        precision="bf16-mixed"
+    )
 
-trainer.fit(model_pl, train_dataloaders=train_loader, val_dataloaders=val_loader)
-trainer.test(model_pl, dataloaders=test_loader)
+    trainer.fit(model_pl, train_dataloaders=train_loader, val_dataloaders=val_loader)
+    trainer.test(model_pl, dataloaders=test_loader)

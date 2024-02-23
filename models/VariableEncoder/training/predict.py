33e3032e1daa905a28f4c659d47ffe8ea05b9a78
wooj1nBot <1403jung@gmail.com>
2024-02-23 16:58:06 +0000

    Auto commit

--- models/VariableEncoder/training/predict.py
+++ models/VariableEncoder/training/predict.py
@@ -1,4 +1,16 @@
+import argparse
+import logging
+import tqdm
+import pytorch_lightning as pl
+import torch
+from typing import Tuple
+from torch.utils.data import DataLoader, Dataset
 from pathlib import Path
+
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from datasets.weather_bench import WeatherDataset
+from models.dVAE.training.lightning import DVAETrainModule
 
 class VariablePredictor:
     def __init__(self, model_path: str, batch_size: int):

05411d7c66599aa558177066526db1407c1a9d11
wooj1nBot <1403jung@gmail.com>
2024-02-23 17:00:27 +0000

    Auto commit

--- models/VariableEncoder/training/predict.py
+++ models/VariableEncoder/training/predict.py
@@ -21,6 +21,6 @@ class VariablePredictor:
                 model = TrainModule.load_from_checkpoint(first_file)
                 self.model = model
         else:
-            print("")
+            print("no ")
 
         return models

312dcf35a6f91e502c71a8ef3fb15525dadb74aa
wooj1nBot <1403jung@gmail.com>
2024-02-23 09:25:01 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -88,27 +88,4 @@ train_loader = DataLoader(
 test_loader = DataLoader(test_ds, batch_size=config.training.batch_size, drop_last=True, num_workers=2)
 val_loader = DataLoader(val_ds, batch_size=config.training.batch_size, drop_last=True, num_workers=2)
 
-print("setting lr rate: ", config.training.learning_rate)
 
-model_pl = TrainModule(model=model, mean_std=mean_std, max_iters=config.training.max_epochs*len(train_loader), 
-   pressure_level=WeatherDataset.PRESSURE_LEVEL, config=config.training)
-
-summary = ModelSummary(model_pl, max_depth=-1)
-print(summary)
-
-trainer = pl.Trainer(
-    accelerator="auto",
-    devices=-1,
-    strategy="ddp",
-    max_epochs=config.training.max_epochs,
-    logger=logger,
-    gradient_clip_val=config.training.gradient_clip_val,
-    callbacks=[
-    LearningRateMonitor(logging_interval="step"),
-
-    ],
-    precision="bf16-mixed"
-)
-
-trainer.fit(model_pl, train_dataloaders=train_loader, val_dataloaders=val_loader)
-trainer.test(model_pl, dataloaders=test_loader)

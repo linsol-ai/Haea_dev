97f3e64851a5eda14e04b4a2ac3fe53b2a7712df
wooj1nBot <1403jung@gmail.com>
2024-02-23 17:01:01 +0000

    Auto commit

--- models/VariableEncoder/training/predict.py
+++ models/VariableEncoder/training/predict.py
@@ -21,6 +21,6 @@ class VariablePredictor:
                 model = TrainModule.load_from_checkpoint(first_file)
                 self.model = model
         else:
-            print("not exists VariableEncoder model")
+            print("Not exists VariableEncoder model")
 
         return models

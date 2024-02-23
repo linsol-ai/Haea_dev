f014bc2b2b6008e1164b1c3fee8a9794931f8751
wooj1nBot <1403jung@gmail.com>
2024-02-23 07:18:41 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -100,7 +100,7 @@ def _main(args) -> None:
         trainer = pl.Trainer(
             accelerator="auto",
             devices=-1,
-            strategy=
+            strategy="ddp",
             max_epochs=config.training.max_epochs,
             logger=logger,
             gradient_clip_val=config.training.gradient_clip_val,

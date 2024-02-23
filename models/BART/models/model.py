ec3d9c1189de628090d4f00cdf937cb5b4050c1e
wooj1nBot <1403jung@gmail.com>
2024-02-23 08:03:33 +0000

    Auto commit

--- models/BART/models/model.py
+++ models/BART/models/model.py
@@ -4,27 +4,26 @@ from torch.nn import functional as F
 import math
 
 
-class PositionalEncoding(nn.Module):
-    def __init__(self, dim_model, max_len, dropout_p):
+class PositionalEmbedding(nn.Module):
+
+    def __init__(self, d_model, max_len=512):
         super().__init__()
-        
-        self.dropout = nn.Dropout(dropout_p)
- 
-        # Encoding - From formula
-        pos_encoding = torch.zeros(max_len, dim_model)
-        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
-        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
- 
-        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
-        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
- 
-        # Saving buffer (same as parameter without gradients needed)
-        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
-        self.register_buffer("pos_encoding", pos_encoding)
- 
-    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
-        # Residual connection + pos encoding
-        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
 
 
 class LinearEncoder(nn.Module):

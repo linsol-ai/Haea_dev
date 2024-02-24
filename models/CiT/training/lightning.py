536252c6c8da9f93a02568b8f9a13b8855ed1ab5
wooj1nBot <1403jung@gmail.com>
2024-02-24 16:03:23 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -60,7 +60,6 @@ class TrainModule(pl.LightningModule):
         # target.size = (batch, var_len, hidden)
         var_len = label.size(2)
         label = denormalize(label, self.mean_std)
-
         # predict.shape = (batch, time_len * var_len, hidden) -> not nomalized
         predict = predict.view(predict.size(0), -1, var_len, predict.size(2))
         # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized

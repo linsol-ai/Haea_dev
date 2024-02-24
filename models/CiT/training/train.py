7339289fe8b03102394e587076171941ba63bc54
wooj1nBot <1403jung@gmail.com>
2024-02-24 16:21:55 +0000

    Auto commit

--- models/CiT/training/train.py
+++ models/CiT/training/train.py
@@ -14,7 +14,7 @@ import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset
 from models.CiT.datasets.dataset import CustomDataset
-from models.CiT.models.model import VariableEncoder
+from models.CiT.models.model import ClimateTransformer
 
 from models.VariableEncoder.training.configs import TrainingConfig
 from models.VariableEncoder.training.configs import TrainingRunConfig

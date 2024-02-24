2b63de1dff1501d3e14ccf0e904df961502aab0e
wooj1nBot <1403jung@gmail.com>
2024-02-24 11:32:54 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -174,7 +174,6 @@ class TrainModule(pl.LightningModule):
         return self(batch)
 
     def forward(self, batch):
-        self.model.init_seq(self.device, batch.size(0))
         src = batch[0].to(self.device)
         tgt = batch[1].to(self.device)
         label = batch[2].to(self.device)

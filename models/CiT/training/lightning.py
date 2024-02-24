dbc8387b63be549f463b97af99ee98c10ebda2ff
wooj1nBot <1403jung@gmail.com>
2024-02-24 16:20:49 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -103,6 +103,6 @@ class TrainModule(pl.LightningModule):
         src.cpu().detach()
         label.cpu().detach()
         delta.cpu().detach()
-        return predict.cpu().detach(), label
+        return predict.cpu().detach(), label.cpu().detach()
     
 

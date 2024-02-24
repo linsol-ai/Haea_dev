05393ce9b42de3070ce07a39dd8d0b4e5303ba39
wooj1nBot <1403jung@gmail.com>
2024-02-24 15:39:34 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -4,7 +4,7 @@ import torch
 class CustomDataset(Dataset):
 
     def __init__(self, source_dataset: torch.Tensor, label_dataset: torch.Tensor, max_lead_time: int = 78):
-        # dataset.shape = (time, 1, var_len, hidden)
+        # dataset.shape = (time, var_len, hidden)
         self.source_dataset = source_dataset.unsqueeze(1)
         self.label_dataset = label_dataset.unsqueeze(1)
         self.max_lead_time = max_lead_time

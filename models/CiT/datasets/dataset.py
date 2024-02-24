30d061eb16fca05967ba99ac718f0cfeb311b266
wooj1nBot <1403jung@gmail.com>
2024-02-24 15:39:26 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -12,7 +12,7 @@ class CustomDataset(Dataset):
     def __len__(self):
         return self.source_dataset.size(0)
 
-    def get_data(self, indicate, dataset, source: bool = True):
+    def get_data(self):
         result = []
         for t in indicate:
             data = dataset[t]

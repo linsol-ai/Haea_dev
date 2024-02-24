3d93c5581cbfb07ed40a06325c3956d1231a640f
wooj1nBot <1403jung@gmail.com>
2024-02-24 15:43:41 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -14,7 +14,7 @@ class CustomDataset(Dataset):
 
     def get_data(self, t):
         range = min(t + self.max_lead_time, self.source_dataset.size(0))
-        sample = (self.max_lead_time - t) * torch.rand(1,)
+        sample = (self.max_lead_time) * torch.rand(1,)
 
         return result
 

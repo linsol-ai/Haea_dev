1b90cafba068bda56a7e52eb8e8444bda690a136
wooj1nBot <1403jung@gmail.com>
2024-02-24 17:06:33 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -59,6 +59,7 @@ class TrainModule(pl.LightningModule):
     def calculate_rmse_loss(self, predict: torch.Tensor, label: torch.Tensor):
         # target.size = (batch, var_len, hidden)
         label = denormalize(label, self.mean_std)
+               print(label.shape)
         # reversed_predict.shape = (batch, var_len, hidden) -> nomalized
         reversed_predict = denormalize(predict, self.mean_std)
         loss = rmse_loss(reversed_predict, label)

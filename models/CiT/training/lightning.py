5b86d2541ed4a4c47f1944422d47d5b6769d3d19
wooj1nBot <1403jung@gmail.com>
2024-02-24 17:06:31 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -61,8 +61,6 @@ class TrainModule(pl.LightningModule):
         label = denormalize(label, self.mean_std)
         # reversed_predict.shape = (batch, var_len, hidden) -> nomalized
         reversed_predict = denormalize(predict, self.mean_std)
-        print(label.shape)
-
         loss = rmse_loss(reversed_predict, label)
         return loss
 

e5d7dd0fb390a23986b987ba866dee879b893c00
wooj1nBot <1403jung@gmail.com>
2024-02-24 16:36:14 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -14,7 +14,6 @@ class CustomDataset(Dataset):
 
     def get_data(self, t):
         next = min(t + self.max_lead_time, self.source_dataset.size(0)-1)
-        delta = next-t
         sample = torch.randint(t, next, (1,))
         return self.source_dataset[t], self.label_dataset[sample], delta
 

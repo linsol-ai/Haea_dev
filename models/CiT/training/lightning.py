97759515a4db7d4b1c7ee05789671f1981297f1c
wooj1nBot <1403jung@gmail.com>
2024-02-24 16:18:44 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -83,7 +83,6 @@ class TrainModule(pl.LightningModule):
         return self._step(batch, "train")
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:  # noqa: D102
-
         return self._step(batch, "val")
 
     def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], _: int) -> None:  # noqa: D102

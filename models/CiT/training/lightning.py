94ec26e7fafb085b180b871f10c4683e3019ac5e
wooj1nBot <1403jung@gmail.com>
2024-02-24 16:20:23 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -100,7 +100,7 @@ class TrainModule(pl.LightningModule):
         label = denormalize(label, self.mean_std)
         predict = denormalize(predict, self.mean_std)
         self.mean_std.cpu().detach()
-        
+        src
         return predict, label
     
 

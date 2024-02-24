0d57df8382e9af5f243ad6ef8629de80601c61f5
wooj1nBot <1403jung@gmail.com>
2024-02-24 15:59:56 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from torch.optim import Adam
 import torch.nn.functional as F
 from models.CiT.models.model import ClimateTransformer
-from models.VariableEncoder.training.configs import TrainingConfig
+from models.CiT.training.configs import TrainingConfig
 from models.VariableEncoder.training.params_schedule import CosineWarmupScheduler
 import wandb
 

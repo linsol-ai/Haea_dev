bfa5137e10144bbfe10f0cfa72719e9c6d58a54f
wooj1nBot <1403jung@gmail.com>
2024-02-24 15:12:18 +0000

    Auto commit

--- models/CiT/models/model.py
+++ models/CiT/models/model.py
@@ -66,7 +66,7 @@ class ClimateTransformer(nn.Module):
             batch_first=True,
             activation=F.gelu
         )
-        self.transformer = nn.TransformerEncoder(
+        self.transformer = nn.  (
             encoder_layers,
             n_layers
         )

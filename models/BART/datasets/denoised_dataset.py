351dae22ca1afc71c2f35515bf628875280c8f97
wooj1nBot <1403jung@gmail.com>
2024-02-24 08:17:16 +0000

    Auto commit

--- models/BART/datasets/denoised_dataset.py
+++ models/BART/datasets/denoised_dataset.py
@@ -38,7 +38,7 @@ class BARTDenoisingDataset(Dataset):
         self.insert_ratio = args.insert
         self.mask_length = args['mask_length']
         self.replace_length = args['replace_length']
-        
+        self.poisson_lambda = args['poisson_lambda']
 
         self.replace_length = self.replace_length
         if not self.replace_length in [-1, 0, 1]:

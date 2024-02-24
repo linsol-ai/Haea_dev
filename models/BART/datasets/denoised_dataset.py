5542a3ab8681be4828c825427e660895cdc01569
wooj1nBot <1403jung@gmail.com>
2024-02-24 08:16:00 +0000

    Auto commit

--- models/BART/datasets/denoised_dataset.py
+++ models/BART/datasets/denoised_dataset.py
@@ -36,6 +36,7 @@ class BARTDenoisingDataset(Dataset):
         self.mask_ratio = args.mask
         self.random_ratio = args.mask_random
         self.insert_ratio = args.insert
+        
 
         self.replace_length = args.replace_length
         if not self.replace_length in [-1, 0, 1]:

44d04ab5c37054ebc6c49944d615fda2d5364a09
wooj1nBot <1403jung@gmail.com>
2024-02-24 15:38:11 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -24,15 +24,6 @@ class CustomDataset(Dataset):
         result = torch.concat(result, dim=0)
         return result
 
-    def make_dataset(self):
-        dataset_inc = []
-        for t in range(self.source_dataset.size(0)):
-            src = t
-            tgt = 
-            dataset_inc.append((src, tgt))
-
-        self.dataset_inc = dataset_inc
-
     def __getitem__(self, item):
         src_ind, tgt_ind = self.dataset_inc[item]
         src = self.get_data(src_ind, self.source_dataset)

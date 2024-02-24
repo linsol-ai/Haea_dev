4d5fc1c2933354cbb042ab304e1e75cf66da2f4e
wooj1nBot <1403jung@gmail.com>
2024-02-24 09:45:39 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -78,7 +78,7 @@ class VariableEncoder(nn.Module):
     def init_seq(self, device, batch_size):
         print("init", device)
         self.tgt_mask = self.get_tgt_mask()
-        self.src_var_seq, self.tgt_var_seq = self.get_var_seq(self.batch_size, device)
+        self.src_var_seq, self.tgt_var_seq = self.get_var_seq(batch_size, device)
         self.tgt_pos_seq = self.positional_encoding(self.batch_size, self.in_dim, len(self.tgt_var_list), self.tgt_time_len, device)
 
 

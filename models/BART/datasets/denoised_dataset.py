067a7bb94c9c5bd0fa5d58bb76f774e336534d4e
wooj1nBot <1403jung@gmail.com>
2024-02-24 08:19:51 +0000

    Auto commit

--- models/BART/datasets/denoised_dataset.py
+++ models/BART/datasets/denoised_dataset.py
@@ -10,6 +10,21 @@ import math
 
 
 
+@contextlib.contextmanager
+def numpy_seed(seed, *addl_seeds):
+    """Context manager which seeds the NumPy PRNG with the specified seed and
+    restores the state afterward"""
+    if seed is None:
+        yield
+        return
+    if len(addl_seeds) > 0:
+        seed = int(hash((seed, *addl_seeds)) % 1e6)
+    state = np.random.get_state()
+    np.random.seed(seed)
+    try:
+        yield
+    finally:
+        np.random.set_state(state)
 
 
 class BARTDenoisingDataset(Dataset):

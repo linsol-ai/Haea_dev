bfb197cb15656ef044b8cdaa07e73aa26d5fc8f9
wooj1nBot <1403jung@gmail.com>
2024-02-25 15:34:08 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -102,7 +102,7 @@ class TrainModule(pl.LightningModule):
         # loss.shape = (batch, var_len, hidden)
         loss = F.mse_loss(predict, label, reduction='none')
         # loss.shape = (batch, var_len)
-        rmse_loss = rmse_loss.mean(dim=-1).cpu().detach()
+        loss = rmse_loss.mean(dim=-1).cpu().detach()
 
         self.mean_std.cpu().detach()
         src.cpu().detach()

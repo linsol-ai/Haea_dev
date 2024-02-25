930d76b1af7fd3da840cbd0542356ce632d313d9
wooj1nBot <1403jung@gmail.com>
2024-02-25 16:11:41 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -183,7 +183,7 @@ class WeatherDataset:
     
     def load_one(self, air_variable, surface_variable, only_input_variable=[], constant_variables=[]):
         variables = air_variable + surface_variable + only_input_variable
-        source_t, mean_std = self.load_data_single(self.datasets[0], variables, constant_variables)
+        source, mean_std = self.load_data_single(self.datasets[0], variables, constant_variables)
         
         offset = len(only_input_variable)
         if offset > 0:

28ca2dc817bb7a72f61df53f46f4d4f44c398851
wooj1nBot <1403jung@gmail.com>
2024-02-25 14:07:32 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -17,7 +17,8 @@ HAS_LEVEL_VARIABLE = [
 NONE_LEVEL_VARIABLE = [
   '2m_dewpoint_temperature', 
   'mean_surface_latent_heat_flux', 
-  'geopotential_at_surface', 'land_sea_mask', '2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'total_cloud_cover', 'total_precipitation'
+  'geopotential_at_surface', 
+  'land_sea_mask', '2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'total_cloud_cover', 'total_precipitation'
 ]
 
 VARIABLE = HAS_LEVEL_VARIABLE + NONE_LEVEL_VARIABLE

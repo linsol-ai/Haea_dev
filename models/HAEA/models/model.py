9f49e1615f36d9b0dc3ff3236804f6fd541988a7
wooj1nBot <1403jung@gmail.com>
2024-02-26 13:22:16 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -97,7 +97,7 @@ class Haea(nn.Module):
         # src.shape = (batch, 1, 99, 1450), tgt.shape = (batch, tgt_time_len, 99, 1450)
         if not hasattr(self, 'src_var_seq'):
             self.init_seq(src.device, src.size(0))
-        src, tgt = src.squeeze(1), tgt.view(tgt.size(0), -1, tgt.size(3))
+        src, tgt = ㄴ.view(tgt.size(0), -1, tgt.size(3)), tgt.view(tgt.size(0), -1, tgt.size(3))
         src = self.embedding(src, self.src_var_seq) * math.sqrt(self.in_dim)
         tgt = self.embedding(tgt, self.tgt_var_seq, self.tgt_pos_seq) * math.sqrt(self.in_dim)
         tgt_mask = self.tgt_mask.to(src.device)

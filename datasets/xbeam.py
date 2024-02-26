e3497421c5ead86a97d9396c941d8d24964bf2b4
wooj1nBot <1403jung@gmail.com>
2024-02-26 11:45:24 +0000

    Auto commit

--- datasets/xbeam.py
+++ datasets/xbeam.py
@@ -118,7 +118,7 @@ def main(argv):
     (
         root
         | xbeam.DatasetToChunks(source_dataset, source_chunks, split_vars=True)
-        | xbeam.SplitChunks()
+        | xbeam.SplitChunks(mid_chunks)
         | xbeam.Rechunk(  # pytype: disable=wrong-arg-types
             source_dataset.sizes,
             source_chunks,

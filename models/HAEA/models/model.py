2a5cef92b32be4789137648d108cdd03dff627d6
wooj1nBot <1403jung@gmail.com>
2024-02-26 14:19:13 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -94,6 +94,7 @@ class Haea(nn.Module):
         self.tgt_pos_seq = self.positional_encoding(batch_size, self.in_dim, len(self.tgt_var_list), self.time_len, device)
 
 
+
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):
         # src.shape = (batch, 1, 99, 1450), tgt.shape = (batch, tgt_time_len, 99, 1450)
         if not hasattr(self, 'src_var_seq'):

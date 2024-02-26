caac9dc656da5c7e565a6b67563e3bf5c6294818
wooj1nBot <1403jung@gmail.com>
2024-02-26 14:11:11 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -103,7 +103,7 @@ class Haea(nn.Module):
         tgt_mask = self.tgt_mask.to(src.device)
 
         x = self.encoder(src)
-        x = self.decoder(tgt, keys=x)
+        x = self.decoder(tgt, keys=x, )
 
         
         out = self.out(transformer_out)

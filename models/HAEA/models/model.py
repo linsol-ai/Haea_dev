41f0c455e52690079af430ecc436f8073bd6496c
wooj1nBot <1403jung@gmail.com>
2024-02-26 13:25:10 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -152,25 +152,6 @@ class Haea(nn.Module):
     
     
 
-    @torch.no_grad()
-    def get_attention_maps(self, x: torch.Tensor) -> torch.Tensor:
-        """Function for extracting the attention matrices of the whole Transformer for a single batch.
-
-        Input arguments same as the forward pass.
-        """
-        x = x.view(x.size(0), -1, x.size(3))
-        src_seq = torch.tensor([self.src_var_list for _ in range(x.size(0))], device=x.device)
-
-        x = self.embedding(x, src_seq) * math.sqrt(self.in_dim)
-
-        attention_maps = []
-        for layer in self.transformer.encoder.layers:
-            _, attn_map = layer.self_attn(query=x, key=x, value=x)
-            attention_maps.append(attn_map)
-            x = layer(x)
-
-        return torch.stack(attention_maps, dim=0)
-
     
 
 

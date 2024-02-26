2f39dd486d1a9eed78666de7649b2f17ef427e71
wooj1nBot <1403jung@gmail.com>
2024-02-26 12:45:19 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -51,7 +51,7 @@ class LinearDecoder(nn.Module):
        return self.seq(x)
 
 
-class HAEA(nn.Module):
+class Haea(nn.Module):
     def __init__(self, src_var_list: List[str], tgt_var_list: List[str], tgt_time_len: int, 
                 in_dim: int, out_dim: int, max_var_len=300, 
                 num_heads=12, n_encoder_layers=3, n_decoder_layers=3, dropout=0.1):

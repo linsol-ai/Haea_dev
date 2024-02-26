94bd43b22c2a60cc44f4a75ae8925efd97521fd4
wooj1nBot <1403jung@gmail.com>
2024-02-26 10:44:39 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -101,7 +101,7 @@ class WeatherDataset:
 
     AIR_VARIABLE = ['geopotential', 'specific_humidity', 'temperature', 'u_component_of_wind', 'v_component_of_wind', 'vertical_velocity']
 
-    SURFACE_VARIABLE = ['2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'total_cloud_cover', 'total_precipitation', 'toa_incident_solar_radiation', 'geopotential_at_surface', 'land_sea_mask']
+    SURFACE_VARIABLE = ['2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'total_cloud_cover', 'total_precipitation', ]
 
     VARIABLES = AIR_VARIABLE + SURFACE_VARIABLE
 

3280adb9d8d6e2f93e9984f2d7f25df571d348fb
wooj1nBot <1403jung@gmail.com>
2024-02-26 09:53:07 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 import math
-from reformer_pytorch import ReformerLM
+from reformer_pytorch import ReformerLM, Reformer
 
 class PositionalEmbedding(nn.Module):
 

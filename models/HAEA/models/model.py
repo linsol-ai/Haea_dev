54311fcc1d74a7f5bbbf8ae1d0d7142cbd773140
wooj1nBot <1403jung@gmail.com>
2024-02-26 12:51:06 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -66,7 +66,7 @@ class Haea(nn.Module):
             depth=n_encoder_layers,
             heads=num_heads,
             causal=False,
-            
+            acti
         )
         
         self.embedding = Embedding(max_var_len, in_dim, dropout)

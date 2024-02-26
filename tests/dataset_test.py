878e43fa171b0c26d2b01c1515be540da36fb55c
wooj1nBot <1403jung@gmail.com>
2024-02-26 09:42:29 +0000

    Auto commit

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -9,7 +9,6 @@ HAS_LEVEL_VARIABLE = [ 'geopotential', 'specific_humidity', 'temperature', 'u_co
 
 NONE_LEVEL_VARIABLE = ['2m_temperature', '10m_u_component_of_wind', '10m_v_component_of_wind', 'mean_sea_level_pressure', 'sea_surface_temperature', 'total_cloud_cover', 'total_precipitation_24hr', 'total_precipitation_6hr']
 
-
 # 파이프라인 실행
 if __name__ == '__main__':
 

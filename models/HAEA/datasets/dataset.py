6f66b74abbbc62c2383b6c7fedf94d97235edbf3
wooj1nBot <1403jung@gmail.com>
2024-02-26 14:50:51 +0000

    Auto commit

---
+++ models/HAEA/datasets/dataset.py
@@ -0,0 +1,44 @@
+from torch.utils.data import Dataset
+import torch
+
+class CustomDataset(Dataset):
+
+    def __init__(self, source_dataset: torch.Tensor, label_dataset: torch.Tensor, tgt_time_len: int, n_only_input: int = 0):
+        # dataset.shape = (time, 1, var_len, hidden)
+        self.source_dataset = source_dataset.unsqueeze(1)
+        self.label_dataset = label_dataset.unsqueeze(1)
+        self.tgt_time_len = tgt_time_len
+        self.n_only_input = n_only_input
+        self.make_dataset()
+
+    def __len__(self):
+        return self.source_dataset.size(0)-(self.tgt_time_len)
+
+    def get_data(self, indicate, dataset, source: bool = True):
+        result = []
+        for t in indicate:
+            data = dataset[t]
+            if not source:
+                data = data[:, :-self.n_only_input, :]
+            result.append(data)
+
+        # dataset.shape = (time_len, var, hidden)
+        result = torch.concat(result, dim=0)
+        return result
+
+    def make_dataset(self):
+        dataset_inc = []
+        for t in range(self.source_dataset.size(0)-self.tgt_time_len):
+            src = [t]
+            tgt = range(t, t + self.tgt_time_len)
+            dataset_inc.append((src, tgt))
+
+        self.dataset_inc = dataset_inc
+
+    def __getitem__(self, item):
+        src_ind, tgt_ind = self.dataset_inc[item]
+        src = self.get_data(src_ind, self.source_dataset)
+        tgt = self.get_data(tgt_ind, self.source_dataset, source=False)
+        label = self.get_data(tgt_ind, self.label_dataset, source=False)
+        return src, tgt, label
+

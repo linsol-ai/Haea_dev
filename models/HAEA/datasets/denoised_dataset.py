ea4f9aed3d36e51d1b0299e1377b62cdcc1444bf
wooj1nBot <1403jung@gmail.com>
2024-02-27 03:34:22 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -32,15 +32,6 @@ class HaeaVocab:
         result = torch.concat(result, dim=0)
         return result
 
-    def make_dataset(self):
-        dataset_inc = []
-        for t in range(self.source_dataset.size(0)-self.tgt_time_len):
-            src = [t]
-            tgt = range(t, t + self.tgt_time_len)
-            dataset_inc.append((src, tgt))
-
-        self.dataset = dataset_inc
-
     def __getitem__(self, item):
         src_ind, tgt_ind = self.dataset_inc[item]
         src = self.get_data(src_ind, self.source_dataset)

3ba2b4e78b133f27ed457c6f574dcb707b642052
wooj1nBot <1403jung@gmail.com>
2024-02-27 03:51:18 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -37,7 +37,7 @@ class HaeaVocab:
 
         return result
 
-    def get(self, items):
+    def get(self, time):
         src = self.get_data(items, self.source_dataset)
         tgt = self.get_data(items, self.label_dataset, source=False)
         return src, tgt

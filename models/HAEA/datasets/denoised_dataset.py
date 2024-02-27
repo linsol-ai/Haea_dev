8ac754d21eb5f407267223ee9e0f18660951de53
wooj1nBot <1403jung@gmail.com>
2024-02-27 05:11:21 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -144,7 +144,7 @@ class BARTDenoisingDataset(Dataset):
             start = i
             end = i + length
             dataset[i] = torch.arange(start, end, step=1)
-        self.
+        self.dataset = dataset
 
 
     def __len__(self):

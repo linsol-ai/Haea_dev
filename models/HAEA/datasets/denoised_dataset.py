20e37ebcf250284c6a5178fc91cc663d4cac8e96
wooj1nBot <1403jung@gmail.com>
2024-02-27 04:36:42 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -11,7 +11,7 @@ import contextlib
 
 class HaeaVocab:
 
-    SPECIAL_
+    SPECIAL_TOKEN = [d]
 
     def __init__(self, source_dataset: torch.Tensor, label_dataset: torch.Tensor, n_only_input: int = 0):
         # dataset.shape = (time, var_len, hidden)

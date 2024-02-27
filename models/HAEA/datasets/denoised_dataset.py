9a3672c68b9996ddee793431b9480f9251241398
wooj1nBot <1403jung@gmail.com>
2024-02-27 04:45:36 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -40,7 +40,7 @@ class HaeaVocab:
         return result
 
     def get(self, times: torch.Tensor):
-        times = times - len(self.S)
+        times = times - len(self.SP)
         src = self.get_data(times, self.source_dataset)
         tgt = self.get_data(times, self.label_dataset, source=False)
         return src, tgt

8b7a913e5c44812807cc99a2e1448f13b8724d67
wooj1nBot <1403jung@gmail.com>
2024-02-27 04:32:14 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -133,7 +133,7 @@ class BARTDenoisingDataset(Dataset):
             'target': target,
         }
     
-    def make_dataset()
+    def make_dataset(self, )
 
     def __len__(self):
         return len(self.dataset)

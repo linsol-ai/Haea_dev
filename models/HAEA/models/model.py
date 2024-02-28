51f74b14bfc0be968e7df5c70e42c9323618b808
wooj1nBot <1403jung@gmail.com>
2024-02-28 08:28:41 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -113,7 +113,7 @@ class Haea(nn.Module):
         for batch in indicate:
             seq =  []
             for item in batch:
-                if indicate[i, j]
+                if item 
 
             src_seq.append(s_seq)
             tgt_seq.append(t_seq)

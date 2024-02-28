a69a72b97fa147ada0fdef99fa5968a1d9d69f4e
wooj1nBot <1403jung@gmail.com>
2024-02-28 04:47:27 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -52,7 +52,7 @@ class HaeaVocab:
                 result.append(torch.zeros(dataset.size(1), dataset.size(2)))
             else:
                 if not source:
-                    data = dataset[pos, :-self.n_only_input, :] + self.pe[i].repeat_interleave
+                    data = dataset[pos, :-self.n_only_input, :] + self.pe[i].repeat_interleave(dim=0)
                     result.append(dataset[pos, :-self.n_only_input, :])
                 else:
                     result.append(dataset[pos, :, :])

f98499126edf7159f9b991625dab1f02331fef13
wooj1nBot <1403jung@gmail.com>
2024-02-28 15:09:01 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -101,7 +101,7 @@ class Haea(nn.Module):
         src_seq = []
 
         # indicate.shape = (batch, max_len + 2)
-        var_seq = torch.em()
+        var_seq = torch.emp()
 
         for batch in indicate:
             seq = []

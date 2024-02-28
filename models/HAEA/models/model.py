8dad3685f7efb7f0c02962b205b47fcda71a5064
wooj1nBot <1403jung@gmail.com>
2024-02-28 16:12:33 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -88,7 +88,7 @@ class Haea(nn.Module):
         tgt_var_seq = self.get_var_seq(self.vocab.tgt_var_list, tgt_id, src.device)
 
         src, tgt = src.view(src.size(0), -1, src.size(3)), tgt.view(tgt.size(0), -1, tgt.size(3))
-        src = self.embedding(src, self.src_var_seq) * math.sqrt(self.in_dim)
+        src = self.embedding(src, src_var_seq) * math.sqrt(self.in_dim)
         tgt = self.embedding(tgt, self.tgt_var_seq) * math.sqrt(self.in_dim)
 
         x = self.encoder(src)

2e27479d19d5a6ba0762e294b3881edcde48229e
wooj1nBot <1403jung@gmail.com>
2024-02-28 08:27:02 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -115,8 +115,7 @@ class Haea(nn.Module):
             t_seq = []
 
             for _ in range(indicate.size(1)):
-                s_seq.extend(self.src_var_list)
-                t_seq.extend(self.tgt_var_list)
+                
 
             src_seq.append(s_seq)
             tgt_seq.append(t_seq)

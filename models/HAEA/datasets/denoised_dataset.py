aa3dead3433d862ae826f13f5907b21875dc1e44
wooj1nBot <1403jung@gmail.com>
2024-02-28 08:36:24 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -23,7 +23,7 @@ class HaeaVocab:
                   max_len, n_only_input: int = 0):
         # dataset.shape = (time, var_len, hidden)
         self.source_dataset = source_dataset
-        self.label_dataset = label_dataset
+        self.target_dataset = target_dataset
         self.src_var_list = src_var_list
         self.
         self.n_only_input = n_only_input

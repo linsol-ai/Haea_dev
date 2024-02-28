79e3d5b9645ae03669b8adadf471ea6e9223fb1d
wooj1nBot <1403jung@gmail.com>
2024-02-28 16:11:50 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -96,7 +96,7 @@ class Haea(nn.Module):
         return out
 
 
-    def get_var_seq(self, var_seq: torch.Tensor, indicate: torch.Tensor, device):
+    def get_var_seq(self, var_list: torch.Tensor, indicate: torch.Tensor, device):
         # indicate.shape = (batch, max_len + 2)
         result = []
         var_seq = var_seq + len(HaeaVocab.SPECIAL_TOKENS)

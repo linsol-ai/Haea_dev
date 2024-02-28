99aa05216ed6169c3989b294efdb9fd53db747dc
wooj1nBot <1403jung@gmail.com>
2024-02-28 14:38:29 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -87,7 +87,7 @@ class HaeaVocab:
         for i in range(self.max_len):
             s =  i * var_len
             e =  (i+1) * var_len
-            matrix[:, s:e, 0]
+            matrix[:, s:e, s:e] = 
 
         return matrix.bool()
 

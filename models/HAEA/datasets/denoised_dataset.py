999e7d779982452711730d334f6872bf204809d0
wooj1nBot <1403jung@gmail.com>
2024-02-28 14:51:08 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -28,7 +28,7 @@ class HaeaVocab:
         self.n_only_input = n_only_input
         self.pe = self.positional_encoding(dataset.size(-1), max_len + 10)
         self.max_len = max_len
-        self.tgt_mask = self.get_tgt_mask(len(tgt_var_list), )
+        self.tgt_mask = self.get_tgt_mask(len(tgt_var_list), max_len)
 
 
     def __len__(self):

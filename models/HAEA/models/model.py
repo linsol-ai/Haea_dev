ffcf2a9ef62881a77c8647c80e6e294ec10c010f
wooj1nBot <1403jung@gmail.com>
2024-02-28 15:07:42 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -107,7 +107,7 @@ class Haea(nn.Module):
             for item in batch:
                 if item in HaeaVocab.SPECIAL_TOKENS:
                     if item == HaeaVocab.SPECIAL_TOKEN_MASK:
-                        seq.extend([item for _ in range])
+                        seq.extend([item for _ in range()])
 
             src_seq.append(s_seq)
             tgt_seq.append(t_seq)

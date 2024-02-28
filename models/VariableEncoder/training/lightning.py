0389d464b22603b212e70e97728ec6da3f7bf888
wooj1nBot <1403jung@gmail.com>
2024-02-28 06:58:46 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -177,16 +177,8 @@ class TrainModule(pl.LightningModule):
         self.mean_std = self.mean_std.to(self.device)
         src = batch[0].to(self.device)
         tgt = batch[1].to(self.device)
-        var_len = tgt.size(2)
-        self.model.init_seq(self.device, src.size(0))
-        predict = self.model(src, tgt)
-        predict = predict.view(predict.size(0), -1, var_len, predict.size(2))
-        # predict.shape = (batch, time_len, var_len, hidden) -> not nomalized
-        reversed_predict = denormalize(predict, self.mean_std).cpu().detach()
-        label = denormalize(label, self.mean_std).cpu().detach()
-        predict.cpu().detach()
-        src.cpu().detach()
-        tgt.cpu().detach()
+        
+        
         return reversed_predict, label
     
 

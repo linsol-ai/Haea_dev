81d3ebc77f977d16dc6bb8e65fe8ef9365b0529d
wooj1nBot <1403jung@gmail.com>
2024-02-28 04:50:43 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -50,7 +50,7 @@ class HaeaVocab:
             pos = t - len(self.SPECIAL_TOKENS)
             if t in self.SPECIAL_TOKENS:
                 data = torch.zeros(dataset.size(1), dataset.size(2))
-                
+                data = data + self.pe[i].repeat_interleave(data.size(0), dim=0)
             else:
                 if not source:
                     data = dataset[pos, :-self.n_only_input, :]

543623a03d2724e088f0f20b4e59848bea37b815
wooj1nBot <1403jung@gmail.com>
2024-02-29 17:48:15 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -30,6 +30,7 @@ def get_normal_dataset(config: TrainingConfig):
     # dataset.shape:  torch.Size([7309, 100, 1450])
     source, mean_std = weather.load_one(config.air_variable, config.surface_variable, config.only_input_variable, 
                                         config.constant_variable, config.train_start, config.train_end)
+    
     dataset = CustomDataset(source, tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))
     src_var_list = weather.get_var_code(config.air_variable, config.surface_variable + config.only_input_variable+config.constant_variable)
     tgt_var_list = weather.get_var_code(config.air_variable, config.surface_variable)

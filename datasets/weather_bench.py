02428bdb1f76e568122d724061206c884c04a19a
wooj1nBot <1403jung@gmail.com>
2024-02-29 17:38:08 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -143,7 +143,7 @@ class WeatherDataset:
 
     def check_dataset(self, start_date: datetime, end_date: datetime):
         for offset in self.DATE_OFFSET:
-            start = datetime.strptime(f'{start}-01-01', '%Y-%m-%d')
+            start = datetime.strptime(f'{offset[0]}-01-01', '%Y-%m-%d')
 
         file_name = f'{start}-01-01_{end}-01-01.zarr'
         dataset_path = []

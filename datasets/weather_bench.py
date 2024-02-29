cefa159e2a382041afc195c143cadafa3536c2fd
wooj1nBot <1403jung@gmail.com>
2024-02-29 17:46:14 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -343,5 +343,5 @@ if __name__ == '__main__':
     start = datetime.strptime('2013-01-01', '%Y-%m-%d')
     end = datetime.strptime('2014-01-01', '%Y-%m-%d')
     
-    weather = WeatherDataset(0, device=device, offline=True)
+    weather = WeatherDataset(start, end, device=device, offline=True)
     print(weather.load_one(weather.AIR_VARIABLE, weather.SURFACE_VARIABLE)[0].shape)

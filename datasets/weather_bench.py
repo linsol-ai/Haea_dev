f1b7ac0f8e25ebc503c0e0c017192ac115304cc4
wooj1nBot <1403jung@gmail.com>
2024-02-29 17:39:04 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -145,7 +145,7 @@ class WeatherDataset:
         for offset in self.DATE_OFFSET:
             start = datetime.strptime(f'{offset[0]}-01-01', '%Y-%m-%d')
             end = datetime.strptime(f'{offset[1]}-01-01', '%Y-%m-%d')
-            if start <= start_date <= end and 
+            if start <= start_date and 
 
         file_name = f'{start}-01-01_{end}-01-01.zarr'
         dataset_path = []

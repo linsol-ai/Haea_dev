b06cf3a7e8d8acf710794cfbb6e259762a5e087d
wooj1nBot <1403jung@gmail.com>
2024-03-01 15:01:21 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -31,7 +31,8 @@ def get_normal_dataset(config: TrainingConfig):
     source, mean_std, var_vocab = weather.load_one(config.air_variable, config.surface_variable, config.only_input_variable, 
                                         config.constant_variable)
     src_var_list = var_vocab.get_code(config.air_variable + config.surface_variable + config.only_input_variable + config.constant_variable)
-    
+        src_var_list = var_vocab.get_code(config.air_variable + config.surface_variable + config.only_input_variable + config.constant_variable)
+
     time_vocab = TimeVocab(source, )
     
     dataset = CustomDataset(source, tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))

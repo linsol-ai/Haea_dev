aa3d5dc3e564e18f0f976810ffde3f3b6c93dcfb
wooj1nBot <1403jung@gmail.com>
2024-03-01 16:03:11 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -91,7 +91,7 @@ class Haea(nn.Module):
         print(src.shape)
         print(src_id.shape)
         print(tgt.shape)
-        print(src_id.shape)
+        print(tgt_id.shape)
 
         src, tgt = src.view(src.size(0), -1, src.size(3)), tgt.view(tgt.size(0), -1, tgt.size(3))
         src = self.embedding(src, src_var_seq) * math.sqrt(self.in_dim)

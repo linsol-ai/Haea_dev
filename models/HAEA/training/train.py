789dfe54085186d98e70aa841b4e12afe5985d7b
wooj1nBot <1403jung@gmail.com>
2024-03-01 15:19:38 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -22,7 +22,7 @@ from models.HAEA.training.configs import TrainingRunConfig
 from models.HAEA.training.lightning import TrainModule
 
 
-def get_normal_dataset(config: TrainingConfig) -> Tuple[]:
+def get_normal_dataset(config: TrainingConfig) -> Tuple[DenoisingDataset, ]:
     time_len = 1 * config.time_len
     device = ("cuda" if torch.cuda.is_available() else "cpu" )
     device = torch.device(device)

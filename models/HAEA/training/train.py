8d99b4bd0e850c7ca083bcefc10a84ef22397794
wooj1nBot <1403jung@gmail.com>
2024-03-01 14:16:36 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -13,7 +13,7 @@ from pytorch_lightning.utilities.model_summary import ModelSummary
 import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset
-from models.HAEA.datasets.denoised_dataset import DenoisingDataset
+from models.HAEA.datasets.denoised_dataset import DenoisingDataset, TimeVocab
 from models.HAEA.models.model import Haea
 
 from models.HAEA.training.configs import TrainingConfig

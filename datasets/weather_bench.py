745974182f578c176c728ce3f2dbb2de7604258c
wooj1nBot <1403jung@gmail.com>
2024-03-01 13:49:00 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -112,7 +112,7 @@ class ERA5Vocab:
             vocab[var] = level_list
         
         for i, var in enumerate(self.surface_vars):
-            
+            vocab[var] = []
         
 
         

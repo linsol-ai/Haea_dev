646c73136596b004f4b602b0fe8face59886e36a
wooj1nBot <1403jung@gmail.com>
2024-03-01 05:25:41 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -99,7 +99,7 @@ def download_zarr(source, output_path, variables=None):
 
 class ERA5Vocab:
     def __init__(self, dataset: xr.Dataset, src_variables, tgt_variables):
-        self.
+        self.dataset = dataset
         
 
 class WeatherDataset:

6ff459c5fd52d4bcbd8371b16b416c7e643d1f48
wooj1nBot <1403jung@gmail.com>
2024-03-01 15:14:52 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -99,7 +99,7 @@ data_module = DataModule(config.training)
 var_list = data_module.var_list
 dataset = data_module.dataset
 mean_std = data_module.mean_std
-max_iters = config.training.max_epochs*(dataset.source_dataset.size(0) // config.training.batch_size)
+max_iters = config.training.max_epochs*(dataset.dataset.size(0) // config.training.batch_size)
 print(f"max_iters: {max_iters}")
 
 logger = WandbLogger(save_dir=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'tb_logs'), name="my_model")

92c08c4a2ad001917fb8c269ff94ae73d17af1f7
wooj1nBot <1403jung@gmail.com>
2024-03-01 15:24:07 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -106,7 +106,7 @@ logger = WandbLogger(save_dir=os.path.join(os.path.dirname(os.path.abspath(os.pa
 model = Haea(
     data_module.time_vocab,
     in_dim=dataset.dataset.size(-1),
-    out_dim=dataset.source_dataset.size(-1),
+    out_dim=dataset.dataset.size(-1),
     num_heads=config.model.num_heads,
     n_encoder_layers=config.model.n_encoder_layers,
     n_decoder_layers=config.model.n_decoder_layers,

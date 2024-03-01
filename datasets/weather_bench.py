2d6249759965b06c3a7e4a930ab392f5410cdb82
wooj1nBot <1403jung@gmail.com>
2024-03-01 05:42:44 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -106,7 +106,7 @@ class ERA5Vocab:
     
     def create_vocab(self):
         for var in self.air_vars:
-            level_list = [ f"{var}_{}" in self.level]
+            level_list = [ f"{var}_{}" for lv in self.level]
         
 
 class WeatherDataset:

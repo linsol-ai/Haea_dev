3f329a4517c9574398895bb629e921aada357c34
wooj1nBot <1403jung@gmail.com>
2024-03-01 14:03:03 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -199,7 +199,7 @@ class WeatherDataset:
         if len(level) > 0:
             dataset = dataset.sel(level=level)
 
-        vocab = ERA5Vocab(dataset=dataset)
+        vocab = ERA5Vocab(dataset)
 
         source, mean_std = self.load_data_single(self.datasets[0], variables, constant_variables)
         

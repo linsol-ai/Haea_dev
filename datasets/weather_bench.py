cb385ddebd55aba5610926c697804c842b315f8f
wooj1nBot <1403jung@gmail.com>
2024-03-01 13:48:30 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -111,7 +111,7 @@ class ERA5Vocab:
             level_list = [((idx-1) * self.level) + k for k in range(self.level)]
             vocab[var] = level_list
         
-        for i, var in 
+        for i, var in enumerate(self.air_vars):
         
 
         

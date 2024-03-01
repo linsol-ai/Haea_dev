3e8ee1d635b3068f8e91c9ac983aa9eae2219adb
wooj1nBot <1403jung@gmail.com>
2024-03-01 04:05:20 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -198,7 +198,7 @@ class WeatherDataset:
             idx = keys.index(i)
             var = dataset[i]
             if 'level' in var.dims:
-                result.extend([k for ])
+                result.extend([k for k in range()])
 
 
 

333951dd09d5e96effeda056d9255721d80a48be
wooj1nBot <1403jung@gmail.com>
2024-03-01 15:31:51 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -127,7 +127,7 @@ class VariableVocab:
     def __len__(self):
         cnt = 0
         for key in self.vocab.keys():
-            cnt += len(self.vocab[key]
+            cnt += len(self.vocab[key])
 
         
 

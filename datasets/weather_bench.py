b3e412d0306d509aadd051b22a87b80600252333
wooj1nBot <1403jung@gmail.com>
2024-03-01 05:35:22 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -105,7 +105,7 @@ class ERA5Vocab:
         self.surface_vars = [var_name for var_name, var in dataset.variables.items() if 'level' not in var.dims]
     
     def create_vocab(self):
-        for vars in self.air_vars:
+        for var in self.air_vars:
             
         
 

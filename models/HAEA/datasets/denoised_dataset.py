8f6575760f9ef98979de0e22884b47e98a454a35
wooj1nBot <1403jung@gmail.com>
2024-03-02 14:58:30 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -94,9 +94,9 @@ class TimeVocab:
         matrix[(max_len-2)*var_len+1, :(max_len-2)*var_len+2] = True
         return matrix
 
-    def get_mask(self, var_len, max_len, pad_len):
+    def get_mask(var_len, max_len, pad_len):
         matrix = torch.zeros(((max_len-2)*var_len)+2+pad_len, dtype=torch.bool)
-        matrix[:(max_len-2)*var_len] = True
+        matrix[:(max_len-2)*var_len + 2] = True
         return matrix
 
 

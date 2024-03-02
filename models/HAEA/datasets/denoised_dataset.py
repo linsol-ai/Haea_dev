980d031a75392d04497d6814c6dc500b6d4fe704
wooj1nBot <1403jung@gmail.com>
2024-03-02 16:18:45 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -20,7 +20,7 @@ class TimeVocab:
         self.src_var_list = src_var_list
         self.tgt_var_list = tgt_var_list
         self.time_len = time_len
-        self.pad_len = self.get_max_len(bucket_size, max_len) - max_len 
+        self.src_max_len, self. = self.get_max_len(bucket_size, max_len) - max_len 
         self.pe = self.positional_encoding(dataset.size(-1), time_len + 10)
         self.tgt_mask = self.get_tgt_mask(len(tgt_var_list), self.max_len, self.pad_len)
         self.mask = self.get_mask(len(src_var_list), self.max_len, self.pad_len)

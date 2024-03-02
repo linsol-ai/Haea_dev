387652f99644bbccaeccc46a092443dffbafa8fb
wooj1nBot <1403jung@gmail.com>
2024-03-02 17:59:16 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -79,7 +79,7 @@ class TimeVocab:
             padding = torch.zeros(self.tgt_pad, self.dataset.size(2))
         
         print(padding.shape)
-        padding = padding + self.pe[len(indicate)].repeat_interleave(padding.size(0), dim=0)
+        padding = padding + self.pe[len(indicate)].unsqueeze(dim=0).repeat_interleave(padding.size(0), dim=0)
                 
         result = torch.cat(result, dim=0)
         result = torch.cat([result, padding], dim=0)

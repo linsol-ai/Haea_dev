c9ab93641b3ea22ad739959bbf7a89f1949f5f40
wooj1nBot <1403jung@gmail.com>
2024-03-02 19:36:52 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -119,7 +119,7 @@ class Haea(nn.Module):
                 
             result.append(seq)
 
-        result.extend([T])
+        result.extend([Time])
         result = torch.tensor(result, device=device)
         return result
     

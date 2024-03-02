df2b380cdbc039081e410240f0889377e1003840
wooj1nBot <1403jung@gmail.com>
2024-03-02 14:53:06 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -9,7 +9,7 @@ from models.HAEA.datasets.denoised_dataset import TimeVocab
 
 class VariableEmbedding(nn.Embedding):
     def __init__(self, var_len, embed_size=768):
-        super().__init__(var_len, embed_size)
+        super().__init__(var_len, embed_size, padding_idx=)
 
 
 class Embedding(nn.Module):

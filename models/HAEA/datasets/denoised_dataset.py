3ea6c07af5dc9bf21c55072964837b2e2c1a908a
wooj1nBot <1403jung@gmail.com>
2024-03-02 12:51:21 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -14,7 +14,7 @@ class TimeVocab:
 
 
     def __init__(self, dataset: torch.Tensor, 
-                 src_var_list: torch.Tensor, tgt_var_list: torch.Tensor, bumax_len: int):
+                 src_var_list: torch.Tensor, tgt_var_list: torch.Tensor,  bucket_size: int, max_len: int):
         # dataset.shape = (time, var_len, hidden)
         self.dataset = dataset
         self.src_var_list = src_var_list

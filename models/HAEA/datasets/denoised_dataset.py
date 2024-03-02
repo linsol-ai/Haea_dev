ba60594309202df596353f269c7b9257c5cbeec3
wooj1nBot <1403jung@gmail.com>
2024-03-02 18:53:06 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -95,7 +95,7 @@ class TimeVocab:
     
     def get_tgt_mask(self, var_len, max_len, pad_len) -> torch.Tensor:
         print()
-        matrix = torch.zeros(((max_len-2)*var_len)+2+pad_len, ((max_len-2)*var_len)+2+pad_len, dtype=torch.bool)
+        matrix = torch.zeros(( (max_len-2)*var_len)+2+pad_len, ((max_len-2)*var_len)+2+pad_len, dtype=torch.bool)
         matrix[:(max_len-2)*var_len, 0] = True
 
         for i in range(max_len-2):

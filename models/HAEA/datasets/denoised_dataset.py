d5f18bfd16cf7e3c01684e9fc328dcd473705fe8
wooj1nBot <1403jung@gmail.com>
2024-03-02 13:04:35 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -190,7 +190,7 @@ class DenoisingDataset(Dataset):
         self.epoch = 0
     
 
-    def make_dataset(self, size, length):
+    def make_dataset(self, size, padd, length):
         dataset = []
         
         for i in range(len(self.vocab.SPECIAL_TOKENS), size-length+2):

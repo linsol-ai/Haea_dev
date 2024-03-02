16a0fdeb09677939fae6ce89c64e72781b66b89d
wooj1nBot <1403jung@gmail.com>
2024-03-02 19:48:58 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -93,7 +93,7 @@ class Haea(nn.Module):
 
         src = self.embedding(src, src_var_seq) * math.sqrt(self.in_dim)
         tgt = self.embedding(tgt, tgt_var_seq) * math.sqrt(self.in_dim)
-        mask = self.mask[:src.size(0)]
+        mask = self.mask[:src.size(0)].to()
 
         x = self.encoder(src, input_mask=mask)
         x = self.decoder(tgt, keys=x, context_mask=mask, input_attn_mask=self.tgt_mask[:src.size(0)])

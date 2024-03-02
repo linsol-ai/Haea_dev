77ab56949f6353e8fa76db99ceaa19fef054cc52
wooj1nBot <1403jung@gmail.com>
2024-03-02 16:47:38 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -74,7 +74,7 @@ class TimeVocab:
             result.append(data)
         
         if source:
-            padding = torch.zeros(self.src_pad, )
+            padding = torch.zeros(self.src_pad, self.)
                 
         result = torch.cat(result, dim=0)
         return result

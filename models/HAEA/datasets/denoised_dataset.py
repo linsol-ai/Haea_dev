c2801905ccd29e374dc00048cbffa3ce1bc68756
wooj1nBot <1403jung@gmail.com>
2024-03-02 15:23:07 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -27,7 +27,6 @@ class TimeVocab:
     
 
     def get_max_len(self, bucket_size: int, var_len: int, max_len: int):
-        re_len = 
         if max_len % bucket_size == 0:
             return max_len
         else:

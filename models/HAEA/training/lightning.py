6d1d429e72d047d760f142a22da429a88aefc7d8
wooj1nBot <1403jung@gmail.com>
2024-03-03 12:12:59 +0000

    Auto commit

--- models/HAEA/training/lightning.py
+++ models/HAEA/training/lightning.py
@@ -53,7 +53,7 @@ class TrainModule(pl.LightningModule):
 
         predict = self.model(src, src_id, tgt, tgt_id)
 
-        if torch.isna:
+        if torch.isnan():
             print("p nan")
         
         if src.isnan():

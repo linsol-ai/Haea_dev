6ef27796b1727b7f1088ac08e63016599380e6c1
wooj1nBot <1403jung@gmail.com>
2024-03-03 14:56:18 +0000

    Auto commit

--- models/HAEA/training/lightning.py
+++ models/HAEA/training/lightning.py
@@ -54,7 +54,7 @@ class TrainModule(pl.LightningModule):
         predict = self.model(src, src_id, tgt, tgt_id)
 
         loss = self.calculate_rmse_loss(predict[:, 1:-1, :], tgt[:, 1:-1, :], len(self.model.vocab.tgt_var_list))
-        indices = torch.tensor([0, predict.shape[1]-1])
+        indices = torch.tensor([0, predict.size()-1])
         loss += rmse_loss(predict[:, 1:-1, :], tgt[:, 1:-1, :])
 
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")

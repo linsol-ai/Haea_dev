46d403eb69bfe7cd501bf83a9adff323a24652f1
wooj1nBot <1403jung@gmail.com>
2024-03-03 14:46:31 +0000

    Auto commit

--- models/HAEA/training/lightning.py
+++ models/HAEA/training/lightning.py
@@ -66,7 +66,7 @@ class TrainModule(pl.LightningModule):
         reversed_predict = denormalize(predict, self.mean_std)
         reversed_predict = reversed_predict.view(reversed_predict.size(0), -1, reversed_predict.size(3))
 
-        predict = predict.view(predict.size(0), -1, var_len, predict.size(2))
+        label = label.view(label.size(0), -1, var_len, label.size(2))
         label = denormalize(label, self.mean_std)
         label = label.view(label.size(0), -1, label.size(3))
 

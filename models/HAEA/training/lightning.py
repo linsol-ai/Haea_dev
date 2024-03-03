e97ab21440bef8dd587c39f3c8c59cc41d9d0f6d
wooj1nBot <1403jung@gmail.com>
2024-03-03 14:44:31 +0000

    Auto commit

--- models/HAEA/training/lightning.py
+++ models/HAEA/training/lightning.py
@@ -61,9 +61,6 @@ class TrainModule(pl.LightningModule):
     def calculate_rmse_loss(self, predict: torch.Tensor, label: torch.Tensor):
         # predict.size = (batch, time_len * var_len, hidden)
 
-
-
-
         var_len = label.size(2)
         label = denormalize(label, self.mean_std)
         label = label.view(label.size(0), -1, label.size(3))

5e95a21902b4457fb42bcabe2f87c674e2064870
wooj1nBot <1403jung@gmail.com>
2024-03-03 14:48:00 +0000

    Auto commit

--- models/HAEA/training/lightning.py
+++ models/HAEA/training/lightning.py
@@ -53,7 +53,7 @@ class TrainModule(pl.LightningModule):
 
         predict = self.model(src, src_id, tgt, tgt_id)
 
-        loss = self.calculate_rmse_loss(predict[:, 1:-1])
+        loss = self.calculate_rmse_loss(predict[:, 1:-1, ])
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")
         return loss
 

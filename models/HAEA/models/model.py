6a57f427ff11ed1517a5379874302e34506b9138
wooj1nBot <1403jung@gmail.com>
2024-03-04 04:49:45 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -70,7 +70,7 @@ class Haea(nn.Module):
         self.embedding = Embedding(max_var_len, in_dim, dropout)
         self.out = LinearDecoder(in_dim, out_dim, dropout=dropout)
 
-    def forward(self, src: torch.Tensor, src_id: torch.Tensor, tgt: torch.Tensor, tgt_id: torch.Tensor):
+    def forward(self, src: torch.Tensor, src_id: torch.Tensor, tgt: torch.Tensor, tgt_id: torch.Tensor, ):
     
         src_var_seq = self.get_var_seq(self.src_var_list, src_id, src.device)
         tgt_var_seq = self.get_var_seq(self.tgt_var_list, tgt_id, tgt.device)

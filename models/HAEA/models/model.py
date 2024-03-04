985d021c0add61969507f9da83ac55741b407d73
wooj1nBot <1403jung@gmail.com>
2024-03-04 05:21:41 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -95,7 +95,7 @@ class Haea(nn.Module):
                     if item == TimeVocab.SPECIAL_TOKEN_MASK:
                         seq.extend([item for _ in range(var_list.size(0))])
                     else:
-                        seq.append(item)
+                        seq.append(torch.tensor())
                 else:
                     seq.extend(var_list)
 

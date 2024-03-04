5b6a7b48e15ca1bfafef749d711fdf2ce821f65e
wooj1nBot <1403jung@gmail.com>
2024-03-04 04:12:02 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -51,6 +51,7 @@ class TrainModule(pl.LightningModule):
         tgt = batch[1]
         predict = self.model(src, tgt)
         
+
         loss = self.calculate_rmse_loss(predict, tgt)
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")
         return loss

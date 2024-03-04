872753b69886cb91553521852c743d8273a96e14
wooj1nBot <1403jung@gmail.com>
2024-03-04 04:19:24 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -37,10 +37,8 @@ def get_normal_dataset(config: TrainingConfig):
                                         config.constant_variable, level=config.levels)
     src_var_list = var_vocab.get_code(vars)
     tgt_var_list = var_vocab.get_code(config.air_variable + config.surface_variable)
-    
+
     dataset = CustomDataset(source, tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))
-    src_var_list = weather.get_var_code(config.air_variable, config.surface_variable + config.only_input_variable+config.constant_variable)
-    tgt_var_list = weather.get_var_code(config.air_variable, config.surface_variable)
     return dataset, mean_std, (src_var_list, tgt_var_list)
 
 

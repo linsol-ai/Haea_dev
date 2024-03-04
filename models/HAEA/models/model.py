01b6e8137e2174cce7860515983f70a90340fd46
wooj1nBot <1403jung@gmail.com>
2024-03-04 07:41:15 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -74,7 +74,7 @@ class Haea(nn.Module):
     def forward(self, src: torch.Tensor, src_id: torch.Tensor, tgt: torch.Tensor, tgt_id: torch.Tensor):
 
         self.src_var_list = self.src_var_list.to(src.device)
-        self.src_var_list = self.src_var_list.to(src.device)
+        self.tgt_var_list = self.src_var_list.to(src.device)
     
         src_var_seq = self.get_var_seq(self.src_var_list, src_id, src.device)
         tgt_var_seq = self.get_var_seq(self.tgt_var_list, tgt_id, tgt.device)

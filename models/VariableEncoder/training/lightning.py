89d2bffc5750375facc2a7ce2457d0d79bc8ffbf
wooj1nBot <1403jung@gmail.com>
2024-03-04 11:24:35 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -188,6 +188,7 @@ class TrainModule(pl.LightningModule):
         predict = predict.view(predict.size(0), -1, var_len, predict.size(2))
         predict = predict.swapaxes(1, 2)
         predict = torch.mean(predict, dim=-1)
+        
         predict = predict.swapaxes(0, 1)
 
         tgt = tgt.swapaxes(1, 2)

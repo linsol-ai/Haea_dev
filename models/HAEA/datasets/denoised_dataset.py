a342fb894513901762bcaeb2fa24ca4aca0954d8
wooj1nBot <1403jung@gmail.com>
2024-03-04 04:52:15 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -237,6 +237,7 @@ class DenoisingDataset(Dataset):
             "source_id": source,
             "target": target_dataset,
             "target_id": target,
+            
             "tgt_mask": self.vocab.tgt_mask,
             "src_var_list": self.vocab.src_var_list,
             "tgt_var_list": self.vocab.tgt_var_list

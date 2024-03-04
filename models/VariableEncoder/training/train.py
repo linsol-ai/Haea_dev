80da4e39d6287833b5cc0d2af9286039c3dcf1ea
wooj1nBot <1403jung@gmail.com>
2024-03-04 04:24:48 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -110,7 +110,7 @@ class DataModule(pl.LightningDataModule):
         return DataLoader(self.val_ds, batch_size=self.config.batch_size, drop_last=True, num_workers=2)
 
     def test_dataloader(self):
-        return DataLoader(self.test_ds, batch_size=config.training.batch_size, drop_last=True, num_workers=2)
+        return DataLoader(self.test_ds, batch_size=self.config.batch_size, drop_last=True, num_workers=2)
 
 
 

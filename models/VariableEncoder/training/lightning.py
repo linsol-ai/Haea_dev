36558bf9c5c812048e0632f65e2e696e2240341c
wooj1nBot <1403jung@gmail.com>
2024-03-04 11:33:08 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -169,6 +169,7 @@ class TrainModule(pl.LightningModule):
     
     def setting(self):
         self.mean_std = self.mean_std.to(self.device)
+        self.model.init_seq(self.dev
 
     def forward(self, batch) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         src = batch[0].to(self.device)

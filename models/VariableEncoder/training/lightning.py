736015f9424a7cc4b7444812f154e5eb2ca04f7d
wooj1nBot <1403jung@gmail.com>
2024-03-04 11:07:13 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -184,7 +184,6 @@ class TrainModule(pl.LightningModule):
         # loss.shape = (var_len, batch, time_len)
         loss = loss.swapaxes(0, 1)
 
-
         src.cpu().detach()
         tgt.cpu().detach()
         predict.cpu().detach()

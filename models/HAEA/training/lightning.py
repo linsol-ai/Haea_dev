294f87f478305a21e345837c513ccd76e4439b7d
wooj1nBot <1403jung@gmail.com>
2024-03-04 04:53:14 +0000

    Auto commit

--- models/HAEA/training/lightning.py
+++ models/HAEA/training/lightning.py
@@ -54,7 +54,7 @@ class TrainModule(pl.LightningModule):
         src_var_list = batch['src_var_list']
         tgt_var_list = batch['tgt_var_list']
 
-        predict = self.model(src, src_id, tgt, tgt_id, tgt)
+        predict = self.model(src, src_id, tgt, tgt_id, tgt_mask, src_var_list, tgt)
 
         loss = rmse_loss(predict, tgt)
 

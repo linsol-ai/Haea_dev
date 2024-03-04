b207511a8eb1119d6523cf1ca779b99f7ea863b7
wooj1nBot <1403jung@gmail.com>
2024-03-04 11:40:30 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -74,7 +74,7 @@ class VariableEncoder(nn.Module):
         self.out = LinearDecoder(in_dim, out_dim, dropout=dropout)
         self.tgt_mask = self.get_tgt_mask()
         self.src_var_seq, self.tgt_var_seq = self.get_var_seq(16)
-        self.tgt_pos_seq = self.positional_encoding(32, self.in_dim, len(self.tgt_var_list), self.tgt_time_len)
+        self.tgt_pos_seq = self.positional_encoding(16, self.in_dim, len(self.tgt_var_list), self.tgt_time_len)
     
 
     def forward(self, src: torch.Tensor, tgt: torch.Tensor):

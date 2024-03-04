cc2d4f9f02d7d754b083685c48b9390aff5e99ff
wooj1nBot <1403jung@gmail.com>
2024-03-04 08:08:19 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -106,7 +106,7 @@ class VariableEncoder(nn.Module):
             tgt_seq.append(t_seq)
         
         tgt_seq = torch.tensor(tgt_seq, device=device)
-        src_seq = self.src_var_list.unsqueeze(0).repeat_interleave()
+        src_seq = self.src_var_list.unsqueeze(0).repeat_interleave(data.size(0), dim=0)
         return src_seq, tgt_seq
 
 

ef8ed50c3a7f471f0ce0b395a8acf167b721e558
wooj1nBot <1403jung@gmail.com>
2024-03-05 13:12:47 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -66,6 +66,7 @@ class TrainModule(pl.LightningModule):
         print(stage)
         self.mean_std = self.mean_std.to(self.device)
         self.tgt_mask = self.tgt_mask.to(self.device)
+        
 
 
     def configure_optimizers(self) -> Adam:  # noqa: D102

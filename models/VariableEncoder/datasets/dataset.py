63bdf8921efbcedf3bb61c18d8e7218e85624e22
wooj1nBot <1403jung@gmail.com>
2024-03-05 12:07:07 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -13,6 +13,7 @@ class CustomDataset(Dataset):
         self.tgt_time_len = tgt_time_len
         self.n_only_input = n_only_input
         self.pe = self.positional_encoding(source_dataset.size(-1), tgt_time_len + 10)
+        self.b
         self.make_dataset()
 
     def __len__(self):

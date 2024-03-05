890065a573890743836f43aa501a7adfaf079114
wooj1nBot <1403jung@gmail.com>
2024-03-05 18:04:28 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -25,7 +25,6 @@ class CustomDataset(Dataset):
             if not source and self.n_only_input > 0:
                 data = data[:, :-self.n_only_input, :]
 
-            data = data + self.pe[i].unsqueeze(dim=0).repeat_interleave(data.size(0), dim=0)
             result.append(data)
 
         # result.shape = (time_len * var, hidden)

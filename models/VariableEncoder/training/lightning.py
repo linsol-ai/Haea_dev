301268b4f21307a9c4c4f2f6c4553726c5d22f7d
wooj1nBot <1403jung@gmail.com>
2024-03-05 10:48:58 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -63,7 +63,7 @@ class TrainModule(pl.LightningModule):
         self.src_var_list = src_var_list
         self.tgt_var_list = tgt_var_list
         self.save_hyperparameters()
-        self.tgt_mask = 
+        self.tgt_mask = get_tgt_mask()
 
 
     

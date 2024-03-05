d1a2131ce16dcd46f3a9a574c9757a59ce3c297d
wooj1nBot <1403jung@gmail.com>
2024-03-05 12:46:57 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -73,7 +73,6 @@ class VariableEncoder(nn.Module):
 
         transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=None, tgt_key_padding_mask=None)
         out = self.out(transformer_out)
-
         return out
 
     

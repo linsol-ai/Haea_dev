b0aa2405d5db84268f773a522592c8faaaf9e124
wooj1nBot <1403jung@gmail.com>
2024-03-05 19:10:38 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -78,7 +78,7 @@ class VariableEncoder(nn.Module):
 
 
     def positional_encoding(self, batch, d_model, var_len, time_len, device):
-        pe = torch.zeros(time_len, d_model).float()
+        pe = torch.zeros(batch, time_len, d_model).float()
         pe.require_grad = False
 
         position = torch.arange(0, time_len).float().unsqueeze(1)

f5dc4ef6faf3ef05c98821911c48e60f234f99d5
wooj1nBot <1403jung@gmail.com>
2024-03-05 14:09:41 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -16,7 +16,7 @@ class CustomDataset(Dataset):
         self.make_dataset()
 
     def __len__(self):
-        return self.source_dataset.size(0)-(self.tgt_time_len)
+        return self.source_dataset.size(0)-(self.tgt_time_len)-1
 
     def get_data(self, indicate, source: bool = True):
         result = []

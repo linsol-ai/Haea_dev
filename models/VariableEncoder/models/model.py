2967ac7f5c988eecf67be1fb0a56e5dca4a06b0b
wooj1nBot <1403jung@gmail.com>
2024-03-05 19:10:17 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -77,6 +77,19 @@ class VariableEncoder(nn.Module):
         return out
 
 
+    def positional_encoding(d_model, var_len, time_len):
+    pe = torch.zeros(time_len, d_model).float()
+    pe.require_grad = False
+
+    position = torch.arange(0, time_len).float().unsqueeze(1)
+    div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+    pe[:, 0::2] = torch.sin(position * div_term)
+    pe[:, 1::2] = torch.cos(position * div_term)
+
+    return pe.repeat_interleave(var_len, dim=0)
+
+
     @torch.no_grad()
     def get_attention_maps(self, x: torch.Tensor) -> torch.Tensor:
         """Function for extracting the attention matrices of the whole Transformer for a single batch.

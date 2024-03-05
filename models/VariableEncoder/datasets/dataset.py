2d7455bfc72c863a1b2f627b02396b6a9ca9802c
wooj1nBot <1403jung@gmail.com>
2024-03-05 17:56:04 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -23,8 +23,8 @@ class CustomDataset(Dataset):
         for i, t in enumerate(indicate):
             data = self.source_dataset[t-2]
             if not source and self.n_only_input > 0:
-                    data = data[:, :-self.n_only_input, :]
-
+                data = data[:, :-self.n_only_input, :]
+                
             data = data + self.pe[i].unsqueeze(dim=0).repeat_interleave(data.size(0), dim=0)
             result.append(data)
 

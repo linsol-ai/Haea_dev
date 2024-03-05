97f5b04664fe54fa41c7db511989b0ad11f52a69
wooj1nBot <1403jung@gmail.com>
2024-03-05 13:01:32 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -149,7 +149,7 @@ def main(argv):
 
     print("setting lr rate: ", config.training.learning_rate)
 
-    model_pl = TrainModule(model=model, mean_std=mean_std, max_iters=max_iters, 
+    model_pl = TrainModule(model=model, mean_std=mean_std, max_iters=max_iters, var_list
                             config=config.training)
     trainer = pl.Trainer(
         accelerator="auto",

f22483e303cc9a3bc237c53cebb76b83cb5b888f
wooj1nBot <1403jung@gmail.com>
2024-03-05 12:02:07 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -28,17 +28,7 @@ class CustomDataset(Dataset):
         result = torch.concat(result, dim=0)
         return result
 
-    def positional_encoding(batch, d_model, var_len, time_len):
-        pe = torch.zeros(batch, time_len, d_model).float()
-        pe.require_grad = False
-
-        position = torch.arange(0, time_len).float().unsqueeze(1)
-        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
-
-        pe[:, :, 0::2] = torch.sin(position * div_term)
-        pe[:, :, 1::2] = torch.cos(position * div_term)
-
-        return pe.repeat_interleave(var_len, dim=1)
+    
 
     def make_dataset(self):
         dataset_inc = []

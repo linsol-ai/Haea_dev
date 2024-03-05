1bddefe7ce05bacea38275ee60d0f601bd4442fe
wooj1nBot <1403jung@gmail.com>
2024-03-05 10:39:10 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -20,7 +20,7 @@ def rmse_loss(x, y):
     return torch.sqrt(F.mse_loss(x, y))
 
 
- def get_var_seq(self, batch_size):
+def get_var_seq(self, batch_size):
         tgt_seq = self.tgt_var_list.repeat_interleave(self.tgt_time_len, dim=0).unsqueeze(0).repeat_interleave(batch_size, dim=0)
         src_seq = self.src_var_list.unsqueeze(0).repeat_interleave(batch_size, dim=0)
         return src_seq, tgt_seq

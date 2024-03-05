47129b72375dde031ea6d1b18be44a24a6c57ba3
wooj1nBot <1403jung@gmail.com>
2024-03-05 19:04:44 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -22,7 +22,7 @@ def rmse_loss(x, y):
     return torch.sqrt(F.mse_loss(x, y))
 
 def positional_encoding(d_model, var_len, time_len):
-    pe = torch.zeros(batch, time_len, d_model).float()
+    pe = torch.zeros(time_len, d_model).float()
     pe.require_grad = False
 
     position = torch.arange(0, time_len).float().unsqueeze(1)

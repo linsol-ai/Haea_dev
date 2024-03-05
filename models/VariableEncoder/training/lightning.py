f6ba6a377f41d9b4d258894020b0c26d950f35da
wooj1nBot <1403jung@gmail.com>
2024-03-05 12:57:34 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -27,7 +27,8 @@ def get_var_seq(src_var_list: torch.Tensor, tgt_var_list: torch.Tensor, tgt_time
     tgt_seq = tgt_var_list.repeat_interleave(tgt_time_len, dim=0)
     tgt_seq = torch.cat([SPECIAL_TOKEN_BOS, tgt_seq, SPECIAL_TOKEN_EOS])
     tgt_seq = tgt_seq.unsqueeze(0).repeat_interleave(batch_size, dim=0)
-    src_seq = src_var_list.unsqueeze(0).repeat_interleave(batch_size, dim=0)
+    src_seq = src_var_list.unsqueeze(0)
+    src_seq = src_seq.repeat_interleave(batch_size, dim=0)
     return src_seq, tgt_seq
 
 

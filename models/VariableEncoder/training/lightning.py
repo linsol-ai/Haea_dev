7cc71a7dabbeda738eade473c0e162ffb7167828
wooj1nBot <1403jung@gmail.com>
2024-03-05 18:24:16 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -85,6 +85,7 @@ class TrainModule(pl.LightningModule):
 
     def _step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], mode: str) -> torch.Tensor:
         src = batch[0]
+        # (batch, time*var, hidden)
         label = batch[1]
         src_seq, tgt_seq = get_var_seq(self.src_var_list, self.tgt_var_list, self.config.tgt_time_len, src.size(0))
         src_seq = src_seq.to(self.device)

9f40dedd348d56548d84e703768cfce69430f047
wooj1nBot <1403jung@gmail.com>
2024-03-05 19:24:38 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -79,17 +79,17 @@ class VariableEncoder(nn.Module):
         return out
 
 
-    def positional_encoding(self, shape, device):
-        pe = torch.zeros(shape[0], shape[2], shape[3]).float()
+    def positional_encoding(batch, d_model, var_len, time_len):
+        pe = torch.zeros(batch, time_len, d_model).float()
         pe.require_grad = False
 
-        position = torch.arange(0, shape[1]).float().unsqueeze(1)
-        div_term = (torch.arange(0, shape[3], 2).float() * -(math.log(10000.0) / shape[3])).exp()
+        position = torch.arange(0, time_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
 
         pe[:, :, 0::2] = torch.sin(position * div_term)
         pe[:, :, 1::2] = torch.cos(position * div_term)
 
-        return pe.repeat_interleave(shape[2], dim=1).to(device)
+        return pe.repeat_interleave(var_len, dim=1)
 
 
     @torch.no_grad()

7e6766906ccb728c19c759dc6c1661aa4cfae27d
wooj1nBot <1403jung@gmail.com>
2024-03-05 19:10:43 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -84,7 +84,7 @@ class VariableEncoder(nn.Module):
         position = torch.arange(0, time_len).float().unsqueeze(1)
         div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
 
-        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, :, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
 
         return pe.repeat_interleave(var_len, dim=0)

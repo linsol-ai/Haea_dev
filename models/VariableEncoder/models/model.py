a2c74f1ec5f4a2fe955246a233c354665986d4a0
wooj1nBot <1403jung@gmail.com>
2024-03-05 10:43:12 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -51,7 +51,7 @@ class LinearDecoder(nn.Module):
 
 
 class VariableEncoder(nn.Module):
-    def __init__(self, src_var_list: torch.Tensor, tgt_var_list: torch.Tensor, tgt_time_len: int, 
+    def __init__(self,
                 in_dim: int, out_dim: int, max_var_len=300, 
                 num_heads=12, n_encoder_layers=3, n_decoder_layers=3, dropout=0.1):
         super().__init__()

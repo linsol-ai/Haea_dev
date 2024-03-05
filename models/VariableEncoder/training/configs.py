cb5378cdc28116045ab834f5670d707cdfdfe37a
wooj1nBot <1403jung@gmail.com>
2024-03-05 22:47:00 +0000

    Auto commit

--- models/VariableEncoder/training/configs.py
+++ models/VariableEncoder/training/configs.py
@@ -31,7 +31,7 @@ class TrainingConfig(BaseModel):
     gradient_clip_val: float | None = None
     """The value to clip the gradients to."""
 
-    tgt_time_len: int = Field(ge=0, default=7)
+    src_time_len: int = Field(ge=0, default=7)
 
     tgt_time_len: int = Field(ge=0, default=7)
 

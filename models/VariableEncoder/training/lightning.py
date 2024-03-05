8b20d5e418a2d8b36ba15f007446674647a559a9
wooj1nBot <1403jung@gmail.com>
2024-03-05 18:07:29 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -45,8 +45,8 @@ def get_var_seq(src_var_list: torch.Tensor, tgt_var_list: torch.Tensor, tgt_time
 
 
 def get_tgt_mask(var_len, time_len) -> torch.Tensor:
-        size = var_len * time_len
-        matrix = torch.full((size, size), float('-inf'))
+    size = var_len * time_len
+     matrix = torch.full((size, size), float('-inf'))
         matrix[0, 0] = 0
         for i in range(time_len):
             s =  (i * var_len) + 1

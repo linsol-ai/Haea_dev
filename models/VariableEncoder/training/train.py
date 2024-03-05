592cc823b1ac6d5c54d494d7fd73df727ecd7e65
wooj1nBot <1403jung@gmail.com>
2024-03-05 22:53:47 +0000

    Auto commit

--- models/VariableEncoder/training/train.py
+++ models/VariableEncoder/training/train.py
@@ -79,7 +79,7 @@ def get_normal_dataset(config: TrainingConfig) -> Tuple[CustomDataset, torch.Ten
     src_var_list = var_vocab.get_code(vars)
     tgt_var_list = var_vocab.get_code(config.air_variable + config.surface_variable)
 
-    dataset = CustomDataset(source, config.tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))
+    dataset = CustomDataset(source, config.tgt_time_len, config.tgt_time_len, n_only_input=len(config.only_input_variable)+len(config.constant_variable))
     return dataset, mean_std, (src_var_list, tgt_var_list)
 
 

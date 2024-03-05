d36e5e38de2f4754276fe0e8464ddd4cd58afd66
wooj1nBot <1403jung@gmail.com>
2024-03-05 10:48:37 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -63,7 +63,8 @@ class TrainModule(pl.LightningModule):
         self.src_var_list = src_var_list
         self.tgt_var_list = tgt_var_list
         self.save_hyperparameters()
-        
+        self.
+
 
     
     def setup(self, stage: str) -> None:

8ac01cce3c0ce81ab1e12ad1d97312c8cf637428
wooj1nBot <1403jung@gmail.com>
2024-03-05 12:38:44 +0000

    Auto commit

--- models/VariableEncoder/datasets/dataset.py
+++ models/VariableEncoder/datasets/dataset.py
@@ -24,7 +24,7 @@ class CustomDataset(Dataset):
         result = []
         for i, t in enumerate(indicate):
             if t == self.SPECIAL_TOKEN_BOS or t == self.SPECIAL_TOKEN_EOS:
-                data = 
+                data = torch.zeros(1, source_dataset.size(-1))
             else:  
                 data = self.source_dataset[t]
                 if not source and self.n_only_input > 0:

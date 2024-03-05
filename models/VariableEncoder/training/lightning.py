7b79ff6cdaa4b3585da4150f4eefe0a39e9657e6
wooj1nBot <1403jung@gmail.com>
2024-03-05 18:32:53 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -89,7 +89,7 @@ class TrainModule(pl.LightningModule):
         label = batch[1]
         zeros_tensor = torch.zeros(batch, 1, var, hidden)
 
-# Insert the zeros tensor at the first index of the time dimension
+        # Insert the zeros tensor at the first index of the time dimension
 new_tensor = torch.cat((zeros_tensor, tensor[:, 1:, :, :]), dim=1)
         src_seq, tgt_seq = get_var_seq(self.src_var_list, self.tgt_var_list, self.config.tgt_time_len, src.size(0))
         src_seq = src_seq.to(self.device)

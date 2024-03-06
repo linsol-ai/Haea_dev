eb1ac75c4e413b721cc23c0cf357dac7175f0ff4
wooj1nBot <1403jung@gmail.com>
2024-03-06 04:30:37 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -8,6 +8,27 @@ class VariableEmbedding(nn.Embedding):
     def __init__(self, var_len, embed_size=768):
         super().__init__(var_len, embed_size)
 
+class PositionalEmbedding(nn.Module):
+
+    def __init__(self, d_model, max_len=512):
+        super().__init__()
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
+
 
 class Embedding(nn.Module):
     def __init__(self, var_len, embed_size, dropout=0.1):

c67865485be1f21bb2f837971a0bf0da105dfb07
wooj1nBot <1403jung@gmail.com>
2024-03-06 04:30:49 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -39,6 +39,7 @@ class Embedding(nn.Module):
         """
         super().__init__()
         self.variable = VariableEmbedding(var_len, embed_size)
+        
         self.dropout = nn.Dropout(p=dropout)
         self.embed_size = embed_size
 

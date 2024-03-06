56d9a37124fd18cfb936447fd01734f857bb6443
wooj1nBot <1403jung@gmail.com>
2024-03-06 05:31:09 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -12,6 +12,7 @@ import math
 SPECIAL_TOKEN_BOS = torch.tensor([0])
 SPECIAL_TOKEN_EOS = torch.tensor([1])
 
+
 def denormalize(inputs, mean_std) -> torch.Tensor:
     mean = mean_std[:, 0].view(1, 1, mean_std.size(0), 1)
     std = mean_std[:, 1].view(1, 1, mean_std.size(0), 1)

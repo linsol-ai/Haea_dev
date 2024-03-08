6c43acad4f9fe810eea44f609763a71aa9256745
wooj1nBot <1403jung@gmail.com>
2024-03-08 19:32:02 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -144,7 +144,8 @@ def main(argv):
 
     print("setting lr rate: ", config.training.learning_rate)
 
-    model_pl = TrainModule(model=model, mean_std=mean_std, max_iters=max_iters, 
+    model_pl = TrainModule(model=model, mean_std=mean_std, max_iters=max_iters,
+                           
                         config=config.training)
     
     trainer = pl.Trainer(

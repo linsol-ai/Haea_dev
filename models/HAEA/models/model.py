942809b1ef49039b2dd338e4105e32d85210fef4
wooj1nBot <1403jung@gmail.com>
2024-03-08 19:17:50 +0000

    Auto commit

--- models/HAEA/models/model.py
+++ models/HAEA/models/model.py
@@ -54,7 +54,7 @@ class Haea(nn.Module):
         super().__init__()
         self.in_dim = in_dim
         self.tgt_mask = tgt_mask
-        self.src_var_list = src_var_list
+        self.var_list = var_list
         self.tgt_var_list = tgt_var_list
 
         self.model = nn.Transformer(

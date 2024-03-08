d49c0f80890e3fcf9c9c724c0ba5f2f20fefdad6
wooj1nBot <1403jung@gmail.com>
2024-03-08 07:24:42 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -33,7 +33,7 @@ def positional_encoding(self, batch, time_len, var_len, d_model):
     pe[:, :, 0::2] = torch.sin(position * div_term)
     pe[:, :, 1::2] = torch.cos(position * div_term)
 
-        return pe.repeat_interleave(var_len, dim=1)
+    return pe.repeat_interleave(var_len, dim=1)
 
 
 def get_var_seq(src_var_list: torch.Tensor, tgt_var_list: torch.Tensor, src_time_len: int, tgt_time_len: int, batch_size: int):

0ed6d70287736bb1a2730ea18631f0bcc7bb64e5
wooj1nBot <1403jung@gmail.com>
2024-03-08 11:19:25 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -39,7 +39,6 @@ def positional_encoding(batch, time_len, var_len, d_model, device, has_special_t
 
 def get_var_seq(src_var_list: torch.Tensor, tgt_var_list: torch.Tensor, src_time_len: int, tgt_time_len: int, batch_size: int):
     tgt_seq = tgt_var_list.repeat_interleave(tgt_time_len, dim=0)
-    tgt_seq = torch.cat([SPECIAL_TOKEN_BOS, tgt_seq], dim=0)
     tgt_seq = tgt_seq.unsqueeze(0).repeat_interleave(batch_size, dim=0)
 
     src_seq = src_var_list.repeat_interleave(src_time_len, dim=0)

80d922f4eaa2484d0db3682a29d2c4b2b0c84a32
wooj1nBot <1403jung@gmail.com>
2024-03-08 16:55:22 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -172,7 +172,7 @@ class DenoisingDataset(Dataset):
 
         source_dataset = self.vocab.get_data(source)
         target_dataset = self.vocab.get_data(target)
-        target_dataset = torch.cat([self.vocab.dataset[target[0]-1], target_dataset], dim)
+        target_dataset = torch.cat([self.vocab.dataset[target[0]-1], target_dataset], dim=0)
 
         return {
             "source": source_dataset,

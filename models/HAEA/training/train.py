99733e47ffd62a573d671767c488c9120ba4bc05
wooj1nBot <1403jung@gmail.com>
2024-03-08 19:31:56 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -126,7 +126,6 @@ class DataModule(pl.LightningDataModule):
 
 def main(argv):
     data_module = DataModule(config.training)
-    dataset = data_module.dataset
     mean_std = data_module.mean_std
     max_iters = config.training.max_epochs*(len(data_module.train_ds) // config.training.batch_size)
     print(f"max_iters: {max_iters}")

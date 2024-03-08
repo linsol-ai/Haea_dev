fbb560db0062d05ffdedaccf5870df8988ecadf9
wooj1nBot <1403jung@gmail.com>
2024-03-08 11:32:37 +0000

    Auto commit

--- models/VariableEncoder/models/model.py
+++ models/VariableEncoder/models/model.py
@@ -71,7 +71,7 @@ class VariableEncoder(nn.Module):
         src_pe = self.positional_encoding(src.shape, src.device)
         tgt_pe = self.positional_encoding(tgt.shape, tgt.device)
 
-        src, tgt = src.view()
+        src, tgt = src.view(src.size(0), )
 
         src = (self.embedding(src, src_var_seq) + src_pe) * math.sqrt(self.in_dim)
         tgt =  (self.embedding(tgt, tgt_var_seq) + tgt_pe) * math.sqrt(self.in_dim)

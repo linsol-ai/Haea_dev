e254e54b66a69d7302b845c4d7a6f123e2be47ea
wooj1nBot <1403jung@gmail.com>
2024-03-08 07:47:19 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -28,8 +28,8 @@ def positional_encoding(batch, time_len, var_len, d_model, device, has_special_t
     if has_special_token:
         pe = torch.zeros(batch, time_len+1, d_model, device=device).float()
     else:
-        pe = torch.zeros(batch, time_len, d_model, device=device).float()
-
+        
+pe = torch.zeros(batch, time_len, d_model, device=device).float()
     pe.require_grad = False
     position = torch.arange(0, time_len).float().unsqueeze(1)
     

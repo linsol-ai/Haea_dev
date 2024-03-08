325a506557170f6e54fa358ab3d2b713f9d96167
wooj1nBot <1403jung@gmail.com>
2024-03-08 15:55:51 +0000

    Auto commit

--- models/HAEA/datasets/denoised_dataset.py
+++ models/HAEA/datasets/denoised_dataset.py
@@ -130,7 +130,7 @@ class DenoisingDataset(Dataset):
     def make_dataset(self, size, length):
         dataset = []
         
-        for i in range(1, size-lengt):
+        for i in range(1, size-lengtt):
             start = i
             end = i + length
             data = [x for x in range(start, end)]

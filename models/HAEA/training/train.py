81165b96f37b6929fee88687d2e310c82c9aa13c
wooj1nBot <1403jung@gmail.com>
2024-03-08 19:33:11 +0000

    Auto commit

--- models/HAEA/training/train.py
+++ models/HAEA/training/train.py
@@ -146,7 +146,7 @@ def main(argv):
 
     model_pl = TrainModule(
         model=model, mean_std=mean_std, max_iters=max_iters,
-        var_list=data_module.,config=config.training)
+        var_list=data_module.time_vocab.var_list, config=config.training)
     
     trainer = pl.Trainer(
         accelerator="auto",

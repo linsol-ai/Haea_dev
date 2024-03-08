4262127bd01d613daff3afff0ed7878a5713b5b1
wooj1nBot <1403jung@gmail.com>
2024-03-08 07:33:52 +0000

    Auto commit

--- models/VariableEncoder/training/lightning.py
+++ models/VariableEncoder/training/lightning.py
@@ -101,7 +101,7 @@ class TrainModule(pl.LightningModule):
         src_seq = src_seq.to(self.device)
         tgt_seq = tgt_seq.to(self.device)
       
-        # predict.shape = (batch, time, var, hidden)
+        # predict.shape = (batch, time * var + 1, hidden)
         predict = self.model(src, tgt, src_seq, tgt_seq, self.tgt_mask)
         predict = predict.view(predict.size(0), -1, self.tgt_var_list.size(0), predict.size(-1))
         predict = predict[:, :-1]

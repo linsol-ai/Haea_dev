fe6bfdd188d2f6f5b45b03b7e501d180d974064c
wooj1nBot <1403jung@gmail.com>
2024-03-09 12:43:34 +0000

    Auto commit

--- models/HAEA/training/finetuning.py
+++ models/HAEA/training/finetuning.py
@@ -119,7 +119,7 @@ def main(argv):
     print(f"max_iters: {max_iters}")
 
     logger = WandbLogger(save_dir=os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'tb_logs'), name="my_model")
-    checkpoint = torch.load(CKPT_PATH)
+    checkpoint = torch.load(FLAGS.M)
 
     print("setting lr rate: ", config.training.learning_rate)
 

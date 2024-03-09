f93d981bee49fd494682dec7be47fc4ca891ff34
wooj1nBot <1403jung@gmail.com>
2024-03-09 12:17:45 +0000

    Auto commit

--- models/HAEA/training/finetuning.py
+++ models/HAEA/training/finetuning.py
@@ -14,7 +14,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset, VariableVocab
 from models.HAEA.datasets.dataset import CustomDataset
-from models.HAEA.models.model import VariableEncoder
+from models.HAEA.models.model import Haea
 
 from models.HAEA.training.configs import TrainingConfig
 from models.HAEA.training.configs import TrainingRunConfig

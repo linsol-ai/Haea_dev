05ef9e16c7d9c4e9ad8373cdbda8c2fd540893e8
wooj1nBot <1403jung@gmail.com>
2024-03-09 12:36:51 +0000

    Auto commit

--- models/HAEA/training/configs.py
+++ models/HAEA/training/configs.py
@@ -49,6 +49,42 @@ class PretrainingConfig(BaseModel):
 
     train_end : datetime.datetime
 
+
+class PretrainingConfig(BaseModel):
+    """A config specification of training the DVAE."""
+
+    learning_rate: float = 2e-4
+    """The learning rate."""
+
+    warmup_step: int = Field(ge=0, default=4000)
+
+    batch_size: int = Field(ge=0, default=256)
+    """"The batch size."""
+
+    max_epochs: int = Field(ge=0, default=100)
+    """The maximum number of training epochs."""
+
+    gradient_clip_val: float | None = None
+    """The value to clip the gradients to."""
+
+    time_len: int = Field(ge=0, default=7)
+
+    mask_ratio: float = 0.3
+
+    air_variable: List[str]
+
+    surface_variable: List[str]
+
+    only_input_variable: List[str] = []
+
+    constant_variable: List[str] = []
+
+    levels: List[int] = []
+
+    train_start: datetime.datetime
+
+    train_end : datetime.datetime
+
     
 
 class PretrainingRunConfig(BaseModel):

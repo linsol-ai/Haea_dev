7db9d859020831ff8dfef21592c8c676ad7c920b
wooj1nBot <1403jung@gmail.com>
2024-03-09 13:29:35 +0000

    Auto commit

--- models/HAEA/training/lightning.py
+++ models/HAEA/training/lightning.py
@@ -37,7 +37,7 @@ class TrainModule(pl.LightningModule):
         super().__init__()
         self.max_iters = max_iters
         self.model = model
-        self.var_list = var_list + 
+        self.var_list = var_list + 4
         self.mean_std = mean_std
         self.config = config
         self.save_hyperparameters()

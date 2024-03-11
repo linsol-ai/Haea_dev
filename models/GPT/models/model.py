90b903cbc3b13b8a19bda82b8311257821882744
wooj1nBot <1403jung@gmail.com>
2024-03-11 17:43:17 +0000

    Auto commit

--- models/GPT/models/model.py
+++ models/GPT/models/model.py
@@ -45,7 +45,7 @@ class LinearDecoder(nn.Module):
     def forward(self, x):
        return self.seq(x)
     
-    
+
 class PositionWiseFeedForwardNetwork(nn.Module):
     def __init__(self, d_model, d_ff):
         super(PositionWiseFeedForwardNetwork, self).__init__()
@@ -95,6 +95,61 @@ class DecoderLayer(nn.Module):
         # |ffn_outputs| : (batch_size, seq_len, d_model)
         
         return ffn_outputs, attn_weights
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, vocab_size, seq_len, d_model, n_layers, n_heads, d_ff, embd_pdrop, attn_pdrop, resid_pdrop, pad_id):
+        super(TransformerDecoder, self).__init__()
+        self.pad_id = pad_id
+
+        # layers
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.dropout = nn.Dropout(embd_pdrop)
+        self.pos_embedding = nn.Embedding(seq_len+1, d_model)
+        self.layers = nn.ModuleList([DecoderLayer(d_model, n_heads, d_ff, attn_pdrop, resid_pdrop) for _ in range(n_layers)])
+        
+        nn.init.normal_(self.embedding.weight, std=0.02)
+        
+    def forward(self, inputs):
+        # |inputs| : (batch_size, seq_len)
+        positions = torch.arange(inputs.size(1), device=inputs.device, dtype=inputs.dtype).repeat(inputs.size(0), 1) + 1
+        position_pad_mask = inputs.eq(self.pad_id)
+        positions.masked_fill_(position_pad_mask, 0)
+        # |positions| : (batch_size, seq_len)
+
+        outputs = self.dropout(self.embedding(inputs)) + self.pos_embedding(positions)
+        # |outputs| : (batch_size, seq_len, d_model)
+        
+        attn_pad_mask = self.get_attention_padding_mask(inputs, inputs, self.pad_id)
+        # |attn_pad_mask| : (batch_size, seq_len, seq_len)
+        subsequent_mask = self.get_attention_subsequent_mask(inputs).to(device=attn_pad_mask.device)
+        # |subsequent_mask| : (batch_size, seq_len, seq_len)
+        attn_mask = torch.gt((attn_pad_mask.to(dtype=subsequent_mask.dtype) + subsequent_mask), 0)
+        # |attn_mask| : (batch_size, seq_len, seq_len)
+        
+        attention_weights = []
+        for layer in self.layers:
+            outputs, attn_weights = layer(outputs, attn_mask)
+            # |outputs| : (batch_size, seq_len, d_model)
+            # |attn_weights| : (batch_size, n_heads, seq_len, seq_len)
+            attention_weights.append(attn_weights)
+        
+        return outputs, attention_weights    
+        
+    def get_attention_padding_mask(self, q, k, pad_id):
+        attn_pad_mask = k.eq(pad_id).unsqueeze(1).repeat(1, q.size(1), 1)
+        # |attn_pad_mask| : (batch_size, q_len, k_len)
+
+        return attn_pad_mask
+    
+    def get_attention_subsequent_mask(self, q):
+        bs, q_len = q.size()
+        subsequent_mask = torch.ones(bs, q_len, q_len).triu(diagonal=1)
+        # |subsequent_mask| : (batch_size, q_len, q_len)
+        
+        return subsequent_mask
+    
+    
 
 
 class VariableEncoder(nn.Module):

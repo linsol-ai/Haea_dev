3d5777e2702eff72487562e53384353034c2fc6f
wooj1nBot <1403jung@gmail.com>
2024-03-11 17:37:53 +0000

    Auto commit

---
+++ models/GPT/datasets/dataset.py
@@ -0,0 +1,45 @@
+from torch.utils.data import Dataset
+import torch
+import math
+
+class CustomDataset(Dataset):
+
+    def __init__(self, source_dataset: torch.Tensor, src_time_len: int, tgt_time_len: int, n_only_input: int = 0):
+        # dataset.shape = (time, var_len, hidden)
+        self.source_dataset = source_dataset.unsqueeze(1)
+        self.src_time_len = src_time_len
+        self.tgt_time_len = tgt_time_len
+        self.n_only_input = n_only_input
+        self.make_dataset()
+
+    def __len__(self):
+        return self.source_dataset.size(0)-(self.tgt_time_len + self.src_time_len+1)
+
+    def get_data(self, indicate, source: bool = True):
+        result = []
+        for t in indicate:
+            data = self.source_dataset[t]
+            if not source and self.n_only_input > 0:
+                data = data[:, :-self.n_only_input, :]
+            result.append(data)
+
+        # result.shape = (time_len * var, hidden)
+        result = torch.concat(result, dim=0)
+        return result
+
+    def make_dataset(self):
+        dataset_inc = []
+        for t in range(self.src_time_len, self.source_dataset.size(0)-self.tgt_time_len-1):
+            src = [r for r in range(t-self.src_time_len, t)]
+            tgt = [r for r in range(t, t + self.tgt_time_len + 1)]
+            dataset_inc.append((src, tgt))
+
+        self.dataset_inc = dataset_inc
+
+
+    def __getitem__(self, item):
+        src_ind, tgt_ind = self.dataset_inc[item]
+        src = self.get_data(src_ind)
+        tgt = self.get_data(tgt_ind, source=False)
+        return src, tgt
+
---
+++ models/GPT/training/predict.py
@@ -0,0 +1,44 @@
+import tqdm
+import torch
+from torch.utils.data import DataLoader, Dataset
+from pathlib import Path
+from training.lightning import TrainModule
+
+class EncoderDataset(Dataset):
+        def __init__(self, data_array: torch.Tensor):
+            self.data_array = data_array
+        def __len__(self):
+            return len(self.data_array)
+        def __getitem__(self, idx):
+            sample = self.data_array[idx]
+            return sample
+
+class VariablePredictor:
+    def __init__(self, model_path: str, batch_size: int):
+        self.model_path = model_path
+        self.batch_size = batch_size
+        self.load_models(model_path)
+        device = ("cuda" if torch.cuda.is_available() else "cpu" )
+        self.device = torch.device(device)
+    
+    def load_models(self, model_path):
+        folder_path = Path(model_path)
+        first_file = next(folder_path.iterdir(), None)
+        if first_file:
+                self.model = TrainModule.load_from_checkpoint(first_file).model
+        else:
+            raise Exception("Not exists VariableEncoder model")
+    
+    def predict(self, dataset: torch.Tensor) -> torch.Tensor:
+        dataset = EncoderDataset(dataset)
+        data_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False, num_workers=8)
+        predictions = []
+        for _, batch in enumerate(tqdm.tqdm(data_loader)):
+            # shape = (batch, var_len, hidden)
+            predict = self.model.encode(batch.to(self.device)).cpu()
+            predictions.append(predict)
+        
+        # predictions.shape = (time, var_len, hidden)
+        predictions = torch.cat(predictions, dim=0)
+        return predictions
+

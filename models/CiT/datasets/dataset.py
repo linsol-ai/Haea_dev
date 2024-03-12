9b87390fd02df77ab5b0770a3d0f0ae66eba679d
wooj1nBot <1403jung@gmail.com>
2024-03-12 12:34:53 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -21,7 +21,7 @@ class CustomDataset(Dataset):
         choice = self.sample[torch.randint(0, self.sample.size(0)-1, (1,))].item()
         t = t - max(0, (t + choice + self.time_len) - (self.source_dataset.size(0) - 1))
         src = self.source_dataset[t-self.time_len+1:t+1]
-        tgt = self.source_dataset[t]
+        tgt = self.source_dataset[]
 
         return src, tgt
 

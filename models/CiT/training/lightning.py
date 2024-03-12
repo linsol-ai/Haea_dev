d9aeeeb7d2eeb7ddf6cfae608514da252e15cf07
wooj1nBot <1403jung@gmail.com>
2024-03-12 13:58:37 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -52,7 +52,7 @@ class TrainModule(pl.LightningModule):
         var_seq = batch[3]
         print(src.shape)
         print(delta.shape)
-        p
+        print(var_seq.)
         predict = self.model(src, delta, var_seq)
         loss = rmse_loss(predict, label)
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")

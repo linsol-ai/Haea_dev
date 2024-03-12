44c398410ae304bd137ef4c75a673aa5f85761ab
wooj1nBot <1403jung@gmail.com>
2024-03-12 11:31:40 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -9,7 +9,7 @@ class CustomDataset(Dataset):
         self.max_lead_time = max_lead_time
         self.time_len = time_len
         self.var_seq = var_seq
-        self.samplin
+        self.sampling_l
 
 
     def __len__(self):

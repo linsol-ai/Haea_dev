25e95f307e9e84187f2cb3d1d84081ebe5f35840
wooj1nBot <1403jung@gmail.com>
2024-03-13 06:43:21 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -92,7 +92,7 @@ class Electra(nn.Module):
 
     def forward(self, src: torch.Tensor, var_seq: torch.Tensor):
         # src.shape = (batch, time, var_len, hidden), lead_time.shape = (batch)
-        var_seq = var_seq.repeat_interleave(x.size(1), dim=1)
+        var_seq = var_seq.repeat_interleave(src.size(1), dim=1)
         src_pe = self.positional_encoding(x.shape, x.device)
         x = x.view(x.size(0), -1, x.size(-1))
 

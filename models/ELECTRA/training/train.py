f7d0526020b2cabe562f9e64b3b3f8f0e39e58ff
wooj1nBot <1403jung@gmail.com>
2024-03-13 16:27:39 +0000

    Auto commit

--- models/ELECTRA/training/train.py
+++ models/ELECTRA/training/train.py
@@ -139,6 +139,7 @@ def main(argv):
         g_layers=config.model.g_layers,
         d_layers=config.model.d_layers,
         dropout=config.model.dropout,
+        di
         max_var_len=len(data_module.var_vocab)
     )
 

c8dc611e781316db9aa3b0abcab1a520390294b1
wooj1nBot <1403jung@gmail.com>
2024-03-13 05:35:47 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -83,7 +83,7 @@ class CliBERT(nn.Module):
         src_pe = self.positional_encoding(x.shape, x.device)
         x = x.view(x.size(0), -1, x.size(-1))
 
-        
+        if lead_time
         lead_time = lead_time.unsqueeze(1).repeat(1, x.size(1))
 
         x = self.embedding(x, var_seq, lead_time, src_pe) * math.sqrt(self.in_dim)

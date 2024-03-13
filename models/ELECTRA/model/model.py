e36bc492b0b137f2464f2d21981180691030355d
wooj1nBot <1403jung@gmail.com>
2024-03-13 19:13:56 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -114,7 +114,7 @@ class CliBERT(nn.Module):
         return x
 
 
-class CliBERTMLM(nn.Module):
+class CliBERT(nn.Module):
     def __init__(self, in_dim: int, num_heads=12, n_layers=3, dropout=0.1):
         super().__init__()
         self.in_dim = in_dim

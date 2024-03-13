10fe77de33b24292f766c06d22bff84a3ccb7b4e
wooj1nBot <1403jung@gmail.com>
2024-03-13 08:04:23 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -110,7 +110,7 @@ class Electra(nn.Module):
         x = self.decoder(x)
         return x
 
-    def generate():
+    def generate(self, ):
     
 
     def get_var_seq(self, var_list: torch.Tensor, indicate: torch.Tensor, device):

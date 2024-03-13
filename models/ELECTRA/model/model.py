38ee928941bf128e11a4bf9bb57302bd9238bcd5
wooj1nBot <1403jung@gmail.com>
2024-03-13 08:21:49 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -32,7 +32,6 @@ def get_var_seq(var_list: torch.Tensor, indicate: torch.Tensor, device):
 def positional_encoding(shape, device):       
         batch, time_len, var_len, d_model = shape 
         pe = torch.zeros(batch, time_len, d_model, device=device).float()
-        pe.require_grad = False
         position = torch.arange(0, time_len).float().unsqueeze(1)
         
         div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()

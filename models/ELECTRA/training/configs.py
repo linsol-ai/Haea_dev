08469164547fa44dcfc5a3958485c636dadd4f67
wooj1nBot <1403jung@gmail.com>
2024-03-13 17:48:03 +0000

    Auto commit

--- models/ELECTRA/training/configs.py
+++ models/ELECTRA/training/configs.py
@@ -37,10 +37,6 @@ class TrainingConfig(BaseModel):
 
     mask_ratio: float = 0.3
 
-    disc_weight: float = 50.
-
-    gen_weight: float = 1.
-
     air_variable: List[str]
 
     surface_variable: List[str]

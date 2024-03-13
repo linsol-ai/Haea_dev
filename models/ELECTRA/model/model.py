e8dba4a4019695a903df6628d63ad2f7ffeb3899
wooj1nBot <1403jung@gmail.com>
2024-03-13 10:23:30 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -154,7 +154,7 @@ class Electra(nn.Module):
 
         masked = torch.cat(masked, dim=0)
 
-         for i in range(x.size(0)):
+        for i in range(x.size(0)):
             x[i, mask_ind[i]] = masked[i]
 
         return masked, mask_ind

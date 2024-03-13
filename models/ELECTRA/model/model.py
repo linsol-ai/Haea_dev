de01d174c788bcebb0fcdf257b5e3e03d4f2fb4f
wooj1nBot <1403jung@gmail.com>
2024-03-13 07:56:43 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -97,6 +97,7 @@ class Electra(nn.Module):
         gen_var_seq, mask_ind = self.get_var_seq(var_list, src_id, src.device)
         src = self.embedding(src, gen_var_seq, src_pe) * math.sqrt(self.in_dim)
         x = self.generator(src)
+        
 
         # out.shape = (batch, var_len, hidden)
         x = self.decoder(x)

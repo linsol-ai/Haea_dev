5c3b5279ad4ca9cf84177e804fbbaf1ca0d86b8e
wooj1nBot <1403jung@gmail.com>
2024-03-13 07:37:57 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -106,7 +106,7 @@ class Electra(nn.Module):
     def get_var_seq(self, var_list: torch.Tensor, indicate: torch.Tensor, device):
         # indicate.shape = (batch, max_len)
         result = []
-
+        var_len = 
         for batch in indicate:
             seq = []
             mask_ind = []

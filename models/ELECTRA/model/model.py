bedb29956c1d1f5461206fd90d41a30f7544bf8c
wooj1nBot <1403jung@gmail.com>
2024-03-13 15:16:42 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -155,6 +155,7 @@ class Electra(nn.Module):
             masked.append(gen[i, mask_ind[i]].unsqueeze(0))
 
         masked = torch.cat(masked, dim=0)
+        
         return masked, mask_ind
     
 

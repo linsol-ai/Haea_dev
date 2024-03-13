72b27e68c11dfad10ff8f265afe3d8eb37ca977a
wooj1nBot <1403jung@gmail.com>
2024-03-13 08:08:36 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -95,7 +95,7 @@ class Electra(nn.Module):
         src_pe = self.positional_encoding(x.shape, x.device)
         x = x.view(x.size(0), -1, x.size(-1))
         masked = self.generate(x, src_pe, var_list, src_id)
-
+        
 
         # out.shape = (batch, var_len, hidden)
         x = self.decoder(x)

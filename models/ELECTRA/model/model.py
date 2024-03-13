18e719b4ea5e3dff25f1bcbb9cfef1a554efdf2c
wooj1nBot <1403jung@gmail.com>
2024-03-13 07:40:47 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -123,7 +123,7 @@ class Electra(nn.Module):
             mask_ind.append(mask)
         result = torch.stack(result, dim=0)
         mask_ind = torch.stack(mask_ind, dim=0)
-        return result
+        return result, 
 
 
     def positional_encoding(self, shape, device):       

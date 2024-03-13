6f11dbe7845736aa33a4b54a3c99ccdd0155d0c9
wooj1nBot <1403jung@gmail.com>
2024-03-13 07:40:55 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -108,7 +108,7 @@ class Electra(nn.Module):
         result = []
         mask_ind = []
         var_len = var_list.size(0)
-        
+
         for batch in indicate:
             seq = []
             mask = []
@@ -122,6 +122,7 @@ class Electra(nn.Module):
             seq = torch.cat(seq, dim=0)
             result.append(seq)
             mask_ind.append(mask)
+            
         result = torch.stack(result, dim=0)
         mask_ind = torch.stack(mask_ind, dim=0)
         return result, mask_ind

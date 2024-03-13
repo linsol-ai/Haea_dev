80e188ca75df30ab879b9a6e4ee31e1355c21c10
wooj1nBot <1403jung@gmail.com>
2024-03-13 08:22:50 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -6,27 +6,27 @@ from models.ELECTRA.datasets.denoised_dataset import TimeVocab
 
 def get_var_seq(var_list: torch.Tensor, indicate: torch.Tensor, device):
     # indicate.shape = (batch, max_len)
-        result = []
-        mask_ind = []
-        var_len = var_list.size(0)
+    result = []
+    mask_ind = []
+    var_len = var_list.size(0)
 
-        for batch in indicate:
-            seq = []
-            mask = []
-            for i, item in enumerate(batch):
-                if item == TimeVocab.SPECIAL_TOKEN_MASK:
-                        seq.append(torch.full_like(var_list, TimeVocab.SPECIAL_TOKEN_MASK, device=device))
-                        mask.extend(range(i*var_len, i*var_len + var_len, 1))
-                else:
-                    seq.append(var_list)
+    for batch in indicate:
+        seq = []
+        mask = []
+        for i, item in enumerate(batch):
+            if item == TimeVocab.SPECIAL_TOKEN_MASK:
+                    seq.append(torch.full_like(var_list, TimeVocab.SPECIAL_TOKEN_MASK, device=device))
+                    mask.extend(range(i*var_len, i*var_len + var_len, 1))
+            else:
+                seq.append(var_list)
 
-            seq = torch.cat(seq, dim=0)
-            result.append(seq)
-            mask_ind.append(mask)
+        seq = torch.cat(seq, dim=0)
+        result.append(seq)
+        mask_ind.append(mask)
             
-        result = torch.stack(result, dim=0)
-        mask_ind = torch.tensor(mask_ind)
-        return result, mask_ind
+    result = torch.stack(result, dim=0)
+    mask_ind = torch.tensor(mask_ind)
+    return result, mask_ind
 
 
 def positional_encoding(shape, device):

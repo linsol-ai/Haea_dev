a671b9a8b50234cddebb491ef3f770f71d271963
wooj1nBot <1403jung@gmail.com>
2024-03-13 16:21:11 +0000

    Auto commit

--- models/ELECTRA/training/train.py
+++ models/ELECTRA/training/train.py
@@ -13,12 +13,12 @@ from absl import flags
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset, VariableVocab
-from models.ELECTRA.datasets.denoised_dataset import DenoisingDataset
-from models.ELECTRA.
+from models.HAEA.datasets.denoised_dataset import DenoisingDataset, TimeVocab
+from models.HAEA.models.model import Haea
 
-from models.ELECTRA.training.configs import TrainingConfig
-from models.ELECTRA.training.configs import TrainingRunConfig
-from models.ELECTRA.training.lightning import PretrainModule
+from models.HAEA.training.configs import TrainingConfig
+from models.HAEA.training.configs import TrainingRunConfig
+from models.HAEA.training.lightning import TrainModule
 
 
 import os

d42a9950eb20b99954ac497e40dd595ed6aa006d
wooj1nBot <1403jung@gmail.com>
2024-03-13 07:35:01 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -110,6 +110,7 @@ class Electra(nn.Module):
 
         for batch in indicate:
             seq = []
+            mask_ind = []
             for i, item in enumerate(batch):
                 if item == TimeVocab.SPECIAL_TOKEN_MASK:
                         seq.append(torch.full_like(var_list, TimeVocab.SPECIAL_TOKEN_MASK, device=device))

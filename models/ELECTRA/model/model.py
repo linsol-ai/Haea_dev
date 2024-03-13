373febbe19a5f5cc4b0d361e3c7f1f6c71382700
wooj1nBot <1403jung@gmail.com>
2024-03-13 06:35:00 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -132,7 +132,8 @@ class CliBERT(nn.Module):
 
 
 class Electra(nn.Module):
-    def __init__(self, in_dim: int, out_dim: int, num_heads=12, g_layers=3, d_layers=12, dropout=0.1, max_lead_time=500, max_var_len=300):
+    def __init__(self, in_dim: int, out_dim: int, 
+                 num_heads=12, g_layers=3, d_layers=12, dropout=0.1, max_lead_time=500, max_var_len=300):
         
         self.in_dim = in_dim
         self.generator = generator

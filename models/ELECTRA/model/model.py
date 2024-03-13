4b6cfc923bcce1ad0ff800cc5a49adff351c2b36
wooj1nBot <1403jung@gmail.com>
2024-03-13 06:35:09 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -140,7 +140,7 @@ class Electra(nn.Module):
         self.discriminator = discriminator
         self.embedding = Embedding(max_lead_time, max_var_len, in_dim, dropout)
         self.decoder = LinearDecoder(in_dim, out_dim, dropout=dropout)
-        
+        self.
         
 
     

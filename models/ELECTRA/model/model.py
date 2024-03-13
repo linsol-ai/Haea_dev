230945169d4ff756d2f7b1e295c74a730042edd5
wooj1nBot <1403jung@gmail.com>
2024-03-13 10:07:48 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -154,7 +154,7 @@ class Electra(nn.Module):
 
         masked = torch.cat(masked, dim=0)
 
-        loss = torch.sqrt(F.mse_loss())
+        loss = torch.sqrt(F.mse_loss(x, ))
 
         return masked, mask_ind
     

6a541d5ca4aca0180a39e6f96e16012732d66d13
wooj1nBot <1403jung@gmail.com>
2024-03-13 15:54:19 +0000

    Auto commit

--- models/ELECTRA/training/lightning.py
+++ models/ELECTRA/training/lightning.py
@@ -19,7 +19,7 @@ def rmse_loss(x, y):
     return torch.sqrt(F.mse_loss(x, y))
 
 
-class PreTrainModule(pl.LightningModule):
+class PretrainModule(pl.LightningModule):
 
     def __init__(self, *, model: ClimateTransformer, mean_std: torch.Tensor, var_list: torch.Tensor,
                  max_iters: int, config: TrainingConfig):

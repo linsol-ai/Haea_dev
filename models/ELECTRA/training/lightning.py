983d9bcac7966ca48fc38e81554b8598521e9eef
wooj1nBot <1403jung@gmail.com>
2024-03-13 15:58:33 +0000

    Auto commit

--- models/ELECTRA/training/lightning.py
+++ models/ELECTRA/training/lightning.py
@@ -54,7 +54,7 @@ class PretrainModule(pl.LightningModule):
         loss, mlm_loss, disc_loss = self.model(src, label, self.var_list, src_id)
         self.log(f"{mode}/loss", loss, prog_bar=mode == "train")
         self.log(f"{mode}/mlm_loss", loss, prog_bar=mode == "train")
-        self.log(f"{mode}/disc_loss", loss, prog_bar=mode == "train")
+        self.log(f"{mode}/disc_loss", disc_loss, prog_bar=mode == "train")
         return loss
 
 

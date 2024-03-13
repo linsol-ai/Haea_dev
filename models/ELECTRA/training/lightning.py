6c1582a8521dd5c9c898ed10be76df6b865039cb
wooj1nBot <1403jung@gmail.com>
2024-03-13 19:20:04 +0000

    Auto commit

--- models/ELECTRA/training/lightning.py
+++ models/ELECTRA/training/lightning.py
@@ -21,7 +21,7 @@ def rmse_loss(x, y):
 
 class PretrainModule(pl.LightningModule):
 
-    def __init__(self, *, model: Electra, mean_std: torch.Tensor, var_list: torch.Tensor,
+    def __init__(self, *, model: CliBERTLM, mean_std: torch.Tensor, var_list: torch.Tensor,
                  max_iters: int, config: TrainingConfig):
         
         super().__init__()

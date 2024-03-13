8ad44d38b59ce4cfdc85af602e047e5a073d095c
wooj1nBot <1403jung@gmail.com>
2024-03-13 17:05:03 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -139,8 +139,6 @@ class Electra(nn.Module):
         # masked.shape = (batch, mask_size, hidden)
         masked, mask_ind, label = self.generate(src, src_pe, var_list, src_id)
         for i in range(src.size(0)):
-            print(masked[i])
-            print(src[i])
             src[i, mask_ind[i]] = masked[i]
 
         mlm_loss = torch.sqrt(F.mse_loss(src, tgt))

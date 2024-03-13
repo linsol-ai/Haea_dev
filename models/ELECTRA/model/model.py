9ed31444ddcba4c574ca2184eade9f3b6c2ffeff
wooj1nBot <1403jung@gmail.com>
2024-03-13 17:23:21 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -145,7 +145,7 @@ class Electra(nn.Module):
 
         mlm_loss = torch.sqrt(F.mse_loss(src, tgt))
 
-        logits = self.discriminate(src, src_pe, var_list)
+        logits = self.discriminate(src, src_pe, var_seq)
 
         disc_loss = F.binary_cross_entropy_with_logits(
             logits,

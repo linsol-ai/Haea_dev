bb3eb976acfb160af900bd0db738e940ff856876
wooj1nBot <1403jung@gmail.com>
2024-03-13 07:55:02 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -96,8 +96,8 @@ class Electra(nn.Module):
         src = src.view(src.size(0), -1, src.size(-1))
         gen_var_seq, mask_ind = self.get_var_seq(var_list, src_id, src.device)
 
-
         
+
         src = self.embedding(src, gen_var_seq, src_pe) * math.sqrt(self.in_dim)
         x = self.generator(src)
 

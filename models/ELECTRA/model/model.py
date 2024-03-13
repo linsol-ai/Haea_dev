41aeb160c38adcbc41cad665f5095bf5eaecfb05
wooj1nBot <1403jung@gmail.com>
2024-03-13 08:12:48 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -96,7 +96,7 @@ class Electra(nn.Module):
         x = x.view(x.size(0), -1, x.size(-1))
         # masked.shape = (batch, mask_size, hidden)
         masked = self.generate(x, src_pe, var_list, src_id)
-        for i in range(x.shape(0)):
+        for i in range(x.size(0)):
             masked.append(gen[i, mask_ind[i]].unsqueeze(0))
 
         # out.shape = (batch, var_len, hidden)

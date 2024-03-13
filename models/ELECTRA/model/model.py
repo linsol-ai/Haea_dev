47712998d835be4fd97cb32127006d95b4df13c2
wooj1nBot <1403jung@gmail.com>
2024-03-13 08:16:10 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -120,6 +120,7 @@ class Electra(nn.Module):
     def discriminate(self, x: torch.Tensor, src_pe: torch.Tensor, var_list: torch.Tensor, src_id: torch.Tensor):
         var_seq = var_list.repeat_interleave(x.size(1), dim=1)
         x = self.embedding(x, var_seq, src_pe) * math.sqrt(self.in_dim)
+        
 
 
     def get_var_seq(self, var_list: torch.Tensor, indicate: torch.Tensor, device):

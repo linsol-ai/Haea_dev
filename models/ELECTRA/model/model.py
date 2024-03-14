4a409a6748242c3a301569d52ca6f2a54e55547f
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:40:14 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -150,7 +150,7 @@ class CliBERTLM(nn.Module):
 
 
 class CliBERTPM(nn.Module):
-    def __init__(self, encoder: nn.TransformerEncoder, embedding: Embedding, in_dim: int, 
+    def __init__(self, encoder: nn.TransformerEncoder, embedding: Embedding, decoder: in_dim: int, 
                  num_heads=12, n_layers=3, dropout=0.1, max_lead_time=500, max_var_len=300):
         super().__init__()
         self.in_dim = in_dim

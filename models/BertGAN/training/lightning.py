dc25048044b81cdc6b09c0609450660ff74b7dba
wooj1nBot <1403jung@gmail.com>
2024-03-14 20:17:59 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -106,7 +106,7 @@ class BertGAN(pl.LightningModule):
         label = batch[1]
         lead_time = batch[2]
 
-        real_label = torch.ones((src.size(0), src.size(0)), device=self.device)
+        real_label = torch.ones((src.size(0), src.size(1)), device=self.device)
         fake_label = torch.zeros((src.size(0), 1), device=self.device)
 
         var_seq = self.var_list.repeat_interleave(src.size(1), dim=0).unsqueeze(0).repeat_interleave(src.size(0), dim=0)

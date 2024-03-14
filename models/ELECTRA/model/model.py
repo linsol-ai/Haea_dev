2de1915a76bd12589c1fdbc6612e7a2b3c72bcd6
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:40:46 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -155,7 +155,7 @@ class CliBERTPM(nn.Module):
         super().__init__()
         self.in_dim = in_dim
         self.encoder = encoder
-        self.
+        self.decoder = decoder
         self.embedding = embedding
         self.time_emb = LeadTimeEmbedding(max_lead_time, in_dim)
     

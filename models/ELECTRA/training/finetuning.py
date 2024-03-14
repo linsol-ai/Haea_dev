76e14b9d3666fd9f734ab5a9f73c2806b3781eb1
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:42:19 +0000

    Auto commit

--- models/ELECTRA/training/finetuning.py
+++ models/ELECTRA/training/finetuning.py
@@ -19,7 +19,7 @@ from models.ELECTRA.model.model import CliBERTLM, CliBERTPM
 
 from models.ELECTRA.training.configs import FinetuningConfig
 from models.ELECTRA.training.configs import FinetuningRunConfig
-from models.ELECTRA.training.lightning import FinetuningModule
+from models.ELECTRA.training.lightning import PretrainModule, FinetuningModule
 
 import os
 env_cp = os.environ.copy()

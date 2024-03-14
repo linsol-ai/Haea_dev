144f49a2cf71c3102221c91d1e284bc88d135864
wooj1nBot <1403jung@gmail.com>
2024-03-14 19:38:02 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -46,7 +46,7 @@ class BertGAN(pl.LightningModule):
         self.g_scheduler = CosineWarmupScheduler(
             g_opt, warmup=self.config.warmup_step, max_iters=self.max_iters
         )
-        self.g_scheduler = CosineWarmupScheduler(
+        self.d_scheduler = CosineWarmupScheduler(
             g_opt, warmup=self.config.warmup_step, max_iters=self.max_iters
         )
 

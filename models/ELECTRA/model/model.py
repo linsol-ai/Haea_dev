8811849f7d4839e87c5eed7df9e62590432c3082
wooj1nBot <1403jung@gmail.com>
2024-03-14 19:04:28 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -146,7 +146,7 @@ class CliBERT(nn.Module):
             encoder_layers,
             n_layers
         )
-        self.embedding = Embedding(max_var_len, in_dim, dropout)
+        self.embedding = Embedding(max_lead_time, max_var_len, in_dim, dropout)
         self.decoder = LinearDecoder(in_dim, out_dim, dropout=dropout)
     
 

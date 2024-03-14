c0ecdf4b247feaa6e75b3473cee9f5d354b21c12
wooj1nBot <1403jung@gmail.com>
2024-03-14 20:40:53 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -148,7 +148,7 @@ class BertGAN(pl.LightningModule):
         )
 
         g_opt.zero_grad()
-        self.manual_backward(errG)
+        self.manual_backward(err_g)
         g_opt.step()
 
         self.log_dict({"g_loss": errG, "d_loss": errD}, prog_bar=True)

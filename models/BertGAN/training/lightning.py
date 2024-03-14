5ed69772dc42b34431bb46bb80cea9bfd02394fc
wooj1nBot <1403jung@gmail.com>
2024-03-14 19:32:20 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -19,7 +19,7 @@ def rmse_loss(x, y):
     return torch.sqrt(F.mse_loss(x, y))
 
 
-class TrainModule(pl.LightningModule):
+class B(pl.LightningModule):
 
     def __init__(self, *, model: ClimateTransformer, mean_std: torch.Tensor,
                  max_iters: int, config: TrainingConfig):

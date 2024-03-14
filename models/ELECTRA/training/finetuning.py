1f409175bfdbfd4b33ef30de5857b1fa04c21c36
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:46:03 +0000

    Auto commit

--- models/ELECTRA/training/finetuning.py
+++ models/ELECTRA/training/finetuning.py
@@ -134,7 +134,7 @@ def main(argv):
 
     print("setting lr rate: ", config.training.learning_rate)
 
-    model_pl = PretrainModule(model=model, mean_std=mean_std, max_iters=max_iter)
+    model_pl = PretrainModule(model=model, mean_std=mean_std, var_list=data_module.var_list, config=config.training, max_iters=max_iter)
 
     summary = ModelSummary(model_pl, max_depth=-1)
     print(summary)

92cdb3f53e192e0f6909f625f5281be6287b87fc
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:31:38 +0000

    Auto commit

--- models/ELECTRA/training/configs.py
+++ models/ELECTRA/training/configs.py
@@ -71,3 +71,54 @@ class TrainingRunConfig(BaseModel):
 
 
 
+class TrainingConfig(BaseModel):
+    """A config specification of training the DVAE."""
+
+    learning_rate: float = 2e-4
+    """The learning rate."""
+
+    warmup_step: int = Field(ge=0, default=4000)
+
+    batch_size: int = Field(ge=0, default=256)
+    """"The batch size."""
+
+    max_epochs: int = Field(ge=0, default=100)
+    """The maximum number of training epochs."""
+
+    gradient_clip_val: float | None = None
+    """The value to clip the gradients to."""
+
+    time_len: int = Field(ge=0, default=7)
+
+    mask_ratio: float = 0.3
+
+    air_variable: List[str]
+
+    surface_variable: List[str]
+
+    only_input_variable: List[str] = []
+
+    constant_variable: List[str] = []
+
+    levels: List[int] = []
+
+    train_start: datetime.datetime
+
+    train_end : datetime.datetime
+
+    
+
+class TrainingRunConfig(BaseModel):
+    """A config specification of the training run."""
+
+    model: ModelConfig
+    """A config specification of the model."""
+
+    training: TrainingConfig
+    """A config specification of the training."""
+
+    seed: int = 123
+    """The random seed.""" 
+
+
+

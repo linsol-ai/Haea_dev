7cd353dc51cfd46e6bfc6123caae02814bef6764
wooj1nBot <1403jung@gmail.com>
2024-03-14 19:50:41 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -76,6 +76,7 @@ class BertGAN(pl.LightningModule):
         self.config = config
         self.var_list = var_list
         self.embedding = Embedding(max_var_len, generator.in_dim, generator.dropout)
+        
 
         self.automatic_optimization = False
         self.save_hyperparameters()

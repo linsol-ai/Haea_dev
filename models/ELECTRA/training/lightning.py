3a6166cc9fe279d47e996d9d667c7d7873684064
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:24:59 +0000

    Auto commit

--- models/ELECTRA/training/lightning.py
+++ models/ELECTRA/training/lightning.py
@@ -96,6 +96,83 @@ class PretrainModule(pl.LightningModule):
         self.mean_std = self.mean_std.to(self.device)
         self.model.eval()
 
+class PretrainModule(pl.LightningModule):
+
+    def __init__(self, *, model: CliBERTLM, mean_std: torch.Tensor, var_list: torch.Tensor,
+                 max_iters: int, config: TrainingConfig):
+        
+        super().__init__()
+        self.max_iters = max_iters
+        self.model = model
+        self.mean_std = mean_std
+        self.config = config
+        self.var_list = var_list + 4
+        self.save_hyperparameters()
+
+    
+    def setup(self, stage: str) -> None:
+        print(stage)
+        self.mean_std = self.mean_std.to(self.device)
+        self.var_list = self.var_list.to(self.device)
+
+
+    def configure_optimizers(self) -> Adam:  # noqa: D102
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.learning_rate)
+        self.lr_scheduler = CosineWarmupScheduler(
+            optimizer, warmup=self.config.warmup_step, max_iters=self.max_iters
+        )
+        return optimizer
+
+
+    def _step(self, batch: Dict, mode: str) -> torch.Tensor:
+        src = batch['source']
+        src_id = batch['source_id']
+        label = batch['target']
+        loss = self.model(src, label, self.var_list, src_id)
+        
+        self.log(f"{mode}/mlm_loss", loss, prog_bar=mode == "train", sync_dist=True)
+        return loss
+
+
+    def calculate_rmse_loss(self, predict: torch.Tensor, label: torch.Tensor):
+        # target.size = (batch, var_len, hidden)
+        label = denormalize(label, self.mean_std)
+        # reversed_predict.shape = (batch, var_len, hidden) -> nomalized
+        reversed_predict = denormalize(predict, self.mean_std)
+        loss = rmse_loss(reversed_predict, label)
+        return loss
+
+
+    def calculate_sqare_loss(self, predict: torch.Tensor, label: torch.Tensor):
+        # target.size = (batch, var_len, hidden)
+        label = denormalize(label, self.mean_std)
+        # reversed_predict.shape = (batch, var_len, hidden) -> nomalized
+        reversed_predict = denormalize(predict, self.mean_std)
+        loss = F.mse_loss(reversed_predict, label, reduction='none')
+        return loss
+    
+
+    def optimizer_step(self, *args, **kwargs):
+        super().optimizer_step(*args, **kwargs)
+        self.lr_scheduler.step()  # Step per iteration
+    
+
+    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], _: int) -> torch.Tensor:  # noqa: D102
+        return self._step(batch, "train")
+
+    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:  # noqa: D102
+        return self._step(batch, "val")
+
+    def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], _: int) -> None:  # noqa: D102
+        self._step(batch, "test")
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        return self(batch)
+
+    def setting(self):
+        self.mean_std = self.mean_std.to(self.device)
+        self.model.eval()
+
     def forward(self, batch) -> Tuple[torch.Tensor, torch.Tensor]:
         with torch.no_grad():
             src = batch[0].to(self.device)

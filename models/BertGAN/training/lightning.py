103bca5efb9e87ad0721d00b93ef6618ef727d57
wooj1nBot <1403jung@gmail.com>
2024-03-14 20:29:20 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -114,6 +114,7 @@ class BertGAN(pl.LightningModule):
         src = src.view(src.size(0), -1, src.size(-1))
         src = (self.embedding(src, var_seq, pe) + time_emb) * math.sqrt(self.generator.in_dim)
 
+        # fake.shape = (batch, time * var, hidden)
         fake = self.generator(src)
 
         label = label.view(src.size(0), -1, src.size(-1))

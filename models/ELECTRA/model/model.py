781bcdf00f42d72523e4c1936b2c622be4109556
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:21:19 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -186,7 +186,6 @@ class CliBERTPM(nn.Module):
 
 
 
-
 class Electra(nn.Module):
     def __init__(self, in_dim: int, out_dim: int, 
                  num_heads=12, g_layers=3, d_layers=12, dropout=0.1, 

c992bf056037ec4deb51fcec190af5e30ffddd60
wooj1nBot <1403jung@gmail.com>
2024-03-14 19:33:08 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -3,7 +3,7 @@ import torch.optim.optimizer
 from typing import Tuple
 from torch.optim import Adam
 import torch.nn.functional as F
-from models.BertGAN.model.model import Discriminator
+from models.BertGAN.model.model import Discriminator, Generator
 from models.CiT.training.configs import TrainingConfig
 from models.CiT.training.params_schedule import CosineWarmupScheduler
 import wandb

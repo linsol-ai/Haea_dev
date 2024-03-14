fb176c367dde5a2ca73115f7d6254b12c8c90a3a
wooj1nBot <1403jung@gmail.com>
2024-03-14 19:24:34 +0000

    Auto commit

--- models/BertGAN/model/model.py
+++ models/BertGAN/model/model.py
@@ -52,5 +52,30 @@ class Generator(nn.Module):
         return x
 
 
+class Generator(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int, num_heads=12, n_layers=3, dropout=0.1):
+        super().__init__()
 
+        self.in_dim = in_dim
+        encoder_layers = nn.TransformerEncoderLayer(
+            d_model=in_dim,
+            nhead=num_heads,
+            dim_feedforward=in_dim*4,
+            dropout=dropout,
+            batch_first=True,
+            activation=F.gelu
+        )
+        self.model = nn.TransformerEncoder(
+            encoder_layers,
+            n_layers
+        )
+        self.decoder = LinearDecoder(in_dim, out_dim, dropout=dropout)
+    
+
+    def forward(self, x: torch.Tensor):
+        # src.shape = (batch, time, var_len, hidden), lead_time.shape = (batch)
+        x = self.model(x)
+        # out.shape = (batch, var_len, hidden)
+        x = self.decoder(x)
+        return x
 

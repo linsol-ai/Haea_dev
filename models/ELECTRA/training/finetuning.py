d07e6921178617a598762feff7822b0a67f7328e
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:34:49 +0000

    Auto commit

--- models/ELECTRA/training/finetuning.py
+++ models/ELECTRA/training/finetuning.py
@@ -17,7 +17,7 @@ from datasets.weather_bench import WeatherDataset
 from models.ELECTRA.datasets.dataset import CustomDataset
 from models.ELECTRA.models.model import ClimateTransformer
 
-from models.ELECTRA.training.configs import Fine
+from models.ELECTRA.training.configs import FinetuningConfig
 from models.ELECTRA.training.configs import TrainingRunConfig
 from models.ELECTRA.training.lightning import TrainModule
 

24534fd6a299bb0be4189aeb1dd65a5b6eb2c361
wooj1nBot <1403jung@gmail.com>
2024-03-14 07:20:01 +0000

    Auto commit

--- models/ELECTRA/model/model.py
+++ models/ELECTRA/model/model.py
@@ -174,7 +174,7 @@ class CliBERTPM(nn.Module):
     def forward(self, x: torch.Tensor, lead_time: torch.Tensor, var_seq: torch.Tensor):
         # src.shape = (batch, time, var_len, hidden), lead_time.shape = (batch)
         var_seq = var_seq.repeat_interleave(x.size(1), dim=1)
-        src_pe = self.positional_encoding(x.shape, x.device)
+        src_pe = positional_encoding(x.shape, x.device)
         x = x.view(x.size(0), -1, x.size(-1))
         lead_time = lead_time.unsqueeze(1).repeat(1, x.size(1))
 

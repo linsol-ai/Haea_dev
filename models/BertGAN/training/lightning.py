c6d8a0e481581c57d526ed9403e4c1b6c5c06b83
wooj1nBot <1403jung@gmail.com>
2024-03-14 20:29:49 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -134,7 +134,7 @@ class BertGAN(pl.LightningModule):
             self.discriminator(tgt_fake),
             fake_label
         )
-        errD = errD_real + errD_fake
+        error = 
         
 
         

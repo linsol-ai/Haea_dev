fe3d55a6a81911921094c33492ee9849724ba875
wooj1nBot <1403jung@gmail.com>
2024-03-15 11:43:57 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -117,9 +117,6 @@ class BertGAN(pl.LightningModule):
 
         src = src.view(src.size(0), -1, src.size(-1))
         src_lead_time = lead_time.unsqueeze(1).repeat(1, src.size(1))
-        tgt_lead_time = torch.zeros_like(src_lead_time, device=self.device)
-
-        print(tgt_lead_time)
      
         src = self.embedding(src, var_seq, pe, src_lead_time) * math.sqrt(self.generator.in_dim)
 

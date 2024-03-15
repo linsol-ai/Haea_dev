a98b9eca4c480340eb1f20f1a732bee1729a0581
wooj1nBot <1403jung@gmail.com>
2024-03-15 14:26:13 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -21,7 +21,7 @@ def sigmoid_function(x):
     return 1 - torch.abs(1/(1+torch.exp(-x)) - 0.5)
 
 
-def mse_loss(x, y):
+def mse_loss(x, y, ):
     # MSE 손실을 계산합니다. reduction='none'은 각 요소의 손실을 유지합니다.
     mse = F.mse_loss(x, y, reduction='none')
     # 배치 차원을 제외한 나머지 차원에 대해 평균을 계산합니다.

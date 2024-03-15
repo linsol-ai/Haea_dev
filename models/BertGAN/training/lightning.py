92600640e838552240f5c350a9b18e85ace76c56
wooj1nBot <1403jung@gmail.com>
2024-03-15 10:44:08 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -75,7 +75,7 @@ class BertGAN(pl.LightningModule):
         self.mean_std = mean_std
         self.config = config
         self.var_list = var_list
-        self.embedding = Embedding(max_var_len, generator.in_dim, generator.dropout)
+        self.embedding = Embedding(max_lead_time, max_var_len, generator.in_dim, generator.dropout)
         self.time_emb = LeadTimeEmbedding(max_lead_time, generator.in_dim)
 
         self.automatic_optimization = False

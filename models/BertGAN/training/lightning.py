b6c1ac136fe7a7062e9e205452d8f5d187aca92e
wooj1nBot <1403jung@gmail.com>
2024-03-15 10:45:57 +0000

    Auto commit

--- models/BertGAN/training/lightning.py
+++ models/BertGAN/training/lightning.py
@@ -114,7 +114,6 @@ class BertGAN(pl.LightningModule):
 
         src = src.view(src.size(0), -1, src.size(-1))
         lead_time = lead_time.unsqueeze(1).repeat(1, src.size(1))
-        time_emb = self.time_emb(lead_time)
      
         src = self.embedding(src, var_seq, pe, time_emb) * math.sqrt(self.generator.in_dim)
 

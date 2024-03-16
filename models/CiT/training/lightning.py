941603aa267d266257142fe3db3ffa34e3cb76f4
wooj1nBot <1403jung@gmail.com>
2024-03-16 17:03:39 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -106,7 +106,7 @@ class TrainModule(pl.LightningModule):
         with torch.no_grad():
             batch_size = batch[0].size(0)
             src = batch[0].to(self.device)
-            src = src.view(src.siz)
+            src = src.view(-1, src.size())
             delta = batch[2].to(self.device)
             var_seq = batch[3].to(self.device)
             predict = self.model(src, delta, var_seq)

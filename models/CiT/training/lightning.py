2ffbae7f4506c285f0fd59eaa66af057965143be
wooj1nBot <1403jung@gmail.com>
2024-03-16 17:10:35 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -123,7 +123,7 @@ class TrainModule(pl.LightningModule):
             predict = denormalize(predict, self.mean_std)
             # loss.shape = (batch, time_len, var_len, hidden)
             loss = F.mse_loss(predict, label, reduction='none')
-            # loss.shape = (batch, time_len, var_len)
+            # loss.shape = (batch(lead_days), time_len, var_len)
             loss = loss.mean(dim=-1)
         
         return loss, delta

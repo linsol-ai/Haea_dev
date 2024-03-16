b969dae07ce08a7d0e04308fcc22485312aa3ed8
wooj1nBot <1403jung@gmail.com>
2024-03-16 16:18:44 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -206,8 +206,7 @@ class WeatherDataset:
     def load_one(self, air_variable, surface_variable, only_input_variable=[], constant_variables=[], level=[]) -> Tuple[torch.Tensor, torch.Tensor, VariableVocab]:
         variables = air_variable + surface_variable + only_input_variable
         dataset = self.datasets[0].sel(time=slice(self.start_date, self.end_date))
-        print(min(dataset.time), max(dataset.time))
-        
+
         if len(level) > 0:
             dataset = dataset.sel(level=level)
 

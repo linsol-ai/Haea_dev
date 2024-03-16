5197bee1ea5f5ea0ea7c8f4ae695b67847dd4a1e
wooj1nBot <1403jung@gmail.com>
2024-03-16 17:19:02 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -108,7 +108,7 @@ class TrainModule(pl.LightningModule):
             src = src.squeeze(0)
 
             delta = batch[2].to(self.device)
-            delta = delta.squeeze(0).unsqueeze
+            delta = delta.squeeze(0).unsqueeze(1)
 
             var_seq = batch[3].to(self.device)
             var_seq = var_seq.squeeze(0)

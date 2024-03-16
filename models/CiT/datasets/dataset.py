a801687dd6873c1742c5323d32f128e623c941a7
wooj1nBot <1403jung@gmail.com>
2024-03-16 16:35:54 +0000

    Auto commit

--- models/CiT/datasets/dataset.py
+++ models/CiT/datasets/dataset.py
@@ -44,4 +44,4 @@ class ValidationDataset(Dataset):
         src_st, src_ed = (day) * self.time_len, (day+1) * self.time_len
         tgt_st = self.sample + src_ed
         tgt_ed = tgt_st + self.time_len
-        src = s
+        src = self.sou

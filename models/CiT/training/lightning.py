3a991a1c5559b7774c84e858d831f31184e548c7
wooj1nBot <1403jung@gmail.com>
2024-03-16 17:00:40 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -104,7 +104,7 @@ class TrainModule(pl.LightningModule):
 
     def forward(self, batch) -> Tuple[torch.Tensor, torch.Tensor]:
         with torch.no_grad():
-            batch_size = 
+            batch_size = batch[0].size(0)
             src = batch[0].to(self.device)
             delta = batch[2].to(self.device)
             var_seq = batch[3].to(self.device)

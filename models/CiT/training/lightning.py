344a0a39f1764b29fe777e77c65c29c7aee5aeb9
wooj1nBot <1403jung@gmail.com>
2024-03-19 04:10:09 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -124,8 +124,8 @@ class TrainModule(pl.LightningModule):
 
             if location is not None:
                 precipitation = predict[:, :, idx, location]
-                if len(loss.shape) == 4:
-                    loss = loss.mean(dim=-1)
+                if len(precipitation.shape) == 4:
+                    precipitation = precipitation.mean(dim=-1)
 
             precipitation = predict[:, :, idx]
 

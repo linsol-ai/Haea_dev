b8d41ff261bfeb4b4f783cb1a3343392909ed1b1
wooj1nBot <1403jung@gmail.com>
2024-03-19 04:10:39 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -122,7 +122,7 @@ class TrainModule(pl.LightningModule):
 
             idx = len(self.config.air_variable) * len(self.config.levels) + 5
 
-            precipitation = predict[:, :, idx, location]
+            precipitation = predict[:, :, idx]
             if location is not None:
                 precipitation = predict[:, :, idx, location]
                 if len(precipitation.shape) == 4:

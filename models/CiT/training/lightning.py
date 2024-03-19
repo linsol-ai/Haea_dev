fb6ed41244b705f8eff2799b4cde1c8c69067e99
wooj1nBot <1403jung@gmail.com>
2024-03-19 04:08:44 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -122,7 +122,7 @@ class TrainModule(pl.LightningModule):
 
             idx = len(self.config.air_variable) * len(self.config.levels) + 5
 
-            
+            totaprecipitation
 
             # loss.shape = (batch, time_len, var_len, hidden)
             loss = F.mse_loss(predict, label, reduction='none')

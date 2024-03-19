2c7b9ccb378858ac2345bc7a5ca06206f6e5cbb0
wooj1nBot <1403jung@gmail.com>
2024-03-19 08:49:41 +0000

    Auto commit

--- datasets/weather_bench.py
+++ datasets/weather_bench.py
@@ -128,8 +128,8 @@ class VariableVocab:
     
 
     def get_location(self, latitude, longitude):
-        lat_indices = np.where((source_dataset.latitude >= 33) & (source_dataset.latitude <= 43))[0]
-        lon_indices = np.where((source_dataset.longitude >= 124) & (source_dataset.longitude <= 132))[0]
+        lat_indices = np.where((self.dataset.latitude >= 33) & (self.dataset.latitude <= 43))[0]
+        lon_indices = np.where((self.dataset.longitude >= 124) & (source_dataset.longitude <= 132))[0]
 
 
     def __len__(self):

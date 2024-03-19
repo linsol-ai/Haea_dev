377d9ccc266dd99560a6a0cdc7693e520ae99d1f
wooj1nBot <1403jung@gmail.com>
2024-03-19 04:29:38 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -136,9 +136,9 @@ class TrainModule(pl.LightningModule):
                 p_pred = p_pred.mean(dim=-1)
                 p_label = p_label.mean(dim=-1)
 
-            p_h = torch.logical_and( (p_pred >= p_threshold), ((p_label >= p_threshold))).sum() # 강수 맞침
-            p_f = torch.logical_and( (p_pred >= p_threshold), ((p_label < p_threshold))).sum() 
-            p_m = torch.logical_and( (p_pred < p_threshold), ((p_label >= p_threshold))).sum()
+            p_h = torch.logical_and( (p_pred >= p_threshold), ((p_label >= p_threshold))).sum() # 강수 맞힘
+            p_f = torch.logical_and( (p_pred >= p_threshold), ((p_label < p_threshold))).sum()  # 강수 틀림
+            p_m = torch.logical_and( (p_pred < p_threshold), ((p_label >= p_threshold))).sum() # 
             p_c = torch.logical_and( (p_pred < p_threshold), ((p_label < p_threshold))).sum()
 
             # loss.shape = (batch, time_len, var_len, hidden)

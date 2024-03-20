1b31e47f76864e33581609566cf332f520bf6a99
wooj1nBot <1403jung@gmail.com>
2024-03-20 15:44:17 +0000

    Auto commit

--- models/CiT/models/model.py
+++ models/CiT/models/model.py
@@ -79,7 +79,7 @@ class ClimateTransformer(nn.Module):
 
     def forward(self, x: torch.Tensor, lead_time: torch.Tensor, var_list: torch.Tensor):
         # src.shape = (batch, time, var_len, hidden), lead_time.shape = (batch)
-        var_seq = var_list.repeat_interleave(x.size(1), dim=1).unsqueeze(0).
+        var_seq = var_list.repeat_interleave(x.size(1), dim=1).unsqueeze(0).repeat_interleave(x.size(1), dim=1)
         src_pe = self.positional_encoding(x.shape, x.device)
         x = x.view(x.size(0), -1, x.size(-1))
         lead_time = lead_time.unsqueeze(1).repeat(1, x.size(1))

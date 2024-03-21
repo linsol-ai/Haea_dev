c80848049e4b404c71a0d43ff213593150daf0c9
wooj1nBot <1403jung@gmail.com>
2024-03-21 16:58:09 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -40,6 +40,7 @@ class TrainModule(pl.LightningModule):
         super().__init__()
         self.max_iters = max_iters
         self.model = model
+        self.var_list = var_list
         self.mean_std = mean_std
         self.config = config
         self.save_hyperparameters()

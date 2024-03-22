e2f0761fff9798d5e9d25f4a9de09cd649b5e32d
wooj1nBot <1403jung@gmail.com>
2024-03-22 16:18:39 +0000

    Auto commit

--- models/BERT/datasets/dataset.py
+++ models/BERT/datasets/dataset.py
@@ -3,13 +3,12 @@ import torch
 
 class CustomDataset(Dataset):
 
-    def __init__(self, source_dataset: torch.Tensor, var_seq: torch.Tensor, time_len: int, max_lead_time: int = 78):
+    def __init__(self, source_dataset: torch.Tensor, time_len: int, max_lead_time: int = 78):
         # dataset.shape = (time, var_len, hidden)
         self.source_dataset = source_dataset
         self.max_lead_time = max_lead_time
-        self.var_seq = var_seq
         self.time_len = time_len
-        self.sample = torch.arange(time_len, max_lead_time, step=time_len, dtype=torch.int32)
+        self.sample = torch.arange(0, max_lead_time, step=time_len, dtype=torch.int32)
 
 
     def __len__(self):
@@ -18,11 +17,38 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, t):
         t = max(t, self.time_len-1)
-        delta = self.sample[torch.randint(0, self.sample.size(0)-1, (1,))].item()
+        delta = self.sample[torch.randint(0, self.sample.size(0), (1,))].item()
         t = t - max(0, (t + delta + self.time_len) - (self.source_dataset.size(0) - 1))
         src = self.source_dataset[t-self.time_len+1:t+1]
         next = t + delta + 1
         tgt = self.source_dataset[next:next+self.time_len]
-        return src, tgt, delta, self.var_seq
+        return src, tgt, delta
+        
+
+class ValidationDataset(Dataset):
+    def __init__(self, source_dataset: torch.Tensor, time_len: int, max_lead_time: int = 78):
+        # dataset.shape = (time, var_len, hidden)
+        self.source_dataset = source_dataset
+        self.max_lead_time = max_lead_time
+        self.time_len = time_len
+        self.sample = torch.arange(0, max_lead_time, step=time_len, dtype=torch.int32)
+
+
+    def __len__(self):
+        return (self.source_dataset.size(0) // 24) - (self.sample.size(0) + 1)
+
+
+    def __getitem__(self, day):
+        src_st, src_ed = (day) * self.time_len, (day+1) * self.time_len
+        tgt_st = self.sample + src_ed
+        tgt_ed = tgt_st + self.time_len
+        #src.shape (lead_days, time, var_len, hidden)
+        src = self.source_dataset[src_st:src_ed].unsqueeze(0).repeat_interleave(repeats=self.sample.size(0), dim=0)
+        tgt = []
+        for i in range(self.sample.size(0)):
+            tgt.append(self.source_dataset[tgt_st[i]:tgt_ed[i]].unsqueeze(0))
+        tgt = torch.cat(tgt, dim=0)
+
+        return src, tgt, self.sample
         
 

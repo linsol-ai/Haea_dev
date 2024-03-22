46d13ccad18029a9e4ae867f889993f96525bf9d
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:20:53 +0000

    Auto commit

--- models/BERT/model/model.py
+++ models/BERT/model/model.py
@@ -56,7 +56,7 @@ class LinearDecoder(nn.Module):
 
 class CliBERT(nn.Module):
     def __init__(self, in_dim: int, out_dim: int, 
-                 num_heads=12, n_layers=3, dropout=0.1, max_lead_time=500, max_var_len=300):
+                 num_heads=12, n_layers=3, dropout=0.1, max_var_len=300):
         super().__init__()
         self.in_dim = in_dim
         encoder_layers = nn.TransformerEncoderLayer(

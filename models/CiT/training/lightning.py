48a1feb19d99e23d878b79df1557eaae581997eb
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:09:49 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -3,7 +3,7 @@ import torch.optim.optimizer
 from typing import Tuple
 from torch.optim import Adam
 import torch.nn.functional as F
-from models.CiT.models.model import ClimateTransformer
+from models.CiT.models.model import ClimateTransformer, CliBERT
 from models.CiT.training.configs import TrainingConfig
 from models.CiT.training.params_schedule import CosineWarmupScheduler
 import wandb

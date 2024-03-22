bf8e99abc726d39b7733b835aa47c610338b6db2
wooj1nBot <1403jung@gmail.com>
2024-03-22 16:26:00 +0000

    Auto commit

--- models/BERT/training/lightning.py
+++ models/BERT/training/lightning.py
@@ -133,7 +133,7 @@ class FinetuningModule(pl.LightningModule):
         label = label.view(label.size(0), -1, label.size(-1))
         predict = self.model(src, self.var_list)
         
-        loss = rmse_loss(predict, label)
+        loss = F.mse_loss(predict, label)
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")
         return loss
 

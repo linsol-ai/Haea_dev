ca85366efde4e37f230b9ed283a5b2d61717ecd2
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:12:52 +0000

    Auto commit

--- models/CiT/models/model.py
+++ models/CiT/models/model.py
@@ -169,7 +169,7 @@ class CliBERT(nn.Module):
 
     def forward(self, x: torch.Tensor, var_list: torch.Tensor, src_id: torch.Tensor):
         # src.shape = (batch, time, var_len, hidden), lead_time.shape = (batch)
-        genvar_seq = get_var_seq(var_list, src_id, x.device)
+        var_seq = get_var_seq(var_list, src_id, x.device)
         src_pe = self.positional_encoding(x.shape, x.device)
         x = x.view(x.size(0), -1, x.size(-1))
 

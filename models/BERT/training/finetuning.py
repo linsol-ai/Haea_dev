0e09ac3fc4481b17a6a7347d26006e8e48d9f3aa
wooj1nBot <1403jung@gmail.com>
2024-03-22 16:20:09 +0000

    Auto commit

--- models/BERT/training/finetuning.py
+++ models/BERT/training/finetuning.py
@@ -19,7 +19,7 @@ from models.BERT.models.model import Cli
 
 from models.BERT.training.configs import TrainingConfig
 from models.BERT.training.configs import TrainingRunConfig
-from models.BERT.training.lightning import FinetuningModule
+from models.BERT.training.lightning import FinetuningModule, P
 
 import os
 env_cp = os.environ.copy()

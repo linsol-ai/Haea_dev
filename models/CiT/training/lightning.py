5e6f6d9c03292ee1e60b34ccbd04fbba51d5b54f
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:08:14 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -151,7 +151,7 @@ class TrainModule(pl.LightningModule):
         return loss, p_pred, p_label
 
 
-class TrainModule(pl.LightningModule):
+class PrerainModule(pl.LightningModule):
 
     def __init__(self, *, model: ClimateTransformer, mean_std: torch.Tensor, var_list: torch.Tensor,
                  max_iters: int, config: TrainingConfig):

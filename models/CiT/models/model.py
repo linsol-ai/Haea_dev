299c30ca6942cab57d9d9fdec77d84be76853652
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:10:50 +0000

    Auto commit

--- models/CiT/models/model.py
+++ models/CiT/models/model.py
@@ -167,7 +167,7 @@ class CliBERT(nn.Module):
         self.decoder = LinearDecoder(in_dim, out_dim, dropout=dropout)
     
 
-    def forward(self, x: torch.Tensor, var_list: torch.Tensor):
+    def forward(self, x: torch.Tensor, src_id: torch.Tensor):
         # src.shape = (batch, time, var_len, hidden), lead_time.shape = (batch)
         var_seq = var_list.repeat_interleave(x.size(1), dim=0).unsqueeze(0).repeat_interleave(x.size(0), dim=0)
         src_pe = self.positional_encoding(x.shape, x.device)

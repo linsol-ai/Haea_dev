8b7cc595ec40caffb7e030fdd7c8ace03c74671f
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:17:10 +0000

    Auto commit

--- models/BERT/training/lightning.py
+++ models/BERT/training/lightning.py
@@ -52,9 +52,11 @@ class PretrainModule(pl.LightningModule):
         src_id = batch['source_id']
         label = batch['target']
         loss = self.model(src, self.var_list, src_id)
-        
-        
-        self.log(f"{mode}/mlm_loss", loss, prog_bar=mode == "train", sync_dist=True)
+
+
+        loss = F.mse_loss(predict, label)
+
+        self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")
         return loss
 
 

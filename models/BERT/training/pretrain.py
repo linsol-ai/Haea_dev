0da680ddbc576b03b380643c72ef1221ab41877e
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:19:02 +0000

    Auto commit

--- models/BERT/training/pretrain.py
+++ models/BERT/training/pretrain.py
@@ -14,7 +14,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))))
 from datasets.weather_bench import WeatherDataset, VariableVocab
 from models.BERT.datasets.denoised_dataset import DenoisingDataset, TimeVocab
-from models.BERT.model.model import CliBERTLM
+from models.BERT.model.model import CliBERT
 
 from models.BERT.training.configs import TrainingConfig
 from models.BERT.training.configs import TrainingRunConfig

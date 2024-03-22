d30c79c89f5beed74eb4c5f299e3b001fd543cec
wooj1nBot <1403jung@gmail.com>
2024-03-22 08:15:56 +0000

    Auto commit

--- models/CiT/training/lightning.py
+++ models/CiT/training/lightning.py
@@ -111,7 +111,7 @@ class TrainModule(pl.LightningModule):
             delta = batch[2].to(self.device)
             delta = delta.squeeze(0)
 
-            predict = self.model(src, delta, self.var)
+            predict = self.model(src, delta, self.var_list)
             predict = predict.view(predict.size(0), self.config.time_len, -1, predict.size(-1))
 
             label = batch[1].to(self.device)

7b001be34847ad94ca57ce9c0cc6a59a77989dba
wooj1nBot <1403jung@gmail.com>
2024-03-22 10:11:36 +0000

    Auto commit

--- models/CiT/models/model.py
+++ models/CiT/models/model.py
@@ -3,7 +3,7 @@ from torch import nn
 from torch.nn import functional as F
 import math
 from typing import List
-from models.ELECTRA.datasets.denoised_dataset import TimeVocab
+from models..datasets.denoised_dataset import TimeVocab
 
 class VariableEmbedding(nn.Embedding):
     def __init__(self, var_len, embed_size=768):

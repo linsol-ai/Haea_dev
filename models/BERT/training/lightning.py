fe29b7d3456ce671bcf7f39fad3be921e1f9a696
wooj1nBot <1403jung@gmail.com>
2024-03-22 16:25:44 +0000

    Auto commit

--- models/BERT/training/lightning.py
+++ models/BERT/training/lightning.py
@@ -129,7 +129,6 @@ class FinetuningModule(pl.LightningModule):
     def _step(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], mode: str) -> torch.Tensor:
         src = batch[0]
         label = batch[1]
-        delta = batch[2]
 
         label = label.view(label.size(0), -1, label.size(-1))
         predict = self.model(src, delta, self.var_list)

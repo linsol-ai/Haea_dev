7bd5f127a8408cafd6218b5d16983549d3e2d635
wooj1nBot <1403jung@gmail.com>
2024-03-23 08:21:05 +0000

    Auto commit

--- models/GPT/models/models.py
+++ models/GPT/models/models.py
@@ -186,7 +186,6 @@ class TransformerDecoder(nn.Module):
         for mod in self.layers:
             output = mod(output, tgt_mask=tgt_mask,
                          tgt_key_padding_mask=tgt_key_padding_mask,
-                         memory_key_padding_mask=memory_key_padding_mask,
                          tgt_is_causal=tgt_is_causal,
                          memory_is_causal=memory_is_causal)
 

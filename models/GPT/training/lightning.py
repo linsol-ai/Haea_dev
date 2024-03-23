c793cf238534b498cd9cb72e4f9af6db7ba49540
wooj1nBot <1403jung@gmail.com>
2024-03-23 09:24:25 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -69,34 +69,6 @@ class TrainModule(pl.LightningModule):
 
         return loss
 
-
-    def calculate_rmse_loss(self, predict: torch.Tensor, label: torch.Tensor):
-        # target.size = (batch, time_len, var_len, hidden)
-        var_len = label.size(2)
-        label = denormalize(label, self.mean_std)
-        label = label.view(label.size(0), -1, label.size(3))
-
-        # predict.shape = (batch, time_len * var_len, hidden) -> not nomalized
-        predict = predict.view(predict.size(0), -1, var_len, predict.size(2))
-        # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized
-        reversed_predict = denormalize(predict, self.mean_std)
-        reversed_predict = reversed_predict.view(reversed_predict.size(0), -1, reversed_predict.size(3))
-        # reversed_predict.shape = (batch, time_len * var_len, 1450) -> nomalized
-        loss = rmse_loss(reversed_predict, label)
-        return loss
-
-
-    def calculate_sqare_loss(self, predict: torch.Tensor, label: torch.Tensor):
-        # target.size = (batch, time_len, var_len, hidden)
-        label = denormalize(label, self.mean_std)
-        # predict.shape = (batch, time_len, var_len, 1450) -> not nomalized
-        reversed_predict = denormalize(predict, self.mean_std)
-        # reversed_predict.shape = (batch, time_len * var_len, 1450) -> nomalized
-        loss = F.mse_loss(reversed_predict, label, reduction='none')
-        reversed_predict.cpu().detach()
-        predict.cpu().detach()
-        reversed_predict.cpu().detach()
-        return loss
     
 
     def optimizer_step(self, *args, **kwargs):

e5c75e5020ff207816c06b1d9ba6b17b0f900dae
wooj1nBot <1403jung@gmail.com>
2024-03-23 08:50:26 +0000

    Auto commit

--- models/GPT/datasets/dataset.py
+++ models/GPT/datasets/dataset.py
@@ -16,7 +16,7 @@ class CustomDataset(Dataset):
     def __getitem__(self, t):
         t = max(t, 1)
         t = min(t + self.time_len + 2, self.source_dataset.size(0) - 1) - (self.time_len + 2)
-        src = self.source_dataset[t:next+self.time_len]
+        src = self.source_dataset[t:t + self.time_len + 2]
         return src, tgt, delta
         
 

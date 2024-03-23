c692896c9b694dd6f998cc3268ebf27e279f5ddc
wooj1nBot <1403jung@gmail.com>
2024-03-23 09:03:43 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -3,7 +3,7 @@ import torch.optim.optimizer
 from typing import Tuple
 from torch.optim import Adam
 import torch.nn.functional as F
-from models.GPT.models
+from models.GPT.models.models import CliGPT
 from models.GPT.training.configs import TrainingConfig
 from models.GPT.training.params_schedule import CosineWarmupScheduler
 import wandb

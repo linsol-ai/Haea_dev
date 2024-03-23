7e3b56e76f881c4850b1374a9781b6bd5e7da62d
wooj1nBot <1403jung@gmail.com>
2024-03-23 08:27:35 +0000

    Auto commit

--- models/GPT/models/models.py
+++ models/GPT/models/models.py
@@ -161,7 +161,7 @@ class CliGPT(nn.Module):
         super().__init__()
         self.in_dim = in_dim
 
-        self.model = Tra(
+        self.model = TransformerDecoder(
             d_model=in_dim,
             nhead=num_heads,
             num_encoder_layers=n_encoder_layers,

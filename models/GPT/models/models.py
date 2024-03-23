fb543d2822590449d7aebc2266be3c8c5923e27e
wooj1nBot <1403jung@gmail.com>
2024-03-23 08:28:42 +0000

    Auto commit

--- models/GPT/models/models.py
+++ models/GPT/models/models.py
@@ -170,8 +170,8 @@ class CliGPT(nn.Module):
         )
 
         self.model = TransformerDecoder(
-            self.decoder_layer,
-            
+            decoder_layer=self.decoder_layer,
+
         )
         
         self.embedding = Embedding(max_var_len, in_dim, dropout)

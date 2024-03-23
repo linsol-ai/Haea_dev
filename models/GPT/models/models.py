57f1a1de70553a093a91dc02c4de4088a2d2cdd9
wooj1nBot <1403jung@gmail.com>
2024-03-23 08:14:06 +0000

    Auto commit

--- models/GPT/models/models.py
+++ models/GPT/models/models.py
@@ -104,7 +104,6 @@ class TransformerDecoderLayer(nn.Module):
             x = x + self._ff_block(self.norm3(x))
         else:
             x = self.norm1(x + self._sa_block(x, tgt_mask, tgt_key_padding_mask, tgt_is_causal))
-            x = self.norm2(x + self._mha_block(x, memory, memory_mask, memory_key_padding_mask, memory_is_causal))
             x = self.norm3(x + self._ff_block(x))
 
         return x

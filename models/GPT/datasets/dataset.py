d46a2df9fcd843eae00e16e1c65b7e8946f84794
wooj1nBot <1403jung@gmail.com>
2024-03-23 08:50:13 +0000

    Auto commit

--- models/GPT/datasets/dataset.py
+++ models/GPT/datasets/dataset.py
@@ -16,11 +16,6 @@ class CustomDataset(Dataset):
     def __getitem__(self, t):
         t = max(t, 1)
         t = min(t + self.time_len + 2, self.source_dataset.size(0) - 1) - (self.time_len + 2)
-
-        delta = self.sample[torch.randint(0, self.sample.size(0), (1,))].item()
-        t = t - max(0, (t + delta + self.time_len) - (self.source_dataset.size(0) - 1))
-        src = self.source_dataset[t-self.time_len+1:t+1]
-        next = t + delta + 1
         tgt = self.source_dataset[next:next+self.time_len]
         return src, tgt, delta
         

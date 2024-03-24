f2f7bde8b77db586434eb7939823f51937532476
wooj1nBot <1403jung@gmail.com>
2024-03-24 09:40:42 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -50,7 +50,6 @@ class TrainModule(pl.LightningModule):
 
     def configure_optimizers(self) -> Adam:  # noqa: D102
         opt1 = torch.optim.Adam(self.parameters(), lr=self.config.learning_rate)
-        opt2 = torch.optim.Adam(self.parameters(), lr=self.config.learning_rate)
         self.lr1 = CosineWarmupScheduler(
             opt1, warmup=self.config.warmup_step, max_iters=self.max_iters
         )

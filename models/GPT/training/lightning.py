22396b2e88bd2633dae1f306f3c2d5198fa8ab7e
wooj1nBot <1403jung@gmail.com>
2024-03-24 04:24:11 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -93,7 +93,7 @@ class TrainModule(pl.LightningModule):
     def forward(self, batch: torch.Tensor) -> torch.Tensor:
         with torch.no_grad():
             batch = batch.to(self.device)
-            src = batch[:self.config.]
+            src = batch[:self.config.tie]
             
 
             return loss

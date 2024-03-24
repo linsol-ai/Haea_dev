2fc4a1f70eb039f5db6b1648c30cba063e942cbd
wooj1nBot <1403jung@gmail.com>
2024-03-24 04:33:14 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -96,7 +96,7 @@ class TrainModule(pl.LightningModule):
             src = batch[:self.config.time_len]
             for i in range(src.size(1)-self.config.time_len):
                 predict = self.model(src, self.var_list, self.tgt_mask)
-                token = 
+                token = predict[]
             
 
             return loss

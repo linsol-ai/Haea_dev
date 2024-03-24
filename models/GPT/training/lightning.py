5d8b1a10cc2bae91f9b5737d7ce8942d5e01dcf3
wooj1nBot <1403jung@gmail.com>
2024-03-24 04:46:31 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -104,7 +104,7 @@ class TrainModule(pl.LightningModule):
                 predict_all[:, i] = token
                 src = torch.cat([src[:, 1:], token], dim=1)
 
-
+            
 
             return loss
     

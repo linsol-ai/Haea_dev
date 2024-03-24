ad491090210982ed7c3892d239fa9bfea532685c
wooj1nBot <1403jung@gmail.com>
2024-03-24 09:45:23 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -65,10 +65,12 @@ class TrainModule(pl.LightningModule):
         label = batch[:, 1:-1]
         label = label.view(label.size(0), -1, label.size(-1))
         loss = F.mse_loss(pred, label)
-        
+
         optimizer.zero_grad()
         self.manual_backward(loss)
         optimizer.step()
+
+        
 
 
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")

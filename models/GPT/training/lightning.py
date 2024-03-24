3d29f4292f7a949273f8715d02503e14de00e1e5
wooj1nBot <1403jung@gmail.com>
2024-03-24 04:27:38 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -94,7 +94,7 @@ class TrainModule(pl.LightningModule):
         with torch.no_grad():
             batch = batch.to(self.device)
             src = batch[:self.config.time_len]
-            for i in range(1, src.size(1))
+            for i in range(1, src.size(1)-se)
             
 
             return loss

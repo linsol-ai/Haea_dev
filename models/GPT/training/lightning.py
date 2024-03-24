9aa2d2c66f41e2e43086e5f5bae3f9fbf9a6d06d
wooj1nBot <1403jung@gmail.com>
2024-03-24 09:47:38 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -71,6 +71,7 @@ class TrainModule(pl.LightningModule):
         optimizer.step()
 
         pred = self.model(pred, self.var_list, self.tgt_mask)
+        
 
 
         self.log(f"{mode}/mse_loss", loss, prog_bar=mode == "train")

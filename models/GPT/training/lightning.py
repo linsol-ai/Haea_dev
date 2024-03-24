a05bedf8cd0fc48c72bf4a50d0cd87b20dd796ce
wooj1nBot <1403jung@gmail.com>
2024-03-24 10:28:22 +0000

    Auto commit

--- models/GPT/training/lightning.py
+++ models/GPT/training/lightning.py
@@ -81,7 +81,7 @@ class TrainModule(pl.LightningModule):
 
         optimizer.zero_grad()
         self.manual_backward(loss)
-          self.clip_gradients(optimizer, gradient_clip_val=0.5, gradient_clip_algorithm="norm")
+        self.clip_gradients(optimizer, gradient_clip_val=0.5, gradient_clip_algorithm="norm")
         optimizer.step()
         self.lr_scheduler.step()
 
